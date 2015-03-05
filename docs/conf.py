b9bab5ed13a5e7ab402082d6bb7d5ebdd551e0bb
Daniel Roth <danroth27@hotmail.com>
2015-03-05 10:24:15 -0800

    Fixing copyright

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'aspnet'
-copyright = '2015, Daniel Roth'
+copyright = '2015 Microsoft'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
