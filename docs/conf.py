706aa9c559b035b0ad6ef6449aeb67e3686ddd07
Steve Smith <steve@kentsmiths.com>
2015-03-06 16:39:36 -0500

    Getting RTD theme working locally

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,16 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+
+# This allows sphinx_rtd_theme to work locally
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:
+	import sphinx_rtd_theme
+	html_theme = 'sphinx_rtd_theme'
+	html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+#html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
