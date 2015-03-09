e0bbbcbcd071c625faf6ad97aa5c4f56f5d3e1f4
Steve Smith <steve@kentsmiths.com>
2015-03-09 16:24:17 -0400

    Adding support for custom css within RTD theme.

--- docs/conf.py
+++ docs/conf.py
@@ -107,6 +107,17 @@ if not on_rtd:
 	import sphinx_rtd_theme
 	html_theme = 'sphinx_rtd_theme'
 	html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+	def setup(app):
+		app.add_stylesheet('custom.css')
+else:
+	html_context = {
+		'css_files': [
+			'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+			'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+			'_static/custom.css',
+		],
+	}
+	
 
 #html_theme = 'default'
 
