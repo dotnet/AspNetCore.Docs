824c0e173de9c3f34f0fcbd2eae4949c550291e7
Steve Smith <steve@kentsmiths.com>
2015-03-11 16:48:48 -0400

    Trying out new sphinx_rtd_theme feature supporting custom CSS files so previous hack no longer required (see: https://github.com/snide/sphinx_rtd_theme/commit/8c83a8822c3aa1962fa6aa5136fc09124b0215b9 )

--- docs/conf.py
+++ docs/conf.py
@@ -109,15 +109,17 @@ if not on_rtd:
 	html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 	def setup(app):
 		app.add_stylesheet('custom.css')
-else:
-	html_context = {
-		'css_files': [
-			'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-			'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-			'_static/custom.css',
-		],
-	}
+# else:
+	# html_context = {
+		# 'css_files': [
+			# 'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+			# 'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+			# '_static/custom.css',
+		# ],
+	# }
 
+extra_css_files = ['_static/custom.css']
+	
 #html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
