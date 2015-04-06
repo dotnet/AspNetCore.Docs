f458046e1ed402cb954e95213a604026be18373b
Steve Smith <steve@kentsmiths.com>
2015-04-06 11:36:47 -0400

    Moving MVC5 Starter Web sample to /samples

---
+++ samples/MigratingFromMvc5/Mvc5Project/Mvc5Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc5Project.MvcApplication" Language="C#" %>
---
+++ samples/MigratingFromMvc5/Mvc5Project/Mvc5Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5Project.Startup))]
+namespace Mvc5Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
