46605c94385fca64f56900bcf119bade96fa64ae
Steve Smith <steve@kentsmiths.com>
2015-04-06 13:19:34 -0400

    Checking in simple migrated project

---
+++ samples/MigratingFromMvc5/NewMvc6Project/src/NewMvc6Project/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NewMvc6Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ samples/MigratingFromMvc5/NewMvc6Project/src/NewMvc6Project/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.AspNet.Hosting;
+
+namespace NewMvc6Project
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            // Setup configuration sources.
+            Configuration = new Configuration()
+                    .AddJsonFile("config.json")
+                    .AddEnvironmentVariables();
+        }
+
+    // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
