da305044a182cc279e4f841f9cd0642a63faf895
danroth27 <daroth@microsoft.com>
2015-05-07 17:41:24 -0700

    Adding analytics

---
+++ docs/_static/wedc.js
@@ -0,0 +1,8 @@
+var varSegmentation = 0;
+var varClickTracking = 1;
+var varCustomerTracking = 1;
+var varAutoFirePV = 1;
+var Route = 12656;
+var Ctrl = "";
+
+document.write("<script type='text/javascript' src='" + (window.location.protocol) + "//c.microsoft.com/ms.js'" + "'><\/script>");   
