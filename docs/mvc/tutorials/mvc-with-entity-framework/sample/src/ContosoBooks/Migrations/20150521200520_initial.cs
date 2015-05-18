9035e0eb153392ff4a507e693c68f6ac0e38d3f0
Mike Wasson <mwasson@microsoft.com>
2015-05-18 12:59:49 -0700

    # This is a combination of 7 commits. # The first commit's message is: MVC6 with EF7 tutorial
    # This is the 2nd commit message:
    
    Changes from code review
    
    # This is the 3rd commit message:
    
    Use rowan's seed pattern (code review)
    
    # This is the 4th commit message:
    
    Remove stuff under wwwroot/lib
    
    # This is the 5th commit message:
    
    Eilon code review
    
    # This is the 6th commit message:
    
    put AddInMemoryStore back
    
    # This is the 7th commit message:
    
    Tweak sample code to match tutorial that I'm writing.

---
+++ docs/mvc/tutorials/mvc-with-entity-framework/sample/src/ContosoBooks/Migrations/20150521200520_initial.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using Microsoft.Data.Entity.Relational.Migrations;
+using Microsoft.Data.Entity.Relational.Migrations.Builders;
+using Microsoft.Data.Entity.Relational.Migrations.Operations;
+
+namespace ContosoBooks.Migrations
+{
+    public partial class initial : Migration
+    {
+        public override void Up(MigrationBuilder migration)
+        {
+            migration.CreateSequence(
+                name: "DefaultSequence",
+                type: "bigint",
+                startWith: 1L,
+                incrementBy: 10);
+            migration.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    AuthorID = table.Column(type: "int", nullable: false),
+                    FirstMidName = table.Column(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.AuthorID);
+                });
+            migration.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    AuthorID = table.Column(type: "int", nullable: false),
+                    BookID = table.Column(type: "int", nullable: false),
+                    Genre = table.Column(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column(type: "decimal(18, 2)", nullable: false),
+                    Title = table.Column(type: "nvarchar(max)", nullable: true),
+                    Year = table.Column(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.BookID);
+                    table.ForeignKey(
+                        name: "FK_Book_Author_AuthorID",
+                        columns: x => x.AuthorID,
+                        referencedTable: "Author",
+                        referencedColumn: "AuthorID");
+                });
+        }
+        
+        public override void Down(MigrationBuilder migration)
+        {
+            migration.DropSequence("DefaultSequence");
+            migration.DropTable("Author");
+            migration.DropTable("Book");
+        }
+    }
+}
