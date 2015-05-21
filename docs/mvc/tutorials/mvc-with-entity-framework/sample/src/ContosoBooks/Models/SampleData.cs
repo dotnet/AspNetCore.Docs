ad65eb67714de9633ea7d9bbffd38d31cb1b5590
Mike Wasson <mwasson@microsoft.com>
2015-05-21 16:25:20 -0700

    Use rowan's seed pattern (code review)

--- docs/mvc/tutorials/mvc-with-entity-framework/sample/src/ContosoBooks/Models/SampleData.cs
+++ docs/mvc/tutorials/mvc-with-entity-framework/sample/src/ContosoBooks/Models/SampleData.cs
@@ -5,6 +5,7 @@ using Microsoft.Framework.DependencyInjection;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Data.Entity;
+using Microsoft.AspNet.Builder;
 
 namespace ContosoBooks.Models
 {
@@ -12,50 +13,37 @@ namespace ContosoBooks.Models
     {
         public static void Initialize(IServiceProvider serviceProvider)
         {
-            using (var db = serviceProvider.GetService<BookContext>())
-            { 
-                InsertTestData(db);
+            var context = serviceProvider.GetService<BookContext>();
+            if (!context.Books.Any())
+            {
+                var austen = context.Authors.Add(new Author { LastName = "Austen", FirstMidName = "Jane" }).Entity;
+                var dickens = context.Authors.Add(new Author { LastName = "Dickens", FirstMidName = "Charles" }).Entity;
+                var cervantes = context.Authors.Add(new Author { LastName = "Cervantes", FirstMidName = "Miguel" }).Entity;
+
+                context.Books.AddRange(
+                    new Book()
+                    {
+                        Title = "Pride and Prejudice", Year = 1813, Author = austen,
+                        Price = 9.99M, Genre = "Comedy of manners"
+                    },
+                    new Book()
+                    {
+                        Title = "Northanger Abbey", Year = 1817, Author = austen,
+                        Price = 12.95M, Genre = "Gothic parody"
+                    },
+                    new Book()
+                    {
+                        Title = "David Copperfield", Year = 1850, Author = dickens,
+                        Price = 15, Genre = "Bildungsroman"
+                    },
+                    new Book()
+                    {
+                        Title = "Don Quixote", Year = 1617, Author = cervantes,
+                        Price = 8.95M, Genre = "Picaresque"
+                    }
+                );
+                context.SaveChanges();
             }
-        }
-
-        private static void InsertTestData(BookContext db)
-        {
-            var authors = new Author[]
-            {
-                    new Author() { LastName = "Austen", FirstMidName = "Jane" },
-                    new Author() { LastName = "Dickens", FirstMidName = "Charles" },
-                    new Author() { LastName = "Cervantes", FirstMidName = "Miguel" }
-            };
-
-            foreach (var item in authors)
-            {
-                if (!db.Authors.Any(x => x.LastName.Equals(item.LastName)))
-                {
-                    db.Authors.Add(item);
-                }
-            }
-            db.SaveChanges();
-
-            var books = new Book[]
-            {
-                new Book() { Title = "Pride and Prejudice", Year = 1813, AuthorID = authors[0].AuthorID,
-                            Price = 9.99M, Genre = "Comedy of manners" },
-                new Book() { Title = "Northanger Abbey", Year = 1817, AuthorID = authors[0].AuthorID,
-                    Price = 12.95M, Genre = "Gothic parody" },
-                new Book() { Title = "David Copperfield", Year = 1850, AuthorID = authors[1].AuthorID,
-                    Price = 15, Genre = "Bildungsroman" },
-                new Book() { Title = "Don Quixote", Year = 1617, AuthorID = authors[2].AuthorID,
-                    Price = 8.95M, Genre = "Picaresque" }
-                };
-
-            foreach (var item in books)
-            {
-                if (!db.Books.Any(x => x.Title.Equals(item.Title)))
-                {
-                    db.Books.Add(item);
-                }
-            }
-            db.SaveChanges();
         }
     }
 }
