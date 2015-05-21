5e68ff28fabb48354259b39bd96f3c63c8e6657d
Mike Wasson <mwasson@microsoft.com>
2015-05-21 13:16:01 -0700

    Add cors articles

---
+++ docs/mvc/security/cors-policy/sample/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.DependencyInjection;
+
+namespace CorsMvc
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.ConfigureCors(options =>
+            {
+                // Define one or more CORS policies
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder =>
+                    {
+                        builder.WithOrigins("http://example.com");
+                    });
+            });
+
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new CorsAuthorizationFilterFactory("AllowSpecificOrigin"));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
---
+++ docs/security/cors/sample/src/CorsExample1/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace CorsExamples
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+        }
+
+        // Shows UseCos with CorsPolicyBuilder
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCors(builder =>
+                builder.WithOrigins("http://example.com"));
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
