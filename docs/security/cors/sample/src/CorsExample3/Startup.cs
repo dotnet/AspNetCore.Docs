770b309729f7c69c501243ef6ce7d017c5ddfcfa
Mike Wasson <mwasson@microsoft.com>
2015-05-21 13:16:01 -0700

    Add cors example code

---
+++ docs/security/cors/sample/src/CorsExample3/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace CorsExample3
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+        }
+
+        // Shows fluent API
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCors(builder =>
+                builder.WithOrigins("http://example.com")
+                       .AllowAnyHeader()
+                );
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
