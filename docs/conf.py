ed51db100b7686d66f5de79cd46d94e773183f59
Daniel Roth <danroth27@hotmail.com>
2015-05-22 14:47:38 -0700

    Fixing prolog to include authors.txt

--- docs/conf.py
+++ docs/conf.py
@@ -99,14 +99,7 @@ rst_prolog = """
 
 	<noscript><img alt="" width="1" height="1" src="https://c.microsoft.com/trans_pixel.aspx"/></noscript>
 	
-.. _Daniel Roth: https://github.com/danroth27
-.. _Rick Anderson: https://github.com/rick-anderson
-.. _Steve Smith: https://github.com/ardalis
-.. _Noel Rice: https://github.com/noelatfalafel
-.. _Venkata Koppaka: https://github.com/vkoppaka
-.. _Mike Wasson: https://github.com/mikewasson
-.. _Erik Reitan: https://github.com/Erikre
-.. _Pranav Rastogi: https://github.com/rustd
+.. include:: /authors.txt
 """
 
 # -- Options for HTML output ----------------------------------------------
