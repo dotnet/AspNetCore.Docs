09e0390c6fa434907d7cfa90dca6d172d6cbdced
danroth27 <daroth@microsoft.com>
2015-05-22 15:16:37 -0700

    Enable intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,9 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.intersphinx',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
