e548ae5aa278df9317474d5ce652f84aa9d841b2
danroth27 <daroth@microsoft.com>
2015-07-31 16:46:28 -0700

    Update CORS article to beta6

--- aspnet/security/cors/sample/src/CorsExample4/Startup.cs
+++ aspnet/security/cors/sample/src/CorsExample4/Startup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
-using Microsoft.Framework.DepEND0encyInjection;
+using Microsoft.Framework.DependencyInjection;
 
 namespace CorsExample4
 {
