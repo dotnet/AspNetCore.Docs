c1c69f54363235e68769f3e7e1958ade53792ee9
Rick Anderson <riande@microsoft.com>
2015-09-02 13:41:43 -0700

    new TH authoring article

---
+++ mvc/views/tag-helpers/authoring/sample/TagHlp/src/AuthoringTagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using AuthoringTagHelpers.Models;
+
+namespace AuthoringTagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(bool approved = false)
+        {
+            return View(new WebsiteContext
+            {
+                Approved = approved,
+                CopyrightYear = 2015,
+                Version = new Version(1, 3, 3, 7),
+                TagsToShow = 20
+            });
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+             //return View("ContactCopy");
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
