915d92eedde246de0ce045c006c9081d5f4bcbbd
Rick Anderson <riande@microsoft.com>
2015-09-29 10:55:17 -0700

    update

---
+++ aspnet/security/accconfirm/sample/WebApplication1/src/WebApplication1/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
