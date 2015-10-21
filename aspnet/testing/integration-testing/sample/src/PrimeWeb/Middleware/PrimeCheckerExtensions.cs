c0654b06c24985148105f88bf253d1623f286a91
Steve Smith <steve@kentsmiths.com>
2015-10-21 10:34:11 -0400

    Add integration testing article.

---
+++ aspnet/testing/integration-testing/sample/src/PrimeWeb/Middleware/PrimeCheckerExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using System;
+using PrimeWeb.Services;
+
+namespace PrimeWeb.Middleware
+{
+    public static class PrimeCheckerExtensions
+    {
+        public static IApplicationBuilder UsePrimeChecker(this IApplicationBuilder builder,
+            PrimeCheckerOptions options)
+        {
+            if(builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            var primeService = builder.ApplicationServices.GetService(typeof (PrimeService)) as PrimeService;
+            return builder.Use(next => new PrimeCheckerMiddleware(next, options, primeService).Invoke);
+        }
+
+        public static IApplicationBuilder UsePrimeChecker(this IApplicationBuilder builder,
+            PathString path)
+        {
+            return UsePrimeChecker(builder, new PrimeCheckerOptions { Path = path });
+        }
+        public static IApplicationBuilder UsePrimeChecker(this IApplicationBuilder builder,
+            string path)
+        {
+            return UsePrimeChecker(builder, new PrimeCheckerOptions { Path = new PathString(path) });
+        }
+
+        public static IApplicationBuilder UsePrimeChecker(this IApplicationBuilder builder)
+        {
+            return UsePrimeChecker(builder, 
+                new PrimeCheckerOptions()
+                {
+                    Path = new PathString("/checkprime")
+                });
+        }
+    }
+}
