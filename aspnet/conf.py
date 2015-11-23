9a2c03398c71fe83345b76cb26d07b53e42a1d51
danroth27 <daroth@microsoft.com>
2015-11-23 13:34:21 -0800

    Fixing python typo in conf.py

--- aspnet/conf.py
+++ aspnet/conf.py
@@ -118,7 +118,7 @@ if not on_rtd:
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 else:
-    extenions.append('yasfb')
+    extensions.append('yasfb')
     feed_num_items = 15
     feed_skip_regex = '(.)*index'
     feed_base_url = 'http://docs.asp.net/en/latest'
