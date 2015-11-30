c9b5be5371fd57d1e8df8ea8dce94acaed30806a
Erik Reitan <erikre@microsoft.com>
2015-11-30 13:49:10 -0800

    Update to topic - Understanding ASP.NET 5 Web Apps

---
+++ aspnet/conceptual-overview/understanding-aspnet5-apps/sample/src/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            string appName = _config["ApplicationName"];
+            ViewData["Message"] = "You application name: " + appName;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ aspnet/conceptual-overview/understanding-aspnet5-apps/sample/src/WebApplication1/wwwroot/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../gulpfile.js" />
+/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+/// <reference path="lib/jquery-validation/jquery.validate.js" />
+/// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="js/site.js" />
