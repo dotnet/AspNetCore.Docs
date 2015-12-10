6fa99c302e7859c33923849fc312caa46edf040a
Scott Sargent <scott@devresults.com>
2015-12-10 16:05:10 -0500

    Porting deprecated sample to routing specific sample

---
+++ mvc/controllers/routing/sample/routingSample/Models/OrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcOrderManagerSample.Models
+{
+    public class OrderModel
+    {
+        public int OrderID { get; set; }
+        public string Client { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ mvc/controllers/routing/sample/routingSample/Models/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcOrderManagerSample.Models
+{
+ 
+    public class OrderViewModel
+    {
+        public int OrderID { get; set; }
+        public string Subject { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
