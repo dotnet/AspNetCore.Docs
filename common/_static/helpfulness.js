d18b5414284da9319d54ee651a5654a6bf64c221
mscommunities <mscommunities1@hotmail.com>
2016-01-08 12:29:51 -0800

    Add helpfulness API
    Updating link and background colors to match new asp.net design
    
    Clipping body in mobile view
    
    Updating helpfulness to use new api
    
    Using new api for helpfulness
    
    Using https endpoint for api call
    
    Rev'ng the version querystring on static assets
    
    Resolve merge conflict

--- common/_static/helpfulness.js
+++ common/_static/helpfulness.js
@@ -122,7 +122,7 @@ $.throttle = function (fn, threshhold, scope) {
 
 				$.ajax({
 					context: this,
-					url: options.domain + "/base/HelpfulnessLibrary/ReportSummaryById",
+					url: options.domain + "/umbraco/api/helpfulnessapi/reportsummarybyid",
 					type: "POST",
 					data: {
 						contentId: this.targetDocumentId,
@@ -199,7 +199,7 @@ $.throttle = function (fn, threshhold, scope) {
 
 					$.ajax({
 						context: this,
-						url: options.domain + "/base/HelpfulnessLibrary/SubmitYes",
+						url: options.domain + "/umbraco/api/helpfulnessapi/submityes",
 						type: "POST",
 						data: {
 							pickerId: this.targetPickerId,
@@ -239,7 +239,7 @@ $.throttle = function (fn, threshhold, scope) {
 
 					$.ajax({
 						context: this,
-						url: options.domain + "/base/HelpfulnessLibrary/SubmitNo",
+						url: options.domain + "/umbraco/api/helpfulnessapi/submitno",
 						type: "POST",
 						data: {
 							pickerId: this.targetPickerId,
@@ -313,7 +313,7 @@ $(function ()
 	$.Helpfulness({
 		maxCharacters: 1000,
 		alwaysFixed: true,
-		domain: "http://www.asp.net",
+		domain: "https://www.asp.net",
 		isDocs: true
 	}).init();
 });
