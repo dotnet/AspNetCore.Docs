1eda14e9f20db5205bb88c936f602045eecbafe1
danroth27 <daroth@microsoft.com>
2016-01-26 16:53:34 -0800

    Remove unnecessary if check

--- common/_static/disqus.js
+++ common/_static/disqus.js
@@ -8,11 +8,9 @@ $(function(){
       var disqus_shortname = 'aspnetdocs'; // required: replace example with your forum shortname
 
       /* * * DON'T EDIT BELOW THIS LINE * * */
-      if (!$("#disqus_thread").html()) {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-      }
+      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+      dsq.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
       
       button.hide();
     };
