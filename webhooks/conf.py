95b48c3887881f706bbfa559636762f758309da8
Scott Hanselman <scott@hanselman.com>
2016-02-24 12:46:44 -0800

    Copyright year

--- webhooks/conf.py
+++ webhooks/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ASP.NET WebHooks'
-copyright = '2015, Microsoft'
+copyright = '2016, Microsoft'
 author = 'Microsoft'
 
 # The version info for the project you're documenting, acts as replacement for
