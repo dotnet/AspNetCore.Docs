d502eb10f45207af1c492434853652e3303945d6
RachelAppel <rachelap@microsoft.com>
2016-03-03 13:28:23 -0500

    init
    fix link
    
    minor fixes
    
    fix link
    
    code formatting
    
    consolidate sample files
    
    formatting fix
    
    modifications after feedback
    
    changes after feedback
    
    changes after feedback
    
    edits
    
    edits
    
    edits
    
    edits
    
    changes

---
+++ aspnet/mvc/models/validation/sample/ClassicMovieAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class ClassicMovieAttribute : ValidationAttribute, IClientModelValidator
+    {
+        private int _year;
+        public ClassicMovieAttribute(int Year)
+        {
+            _year = Year;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Movie movie = (Movie)validationContext.ObjectInstance;
+
+            if (movie.Genre == Genre.Classic)
+            {
+                if (movie.ReleaseDate.Year < _year)
+                {
+                    return new ValidationResult(
+                        "Classic movies must have a release year earlier than " + this._year);
+
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        public IEnumerable<ModelClientValidationRule>
+        GetClientValidationRules(ClientModelValidationContext context)
+        {
+            yield return new ModelClientValidationRule("classicmovie", 
+                "Classic movies must have a release year earlier than " + this._year);
+        }
+    }
+}
---
+++ aspnet/mvc/models/validation/sample/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCMovie.Models
+{
+    public class Movie   
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [ClassicMovie(1960)]
+        public DateTime ReleaseDate { get; set; }
+        
+        [Required]
+        [StringLength(1000)]
+        public string Description { get; set; }
+ 
+        [Required()]
+        [Range(0, 999.99)]        
+        public float Price { get; set; }
+        
+        [Required]
+        public Genre Genre { get; set; }
+
+        public bool Preorder { get; set; }
+
+        [Required]
+        public Audience Audience { get; set; }
+        public List<Review> Reviews { get; set; }  
+    }
+
+}
+
