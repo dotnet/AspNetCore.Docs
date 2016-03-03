cd921cfd8067e50604172e84132f60af87940f21
mperdeck <matt.perdeck@super-ip.com.au>
2016-03-03 17:10:21 +1100

    Packages clean up

--- aspnet/migration/http-modules/sample/Asp.Net4/Asp.Net4/App_Start/RouteConfig.cs
+++ aspnet/migration/http-modules/sample/Asp.Net4/Asp.Net4/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Asp.Net4
--- aspnet/migration/http-modules/sample/Asp.Net4/Asp.Net4/Global.asax.cs
+++ aspnet/migration/http-modules/sample/Asp.Net4/Asp.Net4/Global.asax.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
+﻿using System.Web.Routing;
 
 namespace Asp.Net4
 {
