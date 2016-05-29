810a11cf1facf6a6654f80d6bf5fde8cf9827845
danroth27 <daroth@microsoft.com>
2016-05-28 19:22:40 -0700

    Revert "Enable sphinx .NET domain links"
    This reverts commit 603381d98767bfa9df6ff0b74376c2115efcb047.

--- api/conf.py
+++ api/conf.py
@@ -305,6 +305,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'aspnet': ('http://docs.asp.net/en/latest', '../common/aspnet.inv')
+    'aspnet': ('http://docs.asp.net/en/latest', '../common/aspnet.inv'),
+    'mvc': ('http://docs.asp.net/projects/mvc/en/latest', '../common/mvc.inv'),
 }
 
