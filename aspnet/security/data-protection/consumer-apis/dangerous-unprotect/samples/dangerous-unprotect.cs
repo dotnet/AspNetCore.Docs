552050cb0fd8daa38aaaa587a062e1e3e4238666
BrennanConroy <brecon@microsoft.com>
2016-06-23 16:42:12 -0700

    Update DataProtection

--- aspnet/security/data-protection/consumer-apis/dangerous-unprotect/samples/dangerous-unprotect.cs
+++ aspnet/security/data-protection/consumer-apis/dangerous-unprotect/samples/dangerous-unprotect.cs
@@ -1,40 +1,37 @@
 using System;
 using System.IO;
 using System.Text;
-using Microsoft.AspNet.DataProtection;
-using Microsoft.AspNet.DataProtection.KeyManagement;
-using Microsoft.Framework.DependencyInjection;
- 
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.DependencyInjection;
+
 public class Program
 {
     public static void Main(string[] args)
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddDataProtection();
-        serviceCollection.ConfigureDataProtection(configure =>
-        {
+        serviceCollection.AddDataProtection()
             // point at a specific folder and use DPAPI to encrypt keys
-            configure.PersistKeysToFileSystem(new DirectoryInfo(@"c:\temp-keys"));
-            configure.ProtectKeysWithDpapi();
-        });
+            .PersistKeysToFileSystem(new DirectoryInfo(@"c:\temp-keys"))
+            .ProtectKeysWithDpapi();
         var services = serviceCollection.BuildServiceProvider();
- 
+
         // get a protector and perform a protect operation
         var protector = services.GetDataProtector("Sample.DangerousUnprotect");
         Console.Write("Input: ");
         byte[] input = Encoding.UTF8.GetBytes(Console.ReadLine());
         var protectedData = protector.Protect(input);
         Console.WriteLine($"Protected payload: {Convert.ToBase64String(protectedData)}");
- 
+
         // demonstrate that the payload round-trips properly
         var roundTripped = protector.Unprotect(protectedData);
         Console.WriteLine($"Round-tripped payload: {Encoding.UTF8.GetString(roundTripped)}");
- 
+
         // get a reference to the key manager and revoke all keys in the key ring
         var keyManager = services.GetService<IKeyManager>();
         Console.WriteLine("Revoking all keys in the key ring...");
         keyManager.RevokeAllKeys(DateTimeOffset.Now, "Sample revocation.");
- 
+
         // try calling Protect - this should throw
         Console.WriteLine("Calling Unprotect...");
         try
@@ -46,7 +43,7 @@ public class Program
         {
             Console.WriteLine($"{ex.GetType().Name}: {ex.Message}");
         }
- 
+
         // try calling DangerousUnprotect
         Console.WriteLine("Calling DangerousUnprotect...");
         try
@@ -56,7 +53,7 @@ public class Program
             {
                 throw new Exception("Can't call DangerousUnprotect.");
             }
- 
+
             bool requiresMigration, wasRevoked;
             var unprotectedPayload = persistedProtector.DangerousUnprotect(
                 protectedData: protectedData,
@@ -72,7 +69,7 @@ public class Program
         }
     }
 }
- 
+
 /*
  * SAMPLE OUTPUT
  *
--- aspnet/security/data-protection/implementation/key-management/samples/key-management.cs
+++ aspnet/security/data-protection/implementation/key-management/samples/key-management.cs
@@ -1,33 +1,30 @@
 using System;
 using System.IO;
 using System.Threading;
-using Microsoft.AspNet.DataProtection;
-using Microsoft.AspNet.DataProtection.KeyManagement;
-using Microsoft.Framework.DependencyInjection;
- 
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.DependencyInjection;
+
 public class Program
 {
     public static void Main(string[] args)
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddDataProtection();
-        serviceCollection.ConfigureDataProtection(configure =>
-        {
+        serviceCollection.AddDataProtection()
             // point at a specific folder and use DPAPI to encrypt keys
-            configure.PersistKeysToFileSystem(new DirectoryInfo(@"c:\temp-keys"));
-            configure.ProtectKeysWithDpapi();
-        });
+            .PersistKeysToFileSystem(new DirectoryInfo(@"c:\temp-keys"))
+            .ProtectKeysWithDpapi();
         var services = serviceCollection.BuildServiceProvider();
- 
+
         // perform a protect operation to force the system to put at least
         // one key in the key ring
         services.GetDataProtector("Sample.KeyManager.v1").Protect("payload");
         Console.WriteLine("Performed a protect operation.");
         Thread.Sleep(2000);
- 
+
         // get a reference to the key manager
         var keyManager = services.GetService<IKeyManager>();
- 
+
         // list all keys in the key ring
         var allKeys = keyManager.GetAllKeys();
         Console.WriteLine($"The key ring contains {allKeys.Count} key(s).");
@@ -35,17 +32,17 @@ public class Program
         {
             Console.WriteLine($"Key {key.KeyId:B}: Created = {key.CreationDate:u}, IsRevoked = {key.IsRevoked}");
         }
- 
+
         // revoke all keys in the key ring
         keyManager.RevokeAllKeys(DateTimeOffset.Now, reason: "Revocation reason here.");
         Console.WriteLine("Revoked all existing keys.");
- 
+
         // add a new key to the key ring with immediate activation and a 1-month expiration
         keyManager.CreateNewKey(
             activationDate: DateTimeOffset.Now,
             expirationDate: DateTimeOffset.Now.AddMonths(1));
         Console.WriteLine("Added a new key.");
- 
+
         // list all keys in the key ring
         allKeys = keyManager.GetAllKeys();
         Console.WriteLine($"The key ring contains {allKeys.Count} key(s).");
@@ -55,7 +52,7 @@ public class Program
         }
     }
 }
- 
+
 /*
  * SAMPLE OUTPUT
  *
