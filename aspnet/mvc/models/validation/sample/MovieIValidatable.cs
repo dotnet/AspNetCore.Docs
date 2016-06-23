83b7f130e757548de3090fb48558feea54c64514
Doug Bunting <dougbu@microsoft.com>
2016-06-23 11:40:24 -0700

    Get validation sample working - add files needed to complete the sample - implement rc2 (and later) `IClientModelValidator` interface - remove validation TODOs; implement them for real - remove unused `Movie` properties - remove and sort `using`s
    Adjust `validation.rst` to match new sample
    - correct a couple of small typos
    - use sample files instead of code blocks where possible

--- aspnet/mvc/models/validation/sample/MovieIValidatable.cs
+++ aspnet/mvc/models/validation/sample/MovieIValidatable.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MVCMovie.Models
 {
     public class MovieIValidatable : IValidatableObject
     {
         private const int _classicYear = 1960;
+
         public int Id { get; set; }
 
         [Required]
@@ -22,28 +21,22 @@ namespace MVCMovie.Models
         [StringLength(1000)]
         public string Description { get; set; }
 
-        [Required()]
+        [Required]
         [Range(0, 999.99)]
-        public float Price { get; set; }
+        public decimal Price { get; set; }
 
         [Required]
         public Genre Genre { get; set; }
 
-        [Required]
-        public Audience Audience { get; set; }
-        public List<Review> Reviews { get; set; }
+        public bool Preorder { get; set; }
 
-        public IEnumerable<ValidationResult> 
-            Validate(ValidationContext validationContext)
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (Genre == Genre.Classic)
+            if (Genre == Genre.Classic && ReleaseDate.Year > _classicYear)
             {
-                if (ReleaseDate.Year > _classicYear)
-                {
-                    yield return new ValidationResult(
-                        "Classic movies must have a release year earlier than " + _classicYear, 
-                        new[] { "ReleaseDate" });
-                }
+                yield return new ValidationResult(
+                    "Classic movies must have a release year earlier than " + _classicYear,
+                    new[] { "ReleaseDate" });
             }
         }
     }
---
+++ aspnet/mvc/models/validation/sample/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCMovie.Controllers
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly HashSet<string> _emailAddresses = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        public bool VerifyEmail(string email)
+        {
+            // In real world, adding a new email address would be a separate step. This would be a contains check.
+            return _emailAddresses.Add(email);
+        }
+    }
+}
