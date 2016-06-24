88438b3c090920f5654e8c50d6ac081f5d354c0e
Scifoni Ivano <iscifoni@libero.it>
2016-06-24 18:23:05 +0200

    AccConfirm sample converted to Core 1.0.0-RC2

---
+++ aspnet/security/authentication/accconfirm/sample/WebApplication3/src/WebApplication3/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
