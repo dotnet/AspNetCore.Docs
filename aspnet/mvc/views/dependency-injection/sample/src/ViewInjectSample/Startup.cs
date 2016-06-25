9b709582ee6b18ce8775738ec12fec7d7114c815
Pranav K <prkrishn@hotmail.com>
2016-06-24 17:23:19 -0700

    Update sample to work with RTM (#1516)
    Modify sample for dependency-injection

--- aspnet/mvc/views/dependency-injection/sample/src/ViewInjectSample/Startup.cs
+++ aspnet/mvc/views/dependency-injection/sample/src/ViewInjectSample/Startup.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using ViewInjectSample.Helpers;
 using ViewInjectSample.Infrastructure;
@@ -26,16 +26,20 @@ namespace ViewInjectSample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=ToDo}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
 
         // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+               .UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .UseIISIntegration()
+               .UseStartup<Startup>()
+               .Build();
+
+            host.Run();
+        }
     }
 }
