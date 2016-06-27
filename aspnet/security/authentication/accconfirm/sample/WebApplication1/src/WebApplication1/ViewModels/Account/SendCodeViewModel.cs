3306fb2bf1bc3271ae2e9f9c841593551a4a56ce
Daniel Roth <danroth27@hotmail.com>
2016-06-27 08:17:02 -0700

    Revert "Update account confirmation article to RC2"

---
+++ aspnet/security/authentication/accconfirm/sample/WebApplication1/src/WebApplication1/ViewModels/Account/SendCodeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.Rendering;
+
+namespace WebApplication1.ViewModels.Account
+{
+    public class SendCodeViewModel
+    {
+        public string SelectedProvider { get; set; }
+
+        public ICollection<SelectListItem> Providers { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
