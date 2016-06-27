d36a05eeb0049ead1e484141e55031bda5416cad
Steve Smith <steve@kentsmiths.com>
2016-06-27 19:46:53 -0400

    Formatting Response Data (#1327)
    * Formatting Response Data
    
    * Formatting Response Data
    
    * Adding sample project
    
    * format-specific action results done with code and screenshots
    
    * small progress
    
    * Cleaned up document after rebase on RC2 master.
    
    * Updated sample to RC2
    
    * Adding search method which uses Ok and NotFound
    
    * Working on Conneg section
    
    * Working on response format url mappings.
    
    * Ready for review
    
    * Responding to feedback
    
    * Responding to feedback
    
    * Fixing typo
    
    * Fixing link
    
    * Fixing (another) link

---
+++ aspnet/mvc/models/formatting/sample/src/ResponseFormattingSample/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ResponseFormattingSample.Model;
+
+namespace ResponseFormattingSample.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        List<Author> List();
+        Author GetByAlias(string twitterAlias);
+        List<Author> GetByNameSubstring(string nameSubstring);
+    }
+}
