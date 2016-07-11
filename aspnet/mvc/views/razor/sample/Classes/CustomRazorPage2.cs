dd72bccc8653dc51659efc7a6ef431b0e8cc0f78
Rick Anderson <riande@microsoft.com>
2016-07-11 11:34:19 -0700

    New Razor syntax article

---
+++ aspnet/mvc/views/razor/sample/Classes/CustomRazorPage2.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+// Doesn't work without <TModel>
+public abstract class CustomRazorPage2 : RazorPage
+{
+    public string CustomText { get; } = "CustomRazorPage2.";
+}
