6f992b16c99379044d6d23004e1918f6c424fc2e
Stephen Halter <halter73@gmail.com>
2016-07-12 16:37:35 -0700

    Update servers docs from RC1 to RTM (#1633)
    * Update ServersDemo for 1.0.0
    
    * Update servers docs from RC1 to RTM

--- aspnet/fundamentals/servers/sample/ServersDemo/src/ServersDemo/Program.cs
+++ aspnet/fundamentals/servers/sample/ServersDemo/src/ServersDemo/Program.cs
@@ -1,43 +1,96 @@
 ﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Hosting;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.AspNet.Builder;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNet.Server.Kestrel;
+using Microsoft.Net.Http.Server;
 
 namespace ServersDemo
 {
     /// <summary>
-    /// This demonstrates how the application can be launched in a console application. 
-    /// Executing the "dnx run" command in the application folder will run this app.
+    /// Executing the "dotnet run --server [Kestrel|WebListener]" command in the application folder will run this app.
+    /// You can also run "dotnet publish" and specify the publish directory as the physical path of a site in IIS Manager.
     /// </summary>
     public class Program
     {
-        private readonly IServiceProvider _serviceProvider;
+        public static string Server;
 
-        public Program(IServiceProvider serviceProvider)
+        public static int Main(string[] args)
         {
-            _serviceProvider = serviceProvider;
-        }
+            // Add command line configuration source to read command line parameters.
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
 
-        public Task<int> Main(string[] args)
-        {
-            //Add command line configuration source to read command line parameters.
-            var builder = new ConfigurationBuilder();
-            builder.AddCommandLine(args);
-            var config = builder.Build();
+            Server = config["server"] ?? "Kestrel";
 
-            using (new WebHostBuilder(config)
-                .UseServer("Microsoft.AspNet.Server.Kestrel")
-                .Build()
-                .Start())
+            var builder = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseStartup<Startup>();
+
+            // The default listening address is http://localhost:5000 if none is specified.
+            // Replace "localhost" with "*" to listen to external requests.
+            // You can use the --urls flag to change the listening address. Ex:
+            // > dotnet run --urls http://*:8080;http://*:8081
+
+            // Uncomment the following to configure URLs programmatically.
+            // Since this is after UseConfiguraiton(config), this will clobber command line configuration.
+            //builder.UseUrls("http://*:8080", "http://*:8081");
+
+            // If this app isn't hosted by IIS, UseIISIntegration() no-ops.
+            // It isn't possible to both listen to requests directly and from IIS using the same WebHost,
+            // since this will clobber your UseUrls() configuration when hosted by IIS.
+            // If UseIISIntegration() is called before UseUrls(), IIS hosting will fail.
+            builder.UseIISIntegration();
+
+            if (string.Equals(Server, "Kestrel", StringComparison.OrdinalIgnoreCase))
             {
-                Console.WriteLine("Started the server..");
-                Console.WriteLine("Press any key to stop the server");
-                Console.ReadLine();
+                Console.WriteLine("Running demo with Kestrel.");
+
+                builder.UseKestrel(options =>
+                {
+                    if (config["threadCount"] != null)
+                    {
+                        options.ThreadCount = int.Parse(config["threadCount"]);
+                    }
+                });
             }
-            return Task.FromResult(0);
+            else if (string.Equals(Server, "WebListener", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Running demo with WebListener.");
+
+                builder.UseWebListener(options =>
+                {
+                    // AllowAnonymous is the default WebListner configuration
+                    options.Listener.AuthenticationManager.AuthenticationSchemes =
+                        AuthenticationSchemes.AllowAnonymous;
+                });
+            }
+            else
+            {
+                Console.WriteLine($"Error: Unknown server value: '{Server}'. The valid server options are 'Kestrel' and 'WebListener'.");
+                Console.WriteLine("IIS cannot be specified at runtime since it does not support self-hosting.");
+                return 1;
+            }
+
+            // ASPNETCORE_PORT is the port that IIS proxies requests to.
+            if (Environment.GetEnvironmentVariable($"ASPNETCORE_PORT") != null)
+            {
+                if (string.Equals(Server, "WebListener", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Error: WebListener cannot be used with the ASP.NET Core Module for IIS.");
+                    return 1;
+                }
+
+                Server = "IIS/Kestrel";
+
+                Console.WriteLine("Hosted by IIS.");
+            }
+
+            var host = builder.Build();
+            host.Run();
+
+            return 0;
         }
     }
 }
