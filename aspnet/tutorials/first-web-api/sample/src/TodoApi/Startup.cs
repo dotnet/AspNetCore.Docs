b558185a387d7dc9fb51a7b51eb8d98759853eb1
Rick Anderson <riande@microsoft.com>
2016-08-02 21:40:24 -0700

    first draft

--- aspnet/tutorials/first-web-api/sample/src/TodoApi/Startup.cs
+++ aspnet/tutorials/first-web-api/sample/src/TodoApi/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,34 +9,36 @@ namespace TodoApi
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
+        #region snippet_AddSingleton
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
 
-            services.AddLogging();
-
-            // Add our repository type
             services.AddSingleton<ITodoRepository, TodoRepository>();
         }
+        #endregion
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseMvcWithDefaultRoute();
-        }
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup<Startup>()
-                .UseKestrel()
-                .UseIISIntegration()
-                .Build();
-
-            host.Run();
+            app.UseMvc();
         }
     }
 }
