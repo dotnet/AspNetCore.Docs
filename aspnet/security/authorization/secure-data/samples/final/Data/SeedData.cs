821092e27596c5b6c6acaccc499db7ab10a244b7
Rick-Anderson <riande@microsoft.com>
2016-08-12 18:00:52 -0700

    fix PR

---
+++ aspnet/security/authorization/secure-data/samples/final/Data/SeedData.cs
@@ -0,0 +1,134 @@
+﻿#define SeedOnly
+#if SeedOnly
+
+using ContactManager.Authorization;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManager.Data
+{
+    public static class SeedData
+    {
+        #region snippet_Initialize
+        public static async Task Initialize(IServiceProvider serviceProvider, string testUserPw)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                var uid = await CreateTestUser(serviceProvider, testUserPw);
+                await CreateCanDeleteRole(serviceProvider, uid, Constants.canDelete);
+                SeedDB(context, uid);
+            }
+        }
+        #endregion
+        private static async Task<string> CreateTestUser(IServiceProvider serviceProvider, string testUserPw)
+        {
+            const string SeedUserName = "test@example.com";
+
+            var userManager = serviceProvider.GetService<UserManager<ApplicationUser>>();
+
+            var user = await userManager.FindByNameAsync(SeedUserName);
+            if (user == null)
+            {
+                user = new ApplicationUser { UserName = SeedUserName };
+                await userManager.CreateAsync(user, testUserPw);
+            }
+
+            return user.Id;
+        }
+
+        #region snippet_CreateCanDeleteRole
+        private static async Task<IdentityResult> CreateCanDeleteRole(IServiceProvider serviceProvider,
+                                                                       string uid, string canDeleteRole)
+        {
+            IdentityResult IR = null;
+            var roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+
+            if (!await roleManager.RoleExistsAsync(canDeleteRole))
+            {
+                IR = await roleManager.CreateAsync(new IdentityRole(canDeleteRole));
+            }
+
+            var userManager = serviceProvider.GetService<UserManager<ApplicationUser>>();
+
+            var user = await userManager.FindByIdAsync(uid);
+
+            IR = await userManager.AddToRoleAsync(user, canDeleteRole);
+
+            return IR;
+        }
+        #endregion
+
+        public static void SeedDB(ApplicationDbContext context, string uid)
+        {
+            if (context.Contact.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Contact.AddRange(
+            #region snippet_Contact
+                new Contact
+                {
+                    Name = "Debra Garcia",
+                    Address = "1234 Main St",
+                    City = "Redmond",
+                    State = "WA",
+                    Zip = "10999",
+                    Email = "debra@example.com",
+                    OwnerID = uid
+                },
+            #endregion
+                new Contact
+                {
+                    Name = "Thorsten Weinrich",
+                    Address = "5678 1st Ave W",
+                    City = "Redmond",
+                    State = "WA",
+                    Zip = "10999",
+                    Email = "thorsten@example.com",
+                    OwnerID = uid
+                },
+             new Contact
+             {
+                 Name = "Yuhong Li",
+                 Address = "9012 State st",
+                 City = "Redmond",
+                 State = "WA",
+                 Zip = "10999",
+                 Email = "yuhong@example.com",
+                 OwnerID = uid
+             },
+             new Contact
+             {
+                 Name = "Jon Orton",
+                 Address = "3456 Maple St",
+                 City = "Redmond",
+                 State = "WA",
+                 Zip = "10999",
+                 Email = "jon@example.com",
+                 OwnerID = uid
+             },
+             new Contact
+             {
+                 Name = "Diliana Alexieva-Bosseva",
+                 Address = "7890 2nd Ave E",
+                 City = "Redmond",
+                 State = "WA",
+                 Zip = "10999",
+                 Email = "diliana@example.com",
+                 OwnerID = uid
+             }
+             );
+            context.SaveChanges();
+        }
+    }
+}
+
+#endif
