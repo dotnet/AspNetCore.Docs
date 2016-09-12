40e82022487de13760d6ac90990f65f497660af5
Scott Addie <tobias.addie@gmail.com>
2016-09-12 08:25:11 -0500

    Update Migrating Configuration doc to RTM (#1859)

--- aspnet/migration/configuration/samples/WebApp1/src/WebApp1/Controllers/HomeController.cs
+++ aspnet/migration/configuration/samples/WebApp1/src/WebApp1/Controllers/HomeController.cs
@@ -1,29 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public IActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public IActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewData["Message"] = "Your contact page.";
 
+            return View();
+        }
+
+        public IActionResult Error()
+        {
             return View();
         }
     }
