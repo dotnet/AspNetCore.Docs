c871b7b1814dc1d5cd0fb380a2bd72f5ba829153
Tom Dykstra <tdykstra@microsoft.com>
2016-09-14 14:12:20 -0700

    initial draft

---
+++ aspnet/data/ef-mvc/intro/samples/cu-final/PaginatedList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+public class PaginatedList<T> : List<T>
+{
+    public int PageIndex { get; private set; }
+    public int TotalPages { get; private set; }
+
+    public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+    {
+        PageIndex = pageIndex;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+        this.AddRange(items);
+    }
+
+    public bool HasPreviousPage
+    {
+        get
+        {
+            return (PageIndex > 1);
+        }
+    }
+
+    public bool HasNextPage
+    {
+        get
+        {
+            return (PageIndex < TotalPages);
+        }
+    }
+
+    public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+    {
+        var count = await source.CountAsync();
+        var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+        return new PaginatedList<T>(items, count, pageIndex, pageSize);
+    }
+}
