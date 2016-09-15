371572cdc13eb3bb8885f4ffe58f0bf5684788f7
Rick Anderson <riande@microsoft.com>
2016-09-15 14:46:02 -0700

    routing init

---
+++ aspnet/mvc/controllers/routing/sample/AreasRouting/Areas/Blog/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace1
+{
+    [Area("Blog")]
+    public class UsersController : Controller
+    {
+        public IActionResult AddUser()
+        {
+            return View();
+        }        
+    }
+}
