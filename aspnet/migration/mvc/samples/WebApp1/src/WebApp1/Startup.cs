896f588f0bdf0370e3e92379af0281f0a54ff3a3
Scott Addie <tobias.addie@gmail.com>
2016-09-22 13:46:15 -0500

    Update MVC migration doc to RTM (#1901)

--- aspnet/migration/mvc/samples/WebApp1/src/WebApp1/Startup.cs
+++ aspnet/migration/mvc/samples/WebApp1/src/WebApp1/Startup.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace WebApp1
 {
@@ -19,8 +15,15 @@ namespace WebApp1
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
