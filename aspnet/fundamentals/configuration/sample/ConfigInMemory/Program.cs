95b8beba1571d1cb205e3690883c270d0f168332
riande@microsoft.com <riande@microsoft.com>
2016-09-27 14:40:30 -0700

    config wip

---
+++ aspnet/fundamentals/configuration/sample/ConfigInMemory/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+// Add NuGet  <package id="Microsoft.Extensions.Configuration"
+public class Program
+{
+    static IReadOnlyDictionary<string, string> ConfigStrings { get; } =
+      new Dictionary<string, string>()
+      {
+          ["Profile:UserName"] = Environment.UserName,
+          [$"AppConfig:MyconfigString"] = "Sample config string.",
+          [$"AppConfig:MainWindow:Height"] = "400",
+      };
+    static public IConfiguration Configuration { get; set; }
+    public static void Main(string[] args = null)
+    {
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddInMemoryCollection(ConfigStrings);
+        Configuration = configurationBuilder.Build();
+
+        Console.WriteLine($"Hello  {Configuration["Profile:UserName"]}");
+        Console.WriteLine(
+          $"My config string: {Configuration["AppConfig:MyconfigString"]}");
+        Console.WriteLine(
+          $"Window Height: {Configuration["AppConfig:MainWindow:Height"]}");
+    }
+}
