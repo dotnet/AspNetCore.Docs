6413155675e794d7f6008ea55507a8952c764e1f
Adrien Torris <adrientorris@gmail.com>
2016-10-03 20:11:16 +0200

    Add identity configuration example (#1939)
    * Add identity configuration example
    
    Add an example about how to configure some basic stuff about ASP.NET Core Identity (password policy, cookie, lockout, ...)
    
    * Add identity configuration example
    
    Add an example of identity configuration in startup class

--- aspnet/security/authentication/identity/sample/src/ASPNET-IdentityDemo/Startup.cs
+++ aspnet/security/authentication/identity/sample/src/ASPNET-IdentityDemo/Startup.cs
@@ -53,6 +53,26 @@ namespace webapptemplate
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
+             
+            // Configure Identity
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = false;
+                
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                
+                // Cookie settings
+                options.Cookies.ApplicationCookie.ExpireTimeSpan = TimeSpan.FromDays(150);
+                options.Cookies.ApplicationCookie.LoginPath = "/Account/LogIn";
+                options.Cookies.ApplicationCookie.LogoutPath = "/Account/LogOff";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
