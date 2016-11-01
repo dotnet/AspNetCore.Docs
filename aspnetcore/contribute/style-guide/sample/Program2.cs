718357dffcb5101dd6dc4ed4d621e58bed7b02e2
Rick Anderson <riande@microsoft.com>
2016-10-31 15:51:59 -1000

    init style draft

---
+++ aspnetcore/contribute/style-guide/sample/Program2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Program
+{
+    #region snippet1
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world!");
+    }
+    #endregion
+}
