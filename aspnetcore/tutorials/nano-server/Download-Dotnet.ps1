2a6dbe4f1bc9cc7fbd95379a40d37184174c5516
rs38 <rs38@users.noreply.github.com>
2016-11-03 11:26:15 +0100

    Update Download-Dotnet.ps1

--- aspnetcore/tutorials/nano-server/Download-Dotnet.ps1
+++ aspnetcore/tutorials/nano-server/Download-Dotnet.ps1
@@ -8,35 +8,29 @@ if (($EditionId -eq "ServerStandardNano") -or
     ($EditionId -eq "NanoServer") -or
     ($EditionId -eq "ServerTuva")) {
 
-    $TempPath = [System.IO.Path]::GetTempFileName()
-    if (($SourcePath -as [System.URI]).AbsoluteURI -ne $null)
+        $SourcePath = "https://go.microsoft.com/fwlink/?LinkID=809115"
+          $DestinationPath = "C:\temp\RS"
+    $TempPath = [System.IO.Path]::GetTempFileName()+".zip"
+
+    try
     {
-        $handler = New-Object System.Net.Http.HttpClientHandler
-        $client = New-Object System.Net.Http.HttpClient($handler)
-        $client.Timeout = New-Object System.TimeSpan(0, 30, 0)
-        $cancelTokenSource = [System.Threading.CancellationTokenSource]::new()
-        $responseMsg = $client.GetAsync([System.Uri]::new($SourcePath), $cancelTokenSource.Token)
-        $responseMsg.Wait()
-        if (!$responseMsg.IsCanceled)
-        {
-            $response = $responseMsg.Result
-            if ($response.IsSuccessStatusCode)
-            {
-                $downloadedFileStream = [System.IO.FileStream]::new($TempPath, [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write)
-                $copyStreamOp = $response.Content.CopyToAsync($downloadedFileStream)
-                $copyStreamOp.Wait()
-                $downloadedFileStream.Close()
-                if ($copyStreamOp.Exception -ne $null)
-                {
-                    throw $copyStreamOp.Exception
-                }
-            }
+      Invoke-WebRequest -URI $SourcePath -OutFile  $TempPath
+      if ( (Get-Item $TempPath).length  -gt 10kb) #probably more than a warning page
+       {
+           Expand-Archive -Path $TempPath -DestinationPath $DestinationPath -Force
+       }
+           else
+       {
+            Write-Warning "Archive not okay"
         }
+
     }
-    else
+    catch [System.Net.WebException],[System.Exception]
     {
-        throw "Cannot copy from $SourcePath"
+        Write-Warning "something was wrong with the download"
+        Write-Error $error[0].Exception
+
     }
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($TempPath, $DestinationPath)
+
     Remove-Item $TempPath
 }
