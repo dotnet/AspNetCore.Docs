32ebdcf6f5bb6bfcd3f65d1f96e42c61edb0e858
rs38 <rs38@users.noreply.github.com>
2016-11-03 11:51:58 +0100

    Update Download-Dotnet.ps1

--- aspnetcore/tutorials/nano-server/Download-Dotnet.ps1
+++ aspnetcore/tutorials/nano-server/Download-Dotnet.ps1
@@ -3,33 +3,28 @@ $DestinationPath = "C:\dotnet"
 
 $EditionId = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name 'EditionID').EditionId
 
-if (($EditionId -eq "ServerStandardNano") -or
-    ($EditionId -eq "ServerDataCenterNano") -or
-    ($EditionId -eq "NanoServer")) {
 
         $SourcePath = "https://go.microsoft.com/fwlink/?LinkID=809115"
-          $DestinationPath = "C:\temp\RS"
-    $TempPath = [System.IO.Path]::GetTempFileName()+".zip"
+        $TempPath = [System.IO.Path]::GetTempFileName()+".zip"
 
-    try
-    {
-      Invoke-WebRequest -URI $SourcePath -OutFile  $TempPath
-      if ( (Get-Item $TempPath).length  -gt 10kb) #probably more than a warning page
-       {
-           Expand-Archive -Path $TempPath -DestinationPath $DestinationPath -Force
-       }
-           else
-       {
-            Write-Warning "Archive not okay"
+        try
+        {
+          Invoke-WebRequest -URI $SourcePath -OutFile  $TempPath
+          if ( (Get-Item $TempPath).length  -gt 10kb) #probably more than a warning page
+          {
+               Expand-Archive -Path $TempPath -DestinationPath $DestinationPath -Force
+          }
+              else
+          {
+              Write-Warning "Archive not okay"
+          }
+      }
+        catch [System.Net.WebException],[System.Exception]
+        {
+            Write-Warning "something was wrong with the download"
+            Write-Error $error[0].Exception
+
         }
 
-    }
-    catch [System.Net.WebException],[System.Exception]
-    {
-        Write-Warning "something was wrong with the download"
-        Write-Error $error[0].Exception
-
-    }
-
     Remove-Item $TempPath
-}
+
