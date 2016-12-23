69d1bb92e02a056b63abb969210f42f9bda047cd
Steve Smith <steve@kentsmiths.com>
2016-12-23 12:42:32 -0500

    Working with the Application Model (#2280)
    Working with the Application Model

---
+++ aspnetcore/mvc/controllers/application-model/sample/src/AppModelSample/Controllers/DescriptionAttributesController.cs
@@ -0,0 +1,24 @@
+﻿using AppModelSample.Conventions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppModelSample.Controllers
+{
+    #region DescriptionAttributesController
+    #region ControllerDescription
+    [ControllerDescription("Controller Description")]
+    public class DescriptionAttributesController : Controller
+    {
+        public string Index()
+        {
+            return "Description: " + ControllerContext.ActionDescriptor.Properties["description"];
+        }
+        #endregion
+
+        [ActionDescription("Action Description")]
+        public string UseActionDescriptionAttribute()
+        {
+            return "Description: " + ControllerContext.ActionDescriptor.Properties["description"];
+        }
+    }
+    #endregion
+}
