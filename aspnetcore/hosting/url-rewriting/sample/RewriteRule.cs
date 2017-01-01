4141bbadf67f02c17631cd7ab4958f2db3aa642d
Luke Latham <GuardRex@users.noreply.github.com>
2017-01-01 05:58:58 -0600

    Group System usings

--- aspnetcore/hosting/url-rewriting/sample/RewriteRule.cs
+++ aspnetcore/hosting/url-rewriting/sample/RewriteRule.cs
@@ -2,10 +2,10 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Net.Http.Headers;
-using System.Text.RegularExpressions;
 
 namespace RewriteRules
 {
