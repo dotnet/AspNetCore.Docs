db3c745f3a3e3ac4a1bb2426038ecd7024d6bf47
Luke Latham <GuardRex@users.noreply.github.com>
2017-01-08 16:09:46 -0600

    Add files via upload

---
+++ aspnetcore/performance/response-compression/sample/DefaultsSample/README.md
@@ -0,0 +1,20 @@
+# Response Compression Sample (DefaultsSample)
+
+This sample illustrates the use of ASP.NET Core Response Compression Middleware to compress application responses with default settings. The default settings for compression use the `GzipCompressionProvider` with fastest compression and the MIME types:
+* text/plain
+* text/css
+* application/javascript
+* text/html
+* application/xml
+* text/xml
+* application/json
+* text/json
+
+## Examples in this sample
+`GzipCompressionProvider`: `text/plain`: **/** - Lorem Ipsum text file response at 2,044 bytes that will compress to 927 bytes
+
+When the request includes the `Accept-Encoding` header, the sample adds a `Vary: Accept-Encoding` header to the response. The `Vary` header instructs caches to maintain multiple copies of the response based on alternative values of `Accept-Encoding`, so both a compressed (gzip) and uncompressed version will be stored in caches for systems that can either accept the compressed or the uncompressed response.
+
+## Using the sample
+1. Make a request using [Fiddler](http://www.telerik.com/fiddler), [Firebug](http://getfirebug.com/), or [Postman](https://www.getpostman.com/) to the application without an `Accept-Encoding` header and note the response payload, response size, and response headers.
+2. Add an `Accept-Encoding: gzip` header and note the compressed response size and response headers. You will see the response size drop and the `Content-Encoding: gzip` response header. When you look at the response body, you will see that the text has been compressed and is unreadable.
---
+++ aspnetcore/performance/response-compression/sample/DefaultsSample/Startup.cs
@@ -0,0 +1,56 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+
+namespace ResponseCompressionSample
+{
+    public class Startup
+    {
+        #region snippet1
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddResponseCompression();
+        }
+        #endregion
+
+        #region snippet2
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseResponseCompression();
+
+            app.Run(async context =>
+            {
+                // If the Accept-Encoding header is present, always add the Vary header
+                var accept = context.Request.Headers[HeaderNames.AcceptEncoding];
+                if (!StringValues.IsNullOrEmpty(accept))
+                {
+                    context.Response.Headers[HeaderNames.Vary] = HeaderNames.AcceptEncoding;
+                }
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync(LoremIpsum.Text);
+            });
+        }
+        #endregion
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .ConfigureLogging(factory =>
+                {
+                    factory.AddConsole(LogLevel.Debug);
+                })
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
