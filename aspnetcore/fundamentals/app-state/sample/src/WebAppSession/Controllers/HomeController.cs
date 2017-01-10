bd40a6a9dc2b84360bd1e6714dfddce7bc60acde
Rick Anderson <riande@microsoft.com>
2017-01-09 15:44:52 -1000

    total rewrite of app state

---
+++ aspnetcore/fundamentals/app-state/sample/src/WebAppSession/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace WebAppSession.Controllers
+{
+    #region snippet1
+    public class HomeController : Controller
+    {
+        const string SessionKeyName = "_SessionKeyName";
+        const string SessionKeyYearsMember = "_SessionKeyYearsMember";
+        const string SessionKeyDate = "_SessionKeyDate";
+
+        public IActionResult Index()
+        {
+            // Requires using Microsoft.AspNetCore.Http;
+            HttpContext.Session.SetString(SessionKeyName, "Rick");
+            HttpContext.Session.SetInt32(SessionKeyYearsMember, 3);
+            return RedirectToAction("SessionNameYears");
+        }
+        public IActionResult SessionNameYears()
+        {
+            var name = HttpContext.Session.GetString(SessionKeyName);
+            var yearsMember = HttpContext.Session.GetInt32(SessionKeyYearsMember);
+
+            return Content($"Name: \"{name}\",  Membership years: \"{yearsMember}\"");
+        }
+        #endregion
+
+        #region snippet2
+        public IActionResult SetDate()
+        {
+            HttpContext.Session.Set<DateTime>(SessionKeyDate, DateTime.Now);
+            return RedirectToAction("GetDate");
+        }
+
+        public IActionResult GetDate()
+        {
+            var date = HttpContext.Session.Get<DateTime>(SessionKeyDate);
+            var sessionTime = date.TimeOfDay.ToString();
+            var currentTime = DateTime.Now.TimeOfDay.ToString();
+
+            return Content($"Current time: {currentTime} - "
+                         + $"session time: {sessionTime}");
+        }
+        #endregion
+    }
+}
