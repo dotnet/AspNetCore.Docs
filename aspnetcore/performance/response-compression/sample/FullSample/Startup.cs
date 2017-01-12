4c88d25c4792ce0ba7608bb66a3377ce0f09adb3
Luke Latham <GuardRex@users.noreply.github.com>
2017-01-11 20:20:13 -0600

    Update Startup.cs

--- aspnetcore/performance/response-compression/sample/FullSample/Startup.cs
+++ aspnetcore/performance/response-compression/sample/FullSample/Startup.cs
@@ -91,6 +91,8 @@ namespace ResponseCompressionSample
         private void ManageVaryHeader(ref HttpContext context)
         {
             // If the Accept-Encoding header is present, always add the Vary header
+            // This will be added as a feature in the next release of the middleware.
+            // https://github.com/aspnet/BasicMiddleware/issues/187
             var accept = context.Request.Headers[HeaderNames.AcceptEncoding];
             if (!StringValues.IsNullOrEmpty(accept))
             {
