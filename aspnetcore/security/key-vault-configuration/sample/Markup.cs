c7a5875909ec9836c39d4d80c521c953be1f04a7
Luke Latham <GuardRex@users.noreply.github.com>
2017-01-15 14:27:20 -0600

    Add files via upload

--- aspnetcore/security/key-vault-configuration/sample/Markup.cs
+++ aspnetcore/security/key-vault-configuration/sample/Markup.cs
@@ -10,36 +10,40 @@ namespace KeyVaultConfigProviderSample
             <head>
                 <meta charset=""utf-8"" />
                 <title>Key Vault Configuration Provider Sample</title>
-                <style>table{{border-collapse:collapse}}th,td{{padding:8px}}tr:nth-child(even){{background-color:#f2f2f2}}th{{background-color:#4CAF50;color:white}}</style>
+                <style>body{{font-family:sans-serif}}div{{overflow-x:auto}}table{{border-collapse:collapse}}th,td{{padding:8px}}tr:nth-child(even){{background-color:#f2f2f2}}th{{background-color:#4CAF50;color:white}}</style>
             </head>
-            <body style=""font-family:sans-serif"">
+            <body>
                 <h1>Key Vault Configuration Provider Sample</h1>
-                <div style=""overflow-x:auto"">
+                <div>
                     <table>
-                        <tr>
-                            <th>Secret</th>
-                            <th>Key Vault Name</th>
-                            <th>Obtained from Key Vault</th>
-                            <th>Value</th> 
-                        </tr>
-                        <tr>
-                            <td>MySecret</td>
-                            <td><b>MySecret</b></td>
-                            <td><code>Configuration[""MySecret""]</code></td>
-                            <td>{0}</td> 
-                        </tr>
-                        <tr>
-                            <td>Section:MySecret</td>
-                            <td><b>Section--MySecret</b></td>
-                            <td><code>Configuration[""Section:MySecret""]</code></td>
-                            <td>{1}</td> 
-                        </tr>
-                        <tr>
-                            <td>Section:MySecret</td>
-                            <td><b>Section--MySecret</b></td>
-                            <td><code>Configuration.GetSection(""Section"")[""MySecret""]</code></td>
-                            <td>{2}</td> 
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th>Secret</th>
+                                <th>Key Vault Name</th>
+                                <th>Obtained from Key Vault</th>
+                                <th>Value</th> 
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>MySecret</td>
+                                <td><b>MySecret</b></td>
+                                <td><code>Configuration[""MySecret""]</code></td>
+                                <td>{0}</td> 
+                            </tr>
+                            <tr>
+                                <td>Section:MySecret</td>
+                                <td><b>Section--MySecret</b></td>
+                                <td><code>Configuration[""Section:MySecret""]</code></td>
+                                <td>{1}</td> 
+                            </tr>
+                            <tr>
+                                <td>Section:MySecret</td>
+                                <td><b>Section--MySecret</b></td>
+                                <td><code>Configuration.GetSection(""Section"")[""MySecret""]</code></td>
+                                <td>{2}</td> 
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </body>
