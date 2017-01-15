c1558046268536885dfa35805727f6fe5a7d5295
Luke Latham <GuardRex@users.noreply.github.com>
2017-01-15 14:19:18 -0600

    Add files via upload

---
+++ aspnetcore/security/key-vault-configuration/sample/Markup.cs
@@ -0,0 +1,48 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace KeyVaultConfigProviderSample
+{
+    internal static class Markup
+    {
+        internal const string Text = @"<!DOCTYPE html>
+            <html lang=""en"">
+            <head>
+                <meta charset=""utf-8"" />
+                <title>Key Vault Configuration Provider Sample</title>
+                <style>table{{border-collapse:collapse}}th,td{{padding:8px}}tr:nth-child(even){{background-color:#f2f2f2}}th{{background-color:#4CAF50;color:white}}</style>
+            </head>
+            <body style=""font-family:sans-serif"">
+                <h1>Key Vault Configuration Provider Sample</h1>
+                <div style=""overflow-x:auto"">
+                    <table>
+                        <tr>
+                            <th>Secret</th>
+                            <th>Key Vault Name</th>
+                            <th>Obtained from Key Vault</th>
+                            <th>Value</th> 
+                        </tr>
+                        <tr>
+                            <td>MySecret</td>
+                            <td><b>MySecret</b></td>
+                            <td><code>Configuration[""MySecret""]</code></td>
+                            <td>{0}</td> 
+                        </tr>
+                        <tr>
+                            <td>Section:MySecret</td>
+                            <td><b>Section--MySecret</b></td>
+                            <td><code>Configuration[""Section:MySecret""]</code></td>
+                            <td>{1}</td> 
+                        </tr>
+                        <tr>
+                            <td>Section:MySecret</td>
+                            <td><b>Section--MySecret</b></td>
+                            <td><code>Configuration.GetSection(""Section"")[""MySecret""]</code></td>
+                            <td>{2}</td> 
+                        </tr>
+                    </table>
+                </div>
+            </body>
+            </html>";
+    }
+}
--- aspnetcore/security/key-vault-configuration/sample/Startup.cs
+++ aspnetcore/security/key-vault-configuration/sample/Startup.cs
@@ -35,45 +35,7 @@ namespace KeyVaultConfigProviderSample
             app.Run(async context =>
             {
                 var encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
-                var document = $@"<!DOCTYPE html>
-                    <html lang=""en"">
-                    <head>
-                        <meta charset=""utf-8"" />
-                        <title>Key Vault Configuration Provider Sample</title>
-                        <style>table{{border-collapse:collapse}}th,td{{padding:8px}}tr:nth-child(even){{background-color:#f2f2f2}}th{{background-color:#4CAF50;color:white}}</style>
-                    </head>
-                    <body style=""font-family:sans-serif"">
-                        <h1>Key Vault Configuration Provider Sample</h1>
-                        <div style=""overflow-x:auto"">
-                            <table>
-                                <tr>
-                                    <th>Secret</th>
-                                    <th>Key Vault Name</th>
-                                    <th>Obtained from Key Vault</th>
-                                    <th>Value</th> 
-                                </tr>
-                                <tr>
-                                    <td>MySecret</td>
-                                    <td><b>MySecret</b></td>
-                                    <td><code>Configuration[""MySecret""]</code></td>
-                                    <td>{Configuration["MySecret"]}</td> 
-                                </tr>
-                                <tr>
-                                    <td>Section:MySecret</td>
-                                    <td><b>Section--MySecret</b></td>
-                                    <td><code>Configuration[""Section:MySecret""]</code></td>
-                                    <td>{Configuration["Section:MySecret"]}</td> 
-                                </tr>
-                                <tr>
-                                    <td>Section:MySecret</td>
-                                    <td><b>Section--MySecret</b></td>
-                                    <td><code>Configuration.GetSection(""Section"")[""MySecret""]</code></td>
-                                    <td>{Configuration.GetSection("Section")["MySecret"]}</td> 
-                                </tr>
-                            </table>
-                        </div>
-                    </body>
-                    </html>";
+                var document = string.Format(Markup.Text, Configuration["MySecret"], Configuration["Section:MySecret"], Configuration.GetSection("Section")["MySecret"]);
 
                 context.Response.ContentLength = encoding.GetByteCount(document);
                 context.Response.ContentType = "text/html";
