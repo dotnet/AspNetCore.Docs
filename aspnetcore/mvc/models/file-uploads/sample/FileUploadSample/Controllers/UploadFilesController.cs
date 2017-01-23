b98bc52a1a3a19d3ad633820bfa60f7a89f7acd2
Luke Latham <GuardRex@users.noreply.github.com>
2017-01-23 10:50:23 -0600

    Update UploadFilesController.cs

--- aspnetcore/mvc/models/file-uploads/sample/FileUploadSample/Controllers/UploadFilesController.cs
+++ aspnetcore/mvc/models/file-uploads/sample/FileUploadSample/Controllers/UploadFilesController.cs
@@ -30,6 +30,7 @@ namespace FileUploadSample.Controllers
             }
 
             // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
 
             return Ok(new { count = files.Count, size, filePath});
         }
