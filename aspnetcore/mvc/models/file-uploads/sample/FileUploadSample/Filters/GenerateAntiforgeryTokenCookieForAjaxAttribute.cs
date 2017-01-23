f63db9dc9ce109cd4eaff4fe09beaeaa253ceb30
Luke Latham <GuardRex@users.noreply.github.com>
2017-01-23 10:58:59 -0600

    Update GenerateAntiforgeryTokenCookieForAjaxAttribute.cs

--- aspnetcore/mvc/models/file-uploads/sample/FileUploadSample/Filters/GenerateAntiforgeryTokenCookieForAjaxAttribute.cs
+++ aspnetcore/mvc/models/file-uploads/sample/FileUploadSample/Filters/GenerateAntiforgeryTokenCookieForAjaxAttribute.cs
@@ -12,7 +12,8 @@ namespace FileUploadSample.Filters
         {
             var antiforgery = context.HttpContext.RequestServices.GetService<IAntiforgery>();
 
-            // We can send the request token as a JavaScript-readable cookie, and Angular will use it by default.
+            // We can send the request token as a JavaScript-readable cookie, 
+            // and Angular will use it by default.
             var tokens = antiforgery.GetAndStoreTokens(context.HttpContext);
             context.HttpContext.Response.Cookies.Append(
                 "XSRF-TOKEN",
