147a86b6097dc344d89f71ae97dda09e12e384f5
Steve Smith <steve@kentsmiths.com>
2017-01-31 02:14:17 -0500

    Moving from previous PR (#2635)

---
+++ aspnetcore/mvc/advanced/app-parts/sample/src/AppPartSample/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using AppPartSample.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppPartSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var entities = EntityTypes.Types;
+            return View(entities);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
