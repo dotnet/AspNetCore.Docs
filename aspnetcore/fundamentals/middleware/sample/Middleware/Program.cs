903f008c4efb12326927eeba2d11e0ff159bf2e3
Rick Anderson <riande@microsoft.com>
2017-02-06 17:23:48 -1000

    Middleware rewrite (#2641)
    * Middleware rewrite
    
    * author
    
    * fix
    
    * fix2
    
    * react to feedback
    
    * react to 2nd feedback
    
    * small fixes

--- aspnetcore/fundamentals/middleware/sample/Middleware/Program.cs
+++ aspnetcore/fundamentals/middleware/sample/Middleware/Program.cs
@@ -1,7 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace MiddlewareSample
+namespace Middleware
 {
     public class Program
     {
