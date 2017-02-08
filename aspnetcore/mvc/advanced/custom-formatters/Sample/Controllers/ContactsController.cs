5e169749e20ec958feb713a91e70ac4e47a193e3
Tom Dykstra <tdykstra@microsoft.com>
2017-02-08 12:25:32 -0800

    initial draft

---
+++ aspnetcore/mvc/advanced/custom-formatters/Sample/Controllers/ContactsController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CustomFormatterDemo.Models;
+
+namespace CustomFormatterDemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        public ContactsController(IContactRepository contacts)
+        {
+            Contacts = contacts;
+        }
+        public IContactRepository Contacts { get; set; }
+
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            return Contacts.GetAll();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}", Name="Get")]
+        public IActionResult Get(string id)
+        {
+            var contact = Contacts.Get(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(contact);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Contact contact)
+        {
+            if (contact == null)
+            {
+                return BadRequest();
+            }
+            Contacts.Add(contact);
+            return CreatedAtRoute("Get", new { id = contact.ID }, contact);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(string id, [FromBody]Contact contact)
+        {
+            if (contact == null || id == null || id != contact.ID)
+            {
+                return BadRequest();
+            }
+
+            var contactToUpdate = Contacts.Get(id);
+            if (contactToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            Contacts.Update(contact);
+            return new NoContentResult();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var contact = Contacts.Get(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            Contacts.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
---
+++ aspnetcore/mvc/advanced/custom-formatters/Sample/Formatters/VcardInputFormatter.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using CustomFormatterDemo.Models;
+
+namespace CustomFormatterDemo.Formatters
+{
+    #region classdef
+    public class VcardInputFormatter : TextInputFormatter
+    #endregion
+    {
+        #region ctor
+        public VcardInputFormatter()
+        {
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("text/vcard"));
+
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedEncodings.Add(Encoding.Unicode);
+        }
+        #endregion
+
+        #region canreadtype
+        protected override bool CanReadType(Type type)
+        {
+            if (type == typeof(Contact))
+            {
+                return base.CanReadType(type);
+            }
+            return false;
+        }
+        #endregion
+
+        #region readrequest
+        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding effectiveEncoding)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (effectiveEncoding == null)
+            {
+                throw new ArgumentNullException(nameof(effectiveEncoding));
+            }
+
+            var request = context.HttpContext.Request;
+
+            using (var reader = new StreamReader(request.Body, effectiveEncoding))
+            {
+                try
+                {
+                    await ReadLineAsync("BEGIN:VCARD", reader);
+                    await ReadLineAsync("VERSION:2.1", reader);
+
+                    var nameLine = await ReadLineAsync("N:", reader);
+                    var split = nameLine.Split(";".ToCharArray());
+                    var contact = new Contact() { LastName = split[0].Substring(2), FirstName = split[1] };
+
+                    await ReadLineAsync("FN:", reader);
+
+                    var idLine = await ReadLineAsync("UID:", reader);
+                    contact.ID = idLine.Substring(4);
+
+                    await ReadLineAsync("END:VCARD", reader);
+
+                    return await InputFormatterResult.SuccessAsync(contact);
+                }
+                catch
+                {
+                    return await InputFormatterResult.FailureAsync();
+                }
+            }
+        }
+
+        private async Task<string> ReadLineAsync(string expectedText, StreamReader reader)
+        {
+            var line = await reader.ReadLineAsync();
+            if (!line.StartsWith(expectedText))
+            {
+                throw new Exception($"Looked for '{expectedText}' and got '{line}'");
+            }
+            return line;
+        }
+        #endregion
+    }
+}
