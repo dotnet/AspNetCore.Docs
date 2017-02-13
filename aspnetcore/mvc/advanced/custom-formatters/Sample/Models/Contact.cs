356362b7666db0bcf5947fc883886ecbe1684c8b
Tom Dykstra <tdykstra@microsoft.com>
2017-02-13 11:14:43 -0800

    Custom formatters  (#2743)

---
+++ aspnetcore/mvc/advanced/custom-formatters/Sample/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomFormatterDemo.Models
+{
+    public class Contact
+    {
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
