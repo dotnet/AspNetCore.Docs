a69900f2bc7df8d5b6eb9b7c5834f954737ff254
Tom Dykstra <tdykstra@microsoft.com>
2017-02-17 13:05:53 -0800

    project.json to csproj

--- aspnetcore/migration/mvc/sample/Startup.cs
+++ aspnetcore/migration/mvc/sample/Startup.cs
@@ -8,7 +8,7 @@ namespace WebApp1
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
