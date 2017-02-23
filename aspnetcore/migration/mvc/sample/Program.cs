0366fd5f72285116beae56ac799bbd300db5304c
Tom Dykstra <tdykstra@microsoft.com>
2017-02-23 09:13:12 -0800

    project.json to csproj for MVC to MVC  Core migration doc (#2797)

--- aspnetcore/migration/mvc/sample/Program.cs
+++ aspnetcore/migration/mvc/sample/Program.cs
@@ -16,6 +16,7 @@ namespace WebApp1
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
 
             host.Run();
