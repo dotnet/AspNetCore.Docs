b2e673209ed4d971e5e0f036d6a6d71e93c917c3
Rick Anderson <riande@microsoft.com>
2017-02-28 16:51:04 -1000

    adding a model

---
+++ aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie/Models/MovieDateFormat.cs
@@ -0,0 +1,20 @@
+﻿/*
+// Start
+#region snippet_1
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+#endregion
+*/
--- aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie/Startup.cs
+++ aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
@@ -25,11 +27,17 @@ namespace MvcMovie
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        #region snippet_cs
+
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
+        #endregion
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
