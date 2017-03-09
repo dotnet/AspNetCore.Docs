ce0be3c4fa4d0f9553657e300275f1818eeea494
Alexei Pshenichnyi <alexei.pshenichnyi@gmail.com>
2017-03-09 20:03:38 +0400

    Sample was corrected.

--- aspnet/mvc/overview/older-versions/hands-on-labs/aspnet-mvc-4-dependency-injection/samples/sample22.cs
+++ aspnet/mvc/overview/older-versions/hands-on-labs/aspnet-mvc-4-dependency-injection/samples/sample22.cs
@@ -1,23 +1,17 @@
-public class 
-
-FilterProvider : IFilterProvider
+public class FilterProvider : IFilterProvider
 {
-      private IUnityContainer container;
+	private IUnityContainer container;
 
-      public FilterProvider(IUnityContainer container)
-      {
-            this.container = container;
-      }
+	public FilterProvider(IUnityContainer container)
+	{
+		this.container = container;
+	}
 
-      public IEnumerable<Filter> GetFilters(ControllerContext 
-
-controllerContext, ActionDescriptor actionDescriptor)
-      {
-            foreach (IActionFilter actionFilter in this.container.ResolveAll<IActionFilter>())
-            {
-                 yield return new Filter
-
-(actionFilter, FilterScope.First, null);
-            }
-      }
+	public IEnumerable<Filter> GetFilters(ControllerContext controllerContext, ActionDescriptor actionDescriptor)
+	{
+		foreach (IActionFilter actionFilter in this.container.ResolveAll<IActionFilter>())
+		{
+			yield return new Filter(actionFilter, FilterScope.First, null);
+		}
+	}
 }
