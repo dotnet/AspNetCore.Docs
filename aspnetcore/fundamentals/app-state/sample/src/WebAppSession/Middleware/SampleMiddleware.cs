ea33a513e86cfed5b8904a945946684565be6fc0
Steve Smith <steve@kentsmiths.com>
2017-03-30 11:33:16 -0400

    Updated project to VS2017 format. Updated HttpContext.Items key guidance.

---
+++ aspnetcore/fundamentals/app-state/sample/src/WebAppSession/Middleware/SampleMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace WebAppSession.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class SampleMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public static readonly object SampleKey = new Object();
+
+        public SampleMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Items[SampleKey] = "some value";
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class SampleMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSampleMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SampleMiddleware>();
+        }
+    }
+}
