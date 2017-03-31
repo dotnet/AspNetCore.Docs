7fc79f215e8513d2e6d580659c1680f0679d403b
Rick Anderson <riande@microsoft.com>
2017-03-30 16:41:44 -1000

    Ra/model xplat (#3114)
    * xplat version of adding a model/intro to MVC
    
    * clean up
    
    * clean up
    
    * clean up

--- aspnetcore/tutorials/first-mvc-app-xplat/start-mvc/sample/MvcMovie/Startup.cs
+++ aspnetcore/tutorials/first-mvc-app-xplat/start-mvc/sample/MvcMovie/Startup.cs
@@ -7,11 +7,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+#region snippet1
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
     public class Startup
     {
+        #endregion
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -25,11 +29,17 @@ namespace MvcMovie
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+#region snippet2
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
+  
         }
+        #endregion
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
