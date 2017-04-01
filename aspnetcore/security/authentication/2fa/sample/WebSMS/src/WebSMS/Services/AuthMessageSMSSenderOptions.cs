9401ba5592add594425ab82ccd1a6f84c2f21a2a
Rick Anderson <riande@microsoft.com>
2017-04-01 11:08:25 -1000

    Add twilio to 2FA tutorial (#3147)
    * Add twilio to 2FA tutorial
    
    * fix

--- aspnetcore/security/authentication/2fa/sample/WebSMS/src/WebSMS/Services/AuthMessageSMSSenderOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebSMS.Services
-{
-    public class AuthMessageSMSSenderOptions
-    {
-        public string Userkey { get; set; }
-        public string Password { get; set; }
-        public string Originator { get; set; }
-    }
-}
