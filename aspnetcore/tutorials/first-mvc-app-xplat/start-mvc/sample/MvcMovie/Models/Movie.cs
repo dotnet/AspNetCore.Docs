3df052629d5bff7947290a9d10df3129d6e7ff33
Rick Anderson <riande@microsoft.com>
2017-04-13 09:59:54 -1000

    validation xplat

--- aspnetcore/tutorials/first-mvc-app-xplat/start-mvc/sample/MvcMovie/Models/Movie.cs
+++ aspnetcore/tutorials/first-mvc-app-xplat/start-mvc/sample/MvcMovie/Models/Movie.cs
@@ -3,17 +3,30 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
 {
-    public class Movie
-    {
-        public int ID { get; set; }
-        public string Title { get; set; }
+   public class Movie
+{
+    public int ID { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
-        public decimal Price { get; set; }
-       public string Rating { get; set; }
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string Title { get; set; }
 
-    }
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string Genre { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+    [StringLength(5)]
+    [Required]
+    public string Rating { get; set; }
+}
 }
