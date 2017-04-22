30b69016037f69ee387df536cc44a98d6b9ae2aa
fiyazbinhasan <fiyazhasan@live.com>
2017-04-23 02:08:20 +0600

    angular sample added

---
+++ aspnetcore/client-side/angular2/sample/DemoUsingAngular/ClientApp/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { UniversalModule } from 'angular2-universal';
+import { AppComponent } from './components/app/app.component'
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { PostComponent } from './components/post/post.component';
+
+@NgModule({
+    bootstrap: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        FetchDataComponent,
+        HomeComponent,
+        BlogComponent,
+        PostComponent
+    ],
+    imports: [
+        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', loadChildren: './components/counter/counter.module#CounterModule' },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'blog', component: BlogComponent },
+            { path: 'blog/:id', component: PostComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+})
+export class AppModule {
+}
---
+++ aspnetcore/client-side/angular2/sample/DemoUsingAngular/Controllers/BlogsController.cs
@@ -0,0 +1,22 @@
+﻿using DemoUsingAngular.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoUsingAngular.Data;
+
+namespace DemoUsingAngular.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlogsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var blogs = SampleData.Blogs();
+
+            return Ok(blogs);
+        }
+    }
+}
