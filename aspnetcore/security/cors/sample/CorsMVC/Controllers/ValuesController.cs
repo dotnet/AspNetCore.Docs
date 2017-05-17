cd3faba444ed292096ebc0d920b7962c2240defe
Tom Dykstra <tdykstra@microsoft.com>
2017-05-17 11:26:41 -0700

    update examples to .csproj

---
+++ aspnetcore/security/cors/sample/CorsMVC/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+
+namespace CorsMVC.Controllers
+{
+    #region EnableOnController
+    [EnableCors("AllowSpecificOrigin")]
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+#endregion
+    {
+        // GET api/values
+        #region EnableOnAction
+        [EnableCors("AllowSpecificOrigin")]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        #endregion
+
+        // GET api/values/5
+        #region DisableOnAction
+        [DisableCors]
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+#endregion
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ aspnetcore/security/cors/sample/CorsMVC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CorsMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
