1dd0126be01c7c086b76406deae6ff432dec72d4
Steve Smith <steve@kentsmiths.com>
2017-06-13 17:23:30 -0400

    adding sample project with default identity controllers and views

---
+++ aspnetcore/security/authentication/identity-custom-storage-providers/sample/CustomIdentityProviderSample/CustomProvider/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace CustomIdentityProviderSample.CustomProvider
+{
+    public class ApplicationUser : ClaimsIdentity
+    {
+        public virtual System.Guid Id { get; set; } = Guid.NewGuid();
+        public virtual string UserName { get; set; }
+        public virtual string Email { get; set; }
+        public virtual bool EmailConfirmed { get; set; }
+        public virtual String PasswordHash { get; set; }
+        public string NormalizedUserName { get; internal set; }
+    }
+}
