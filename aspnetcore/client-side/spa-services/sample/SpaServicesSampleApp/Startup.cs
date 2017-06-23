82aab44fe7be4c78319d528f6f5093829eebe176
Scott Addie <scott.addie@microsoft.com>
2017-06-23 10:43:59 -0500

    Refine existing content

--- aspnetcore/client-side/spa-services/sample/SpaServicesSampleApp/Startup.cs
+++ aspnetcore/client-side/spa-services/sample/SpaServicesSampleApp/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
