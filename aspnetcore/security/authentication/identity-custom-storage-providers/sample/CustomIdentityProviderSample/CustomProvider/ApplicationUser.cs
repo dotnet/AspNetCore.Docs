6293630b103a18446a2ea672a1cb35ddabee754d
Steve Smith <steve@kentsmiths.com>
2017-06-27 14:50:02 -0400

    Updated home page to provide instructions. Updated ApplicationUser to implement IIdentity.

--- aspnetcore/security/authentication/identity-custom-storage-providers/sample/CustomIdentityProviderSample/CustomProvider/ApplicationUser.cs
+++ aspnetcore/security/authentication/identity-custom-storage-providers/sample/CustomIdentityProviderSample/CustomProvider/ApplicationUser.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Security.Claims;
+using System.Security.Principal;
 
 namespace CustomIdentityProviderSample.CustomProvider
 {
-    public class ApplicationUser : ClaimsIdentity
+    public class ApplicationUser : IIdentity
     {
-        public virtual System.Guid Id { get; set; } = Guid.NewGuid();
+        public virtual Guid Id { get; set; } = Guid.NewGuid();
         public virtual string UserName { get; set; }
         public virtual string Email { get; set; }
         public virtual bool EmailConfirmed { get; set; }
         public virtual String PasswordHash { get; set; }
         public string NormalizedUserName { get; internal set; }
+        public string AuthenticationType { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public string Name { get; set; }
     }
 }
