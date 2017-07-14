8090ad3ef1c870592b9c22c55f017908c358c3b0
Rick Anderson <riande@microsoft.com>
2017-07-14 09:53:24 -0600

    Razor Pages update

---
+++ aspnetcore/mvc/razor-pages/index/sample/RazorPagesContacts/Program.cs
@@ -0,0 +1,26 @@
+﻿#define Debug
+
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace RazorPagesContacts
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+#if  Debug
+                .UseStartup<StartupDebug>()
+#else
+                .UseStartup<Startup>()
+#endif
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ aspnetcore/mvc/razor-pages/index/sample/RazorPagesContacts2/Pages/Reports/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesContacts2.Pages.Reports
+{
+    public class Class
+    {
+    }
+}
