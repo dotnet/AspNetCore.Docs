f8b67a4d4742784731bbc2d939f4eeafc742537c
Scott Addie <scott.addie@microsoft.com>
2017-09-11 15:28:55 -0500

    Add DB init code location recommendation

---
+++ aspnetcore/migration/1x-to-2x/samples/AspNetCoreDotNetCore1App/AspNetCoreDotNetCore1App/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreDotNetCore1App.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AspNetCoreDotNetCore1App.Models.Movie> Movie { get; set; }
+    }
+}
