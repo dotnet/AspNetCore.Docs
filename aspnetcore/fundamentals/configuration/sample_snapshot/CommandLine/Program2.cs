84f804d75e3962b453d6c23c06a7c66ea194db41
Luke Latham <github@lukelatham.com>
2017-09-15 00:59:15 -0500

    Document formats supported for command line args provider
    Updates
    
    Updates

---
+++ aspnetcore/fundamentals/configuration/sample_snapshot/CommandLine/Program2.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .AddCommandLine(args)
+            .Build();
+
+        var host = new WebHostBuilder()
+            .UseConfiguration(config)
+            .UseKestrel()
+            .Configure(app =>
+            {
+                app.Run(context => context.Response.WriteAsync("Hello World!"));
+            })
+            .Build();
+
+        host.Run();
+    }
+}
---
+++ aspnetcore/fundamentals/configuration/sample_snapshot/readme.md
@@ -0,0 +1,1 @@
+The code in this folder is used for snapshots of code in the Configuration topic at: https://docs.microsoft.com/aspnet/core/fundamentals/configuration
