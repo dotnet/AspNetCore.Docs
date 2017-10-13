abb99e32a562717c5be1224fe172b1f68a4f2b9e
rick-anderson <riande@microsoft.com>
2017-10-13 10:36:45 -1000

    work

--- aspnetcore/data/ef-rp/intro/samples/cu/Models/StudentVM.cs
+++ aspnetcore/data/ef-rp/intro/samples/cu/Models/StudentVM.cs
@@ -4,6 +4,7 @@ namespace ContosoUniversity.Models
 {
     public class StudentVM
     {
+        public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
