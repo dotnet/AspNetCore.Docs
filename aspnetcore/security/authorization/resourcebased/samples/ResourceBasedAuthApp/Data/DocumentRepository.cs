16a2bcaf961796ec0e6804a50e5b82cb7ecf3c46
Scott Addie <scott.addie@microsoft.com>
2017-11-03 13:55:38 -0500

    Initial commit of sample app

---
+++ aspnetcore/security/authorization/resourcebased/samples/ResourceBasedAuthApp/Data/DocumentRepository.cs
@@ -0,0 +1,24 @@
+﻿using ResourceBasedAuthApp.Models;
+using System;
+
+namespace ResourceBasedAuthApp.Data
+{
+    public class DocumentRepository : IDocumentRepository
+    {
+        public Document Find(Guid documentId)
+        {
+            return new Document
+            {
+                Author = "john.doe@microsoft.com",
+                Content = null,
+                ID = documentId,
+                Title = "Test Document"
+            };
+        }
+    }
+
+    public interface IDocumentRepository
+    {
+        Document Find(Guid documentId);
+    }
+}
---
+++ aspnetcore/security/authorization/resourcebased/samples/ResourceBasedAuthApp/Services/DocumentAuthorizationHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using ResourceBasedAuthApp.Models;
+using System.Threading.Tasks;
+
+namespace ResourceBasedAuthApp.Services
+{
+    public class DocumentAuthorizationHandler : AuthorizationHandler<SameAuthorRequirement, Document>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                                                       SameAuthorRequirement requirement,
+                                                       Document resource)
+        {
+            if (context.User.Identity.Name == resource.Author)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public class SameAuthorRequirement : IAuthorizationRequirement { }
+}
