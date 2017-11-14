ed26597054f5f04cb4c7547726a45e73fa410421
Isaac Levin <isaac.r.levin@gmail.com>
2017-11-14 09:06:48 -0500

    revert change and slight rename

---
+++ aspnetcore/mvc/razor-pages/index/sample/RazorPagesContacts2/Pages/Customers/Create2Dot.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesContacts.Data;
+
+namespace RazorPagesContacts.Pages.Customers
+{
+    public class Create2DotModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public Create2DotModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Customers.Add(Customer);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("../Index");
+        }
+    }
+}
