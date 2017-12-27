a5d6221e9858e00f2e0b910346f7a4b98b180fed
Shimmy <weitzhandler@users.noreply.github.com>
2017-12-27 08:09:48 +0200

    Making use of the ValueProviderFactoryExtensions

--- aspnetcore/mvc/models/file-uploads/sample/FileUploadSample/Filters/DisableFormValueModelBindingAttribute.cs
+++ aspnetcore/mvc/models/file-uploads/sample/FileUploadSample/Filters/DisableFormValueModelBindingAttribute.cs
@@ -1,36 +1,21 @@
-﻿using System;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 
 namespace FileUploadSample.Filters
 {
-    #region snippet1
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class DisableFormValueModelBindingAttribute : Attribute, IResourceFilter
     {
         public void OnResourceExecuting(ResourceExecutingContext context)
         {
-            var formValueProviderFactory = context.ValueProviderFactories
-                .OfType<FormValueProviderFactory>()
-                .FirstOrDefault();
-            if (formValueProviderFactory != null)
-            {
-                context.ValueProviderFactories.Remove(formValueProviderFactory);
-            }
-
-            var jqueryFormValueProviderFactory = context.ValueProviderFactories
-                .OfType<JQueryFormValueProviderFactory>()
-                .FirstOrDefault();
-            if (jqueryFormValueProviderFactory != null)
-            {
-                context.ValueProviderFactories.Remove(jqueryFormValueProviderFactory);
-            }
+            var factories = context.ValueProviderFactories;
+            factories.RemoveType<FormValueProviderFactory>();
+            factories.RemoveType<JQueryFormValueProviderFactory>();
         }
 
         public void OnResourceExecuted(ResourceExecutedContext context)
         {
         }
     }
-    #endregion
 }
