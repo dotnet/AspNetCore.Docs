245f1f33f8a056e588050854be3216044236de8f
Scott Addie <scott.addie@microsoft.com>
2018-01-12 12:14:17 -0600

    Fix broken download link for samples

--- aspnetcore/fundamentals/static-files/samples/2x/Sample2x/Startup.cs
+++ aspnetcore/fundamentals/static-files/samples/2x/Sample2x/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
