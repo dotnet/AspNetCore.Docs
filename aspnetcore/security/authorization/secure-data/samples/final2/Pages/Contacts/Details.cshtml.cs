1a1460c68fc1153eaa96ac1c0f2a46ad3ab92da0
rick-anderson <riande@microsoft.com>
2018-01-19 11:42:24 -1000

    work

--- aspnetcore/security/authorization/secure-data/samples/final2/Pages/Contacts/Details.cshtml.cs
+++ aspnetcore/security/authorization/secure-data/samples/final2/Pages/Contacts/Details.cshtml.cs
@@ -1,25 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ContactManager.Authorization;
+using ContactManager.Data;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using ContactManager.Data;
-using ContactManager.Models;
+using System.Threading.Tasks;
 
 namespace ContactManager.Pages.Contacts
 {
     public class DetailsModel : PageModel
     {
-        private readonly ContactManager.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly IAuthorizationService _authorizationService;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public DetailsModel(ContactManager.Data.ApplicationDbContext context)
+        public DetailsModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
+            _authorizationService = authorizationService;
         }
 
         public Contact Contact { get; set; }
+        public object AuthorizationService { get; private set; }
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
@@ -30,6 +38,31 @@ namespace ContactManager.Pages.Contacts
                 return NotFound();
             }
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id, ContactStatus status)
+        {
+             var contact = await _context.Contact.FirstOrDefaultAsync(m => m.ContactId == id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            var contactOperation = (status == ContactStatus.Approved) ? ContactOperations.Approve
+                                                                     : ContactOperations.Reject;
+
+            var isAuthorized = await _authorizationService.AuthorizeAsync(User, contact,
+                                        contactOperation);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+            contact.Status = status;
+            _context.Contact.Update(contact);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
