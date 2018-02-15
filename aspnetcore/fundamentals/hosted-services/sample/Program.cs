f851ee9cef08a1edc89efd9fc305c4abf20900f1
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-02-15 12:28:27 -0600

    React to feedback

--- aspnetcore/fundamentals/hosted-services/sample/Program.cs
+++ aspnetcore/fundamentals/hosted-services/sample/Program.cs
@@ -14,18 +14,6 @@ namespace BackgroundTasksSample
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                    // Remove the Debug provider (and other providers) and filter most
-                    // of the remaining logging. Reducing the logging output makes it
-                    // easier to see the messages produced by the background tasks
-                    // demonstrated in this sample app.
-                    logging.ClearProviders();
-                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                    logging.AddFilter("Microsoft.AspNetCore.Mvc.RedirectToRouteResult", LogLevel.None);
-                    logging.AddFilter("Microsoft.AspNetCore.Mvc.RazorPages.Internal.PageActionInvoker", LogLevel.None);
-                    logging.AddFilter("Microsoft.AspNetCore.StaticFiles.StaticFileMiddleware", LogLevel.None);
-                })
                 .Build();
     }
 }
