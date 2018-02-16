901bf5bf1fccfc27d8df2420b2705d965024086b
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-02-15 18:00:39 -0600

    Update sample

--- aspnetcore/fundamentals/hosted-services/sample/Services/TimedHostedService.cs
+++ aspnetcore/fundamentals/hosted-services/sample/Services/TimedHostedService.cs
@@ -2,31 +2,39 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace BackgroundTasksSample.Services
 {
     #region snippet1
     internal class TimedHostedService : IHostedService, IDisposable
     {
+        private readonly ILogger _logger;
         private Timer _timer;
+
+        public TimedHostedService(ILogger<TimedHostedService> logger)
+        {
+            _logger = logger;
+        }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            Console.WriteLine($"{DateTime.UtcNow} - Timed Background Service is starting.");
+            _logger.LogInformation("Timed Background Service is starting.");
 
-            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, 
+                TimeSpan.FromSeconds(5));
 
             return Task.CompletedTask;
         }
 
         private void DoWork(object state)
         {
-            Console.WriteLine($"{DateTime.UtcNow} - Timed Background Service is working.");
+            _logger.LogInformation("Timed Background Service is working.");
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            Console.WriteLine($"{DateTime.UtcNow} - Timed Background Service is stopping.");
+            _logger.LogInformation("Timed Background Service is stopping.");
 
             _timer?.Change(Timeout.Infinite, 0);
 
