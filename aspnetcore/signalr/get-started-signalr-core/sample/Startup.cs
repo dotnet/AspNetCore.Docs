daae3cccf1fd0c70b3ec0431d5ea53d079d83c22
rachelappel <rachelap@microsoft.com>
2018-03-01 10:44:08 -0500

    minor code enhancements

--- aspnetcore/signalr/get-started-signalr-core/sample/Startup.cs
+++ aspnetcore/signalr/get-started-signalr-core/sample/Startup.cs
@@ -39,7 +39,7 @@ namespace SignalRChat
 
             app.UseSignalR(routes =>
             {
-                routes.MapHub<ChatHub>("/chat");
+                routes.MapHub<ChatHub>("/chathub");
             });
 
             app.UseMvc(routes =>
