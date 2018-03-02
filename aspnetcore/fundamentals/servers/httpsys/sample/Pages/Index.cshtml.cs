bef5541327855caac1244657747e363a59466cee
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-03-02 12:43:13 -0600

    Servers > HTTP.sys UE pass (#5558)
    * Servers > HTTP.sys UE pass
    
    Updates
    
    Updates
    
    Updates
    
    Update
    
    * Move MaxRequestBodySize to a section
    
    * React to feedback
    
    * React to feedback
    
    Update

---
+++ aspnetcore/fundamentals/servers/httpsys/sample/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HttpSysSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
