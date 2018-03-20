863af9c5a2731c27e0622b30205a84717a256830
Scott Addie <scott.addie@microsoft.com>
2018-03-20 14:48:34 -0500

    Add Web API controller action return types doc

---
+++ aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiSample.DataAccess.Models
+{
+    #region snippet_ProductClass
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+    #endregion
+}
