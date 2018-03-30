dbf2bbda2f10d43e16e82024527a5a6af1e4d228
Rachel Appel <rachelappel@users.noreply.github.com>
2018-03-30 12:34:42 +0200

    SignalR Core Hubs (#5639)

---
+++ aspnetcore/signalr/hubs/sample/chat.js
@@ -0,0 +1,22 @@
+﻿const connection = new signalR.HubConnection('/chathub');
+
+connection.on('ReceiveMessage', (timestamp, user, message) => {
+    const encodedUser = user;
+    const encodedMsg = message;
+    const listItem = document.createElement('li');
+    listItem.innerHTML = timestamp + ' <b>' + encodedUser + '</b>: ' + encodedMsg;
+    document.getElementById('messages').appendChild(listItem);
+});
+
+document.getElementById('sendButton').addEventListener('click', event => {
+    const msg = document.getElementById('message').value;
+    const usr = document.getElementById('user').value;
+    connection.send('SendMessageToAll', usr, msg).catch(err => logErr(errorMsg));
+    event.preventDefault();
+});
+
+function logErr(errorMsg) {
+    console.log(errorMsg);
+}
+
+connection.start().catch(err => logErr(errorMsg));
