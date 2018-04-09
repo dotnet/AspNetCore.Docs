dc7414bf0b5dc564e64bbf3dcb6dad64e617be3c
Rachel Appel <rachelappel@users.noreply.github.com>
2018-04-09 19:47:50 -0400

    SignalR JavaScript client (#5861)
    * SignalR JavaScript client
    
    * feedback changes
    
    * feedback mods
    
    * feedback mods
    
    * mods after feedback

---
+++ aspnetcore/signalr/javascript-client/sample/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub 
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
