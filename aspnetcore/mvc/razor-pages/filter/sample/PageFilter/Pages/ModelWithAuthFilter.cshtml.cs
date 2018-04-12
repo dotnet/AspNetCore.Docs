288347e571aaf80580c4530598ae6d8f10826ae4
Rick Anderson <riande@microsoft.com>
2018-04-12 11:29:02 -1000

    Razor Pages Filters (#5835)
    * WIP Razor Pages Filters
    
    * spelen
    
    * clean up
    
    * clean up
    
    * mvc filters UE edit
    
    * wrok
    
    * wrok
    
    * wrok
    
    * react to feedback
    
    * minor
    
    * minor
    
    * minor
    
    * minor
    
    * minor
    
    * minor
    
    * minor
    
    * minor
    
    * minor
    
    * minor
    
    * minor
    
    * react to feedback
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

---
+++ aspnetcore/mvc/razor-pages/filter/sample/PageFilter/Pages/ModelWithAuthFilter.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PageFilter.Pages
+{
+    [Authorize]
+    public class ModelWithAuthFilterModel : PageModel
+    {
+        public IActionResult OnGet() => Page();
+    }
+}
