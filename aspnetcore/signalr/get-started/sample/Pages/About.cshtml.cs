9678773790522fb5fdb652f89f5334f2a585912d
rachelappel <rachelap@microsoft.com>
2018-04-16 18:35:46 -0400

    Update doc with another sample for Es5 JS

--- aspnetcore/signalr/get-started/sample/Pages/About.cshtml.cs
+++ aspnetcore/signalr/get-started/sample/Pages/About.cshtml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SignalRCoreChat.Pages
+namespace SignalRChat.Pages
 {
     public class AboutModel : PageModel
     {
