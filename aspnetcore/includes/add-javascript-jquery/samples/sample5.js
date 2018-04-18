361b46af1fcfcc3cef45fdd9f3214b737af0c007
Rick Anderson <riande@microsoft.com>
2018-04-18 10:30:51 -1000

    Revert "Update jQuery Example per 6004 (#6016)"
    This reverts commit 2dec209096a7f3d301cfd67f1715ff3c9d0ec777.

--- aspnetcore/includes/add-javascript-jquery/samples/sample5.js
+++ aspnetcore/includes/add-javascript-jquery/samples/sample5.js
@@ -1,20 +1,10 @@
-function addItem() {
-    var item = {
-        'name': $('#add-name').val(),
-        'isComplete': false
-    };
-
-    $.ajax(
-        {
-            type: "post",
-            accepts: "application/json",
-            url: uri,
-            contentType: "application/json",
-            data: JSON.stringify(item),
-            success: function (result) {
-                getData();
-                $('#add-name').val('');
-            }
-        }
-    );
-};
+function find() {
+    var id = $('#todoId').val();
+    $.getJSON(apiUrl + '/' + id)
+        .done(function (data) {
+            $('#todo').text(formatItem(data));
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            $('#todo').text('Error: ' + err);
+        });
+}
