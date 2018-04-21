03fe9cda834aa5760430b8c4871950d4c431f4a5
Rick Anderson <riande@microsoft.com>
2018-04-20 17:12:43 -1000

    SetCompatibilityVersion (#5948)
    * WIP: SetCompatibilityVersion
    
    * work
    
    * work
    
    * work
    
    * work
    
    * spelen
    
    * spelen
    
    * react to feedback
    
    * react to feedback
    
    * react to feedback
    
    * react to feedback
    
    * react to feedback
    
    * react to feedback

---
+++ aspnetcore/fundamentals/startup/sampleCompatibility/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp1
+{
+    public class Startup
+    {
+        #region snippet1
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+        #endregion
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
