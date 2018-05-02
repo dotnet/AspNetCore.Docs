039252bc7a9e2037065d366fca836a987783974d
Steve Gordon <sgordon@hotmail.co.uk>
2018-05-02 18:50:34 +0100

    Starting work on HttpClientFactory documentation (#5483)
    Starting work on HttpClientFactory documentation

---
+++ aspnetcore/fundamentals/http-requests/samples/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HttpClientFactorySample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder<Startup>(args);
+    }
+}
