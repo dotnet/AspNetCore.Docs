d49cd5e34b77a05b0fed1e03f96657e1c27c46d4
user135711 <user135711@fake.com>
2018-05-08 01:14:30 -0700

    missing db vscode api

--- aspnetcore/tutorials/first-web-api/samples/2.1/TodoApi/Startup.cs
+++ aspnetcore/tutorials/first-web-api/samples/2.1/TodoApi/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TodoApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TodoApi
 {
@@ -18,6 +20,8 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => 
+                opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
