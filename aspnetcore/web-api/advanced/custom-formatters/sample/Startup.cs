876849285c4fcf8ea877b91c38823cdf3953eea5
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-05-18 10:06:43 -0500

    Change sample folder casing (#6475)

---
+++ aspnetcore/web-api/advanced/custom-formatters/sample/Startup.cs
@@ -0,0 +1,49 @@
+﻿using CustomFormatterDemo.Formatters;
+using CustomFormatterDemo.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CustomFormatterDemo
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            #region mvcoptions
+            services.AddMvc(options =>
+            {
+                options.InputFormatters.Insert(0, new VcardInputFormatter());
+                options.OutputFormatters.Insert(0, new VcardOutputFormatter());
+            });
+            #endregion
+
+            services.AddSingleton<IContactRepository, ContactRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
