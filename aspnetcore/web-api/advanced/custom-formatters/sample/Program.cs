65e4b5b49706ce842532bb13436791f4f6d690c2
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-05-18 09:29:03 -0500

    Change sample folder casing

---
+++ aspnetcore/web-api/advanced/custom-formatters/sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace CustomFormatterDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
