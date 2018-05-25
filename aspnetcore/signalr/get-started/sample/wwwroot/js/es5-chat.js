0d724b5c7bdde35fd686d6a30e27d2726dd56991
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-05-25 09:43:08 -0700

    update SignalR getting started sample to RC1 (#6597)
    We missed a file in the earlier update! 😄 
    
    Also, the ES5 version seemed to be out of date and was missing the `.start` call.

--- aspnetcore/signalr/get-started/sample/wwwroot/js/es5-chat.js
+++ aspnetcore/signalr/get-started/sample/wwwroot/js/es5-chat.js
@@ -5,24 +5,25 @@
 
 "use strict";
 
-var connection = new signalR.HubConnection("/chathub", { logger: signalR.LogLevel.Information });
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 connection.on("ReceiveMessage", function (user, message) {
-    var encodedMsg = user + " says " + message;
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
     var li = document.createElement("li");
     li.textContent = encodedMsg;
     document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error;
+        return console.error(err.toString());
     });
     event.preventDefault();
-});
-
-connection.start().catch(function (err) {
-    return console.error;
 });
