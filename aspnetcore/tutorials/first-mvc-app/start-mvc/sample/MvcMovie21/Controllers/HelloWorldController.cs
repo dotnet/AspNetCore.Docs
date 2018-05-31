783c91708a854e909e032434a60573c3c7582245
Rick Anderson <riande@microsoft.com>
2018-05-30 20:01:13 -1000

    Update intro to MVC to 2.1 (#6708)
    * WIP: Update intro to MVC to 2.1
    
    * WIP: Update intro to MVC to 2.1
    
    * work
    
    * SQL
    
    * controller methods
    
    * validation
    
    * details and delete
    
    * clean up
    
    * clean up

---
+++ aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie21/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie21/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
