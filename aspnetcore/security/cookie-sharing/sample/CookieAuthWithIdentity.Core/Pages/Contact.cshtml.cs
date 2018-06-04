515b23ba30c5481977e8077e36295647b84feb88
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-06-04 12:04:36 -0500

    2.1 Sample Updates (Round 9) (#6839)

--- aspnetcore/security/cookie-sharing/sample/CookieAuthWithIdentity.Core/Pages/Contact.cshtml.cs
+++ aspnetcore/security/cookie-sharing/sample/CookieAuthWithIdentity.Core/Pages/Contact.cshtml.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CookieAuthCore.Pages
+namespace CookieAuthWithIdentity.Core.Pages
 {
-    public class AboutModel : PageModel
+    public class ContactModel : PageModel
     {
         public string Message { get; set; }
 
         public void OnGet()
         {
-            Message = "Your application description page.";
+            Message = "Your contact page.";
         }
     }
 }
