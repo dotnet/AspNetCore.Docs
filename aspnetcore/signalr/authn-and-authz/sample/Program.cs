604d864b320718abb5519f04fe67dc48166910c9
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-06-29 10:46:15 -0700

    add sample and docs on Windows Auth

---
+++ aspnetcore/signalr/authn-and-authz/sample/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SignalRAuthenticationSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
