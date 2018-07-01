9ee7a38dfcbba0bbb0682f6457fecf5c776887c9
granthoff1107 <granthoff1107@gmail.com>
2018-07-01 00:45:30 -0400

    Fixing Bugs and Reuses Implementation.
    Reusing BackgroundService abstract class,  Removing call Task.Run

--- aspnetcore/fundamentals/host/hosted-services/samples/2.x/BackgroundTasksSample-WebHost/Services/QueuedHostedService.cs
+++ aspnetcore/fundamentals/host/hosted-services/samples/2.x/BackgroundTasksSample-WebHost/Services/QueuedHostedService.cs
@@ -7,15 +7,14 @@ using Microsoft.Extensions.Logging;
 namespace BackgroundTasksSample.Services
 {
     #region snippet1
-    public class QueuedHostedService : IHostedService
+    
+    public class QueuedHostedService : BackgroundService
     {
-        private CancellationTokenSource _shutdown = 
-            new CancellationTokenSource();
+        private CancellationTokenSource _shutdown = new CancellationTokenSource();
         private Task _backgroundTask;
         private readonly ILogger _logger;
 
-        public QueuedHostedService(IBackgroundTaskQueue taskQueue, 
-            ILoggerFactory loggerFactory)
+        public QueuedHostedService(IBackgroundTaskQueue taskQueue, ILoggerFactory loggerFactory)
         {
             TaskQueue = taskQueue;
             _logger = loggerFactory.CreateLogger<QueuedHostedService>();
@@ -23,43 +22,22 @@ namespace BackgroundTasksSample.Services
 
         public IBackgroundTaskQueue TaskQueue { get; }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _logger.LogInformation("Queued Hosted Service is starting.");
-
-            _backgroundTask = Task.Run(BackgroundProceessing);
-
-            return Task.CompletedTask;
-        }
-
-        private async Task BackgroundProceessing()
-        {
-            while (!_shutdown.IsCancellationRequested)
+            while (false == stoppingToken.IsCancellationRequested)
             {
-                var workItem = 
-                    await TaskQueue.DequeueAsync(_shutdown.Token);
-
+                var workItem = await TaskQueue.DequeueAsync(stoppingToken);
                 try
                 {
-                    await workItem(_shutdown.Token);
+                    await workItem(this._shutdown.Token);
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError(ex, 
-                        $"Error occurred executing {nameof(workItem)}.");
+                    this._logger.LogError(ex, $"Error occurred executing {nameof(workItem)}.");
                 }
             }
         }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            _logger.LogInformation("Queued Hosted Service is stopping.");
-
-            _shutdown.Cancel();
-
-            return Task.WhenAny(_backgroundTask, 
-                Task.Delay(Timeout.Infinite, cancellationToken));
-        }
     }
+    
     #endregion
 }
