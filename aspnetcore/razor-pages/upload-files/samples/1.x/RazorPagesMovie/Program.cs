53f4340dec1ad8c593cdc01d271bfbddaf420af6
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-07-11 16:24:22 -0500

    Move RP tutorial file uploads into its own topic

---
+++ aspnetcore/razor-pages/upload-files/samples/1.x/RazorPagesMovie/Program.cs
@@ -0,0 +1,45 @@
+﻿// Unused usings removed.
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Models;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<MovieContext>();
+                    // requires using Microsoft.EntityFrameworkCore;
+                    context.Database.Migrate();
+                    // Requires using RazorPagesMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
