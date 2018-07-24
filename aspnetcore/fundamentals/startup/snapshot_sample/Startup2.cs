0b36bb80300e1d2faf81d7c3cea9184bd18e4d37
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-07-24 16:03:36 -0500

    Enhance DI remarks in Startup topic

--- aspnetcore/fundamentals/startup/snapshot_sample/Startup2.cs
+++ aspnetcore/fundamentals/startup/snapshot_sample/Startup2.cs
@@ -1,27 +1,35 @@
 public class Startup
 {
-    public Startup(IHostingEnvironment env, IConfiguration config)
-    {
-        HostingEnvironment = env;
-        Configuration = config;
-    }
+    private readonly IHostingEnvironment _env;
+    private readonly IConfiguration _config;
+    private readonly ILogger<Startup> _logger;
 
-    public IHostingEnvironment HostingEnvironment { get; }
-    public IConfiguration Configuration { get; }
+    public Startup(IHostingEnvironment env, IConfiguration config, 
+        ILogger<Startup> logger)
+    {
+        _env = env;
+        _config = config;
+        _logger = logger;
+    }
 
     public void ConfigureServices(IServiceCollection services)
     {
-        if (HostingEnvironment.IsDevelopment())
+        if (_env.IsDevelopment())
         {
-            // Development configuration
+            // Development service configuration
+
+            _logger.LogInformation("Development environment");
         }
         else
         {
-            // Staging/Production configuration
+            // Non-development service configuration
+
+            _logger.LogInformation($"Environment: {_env.EnvironmentName}");
         }
 
-        // Configuration is available during startup. Examples:
-        // Configuration["key"]
-        // Configuration["subsection:suboption1"]
+        // Configuration is available during startup.
+        // Examples:
+        //   _config["key"]
+        //   _config["subsection:suboption1"]
     }
 }
