07a71bfede5de255ee02c3999192fc267856e111
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-08-01 16:02:23 -0500

    File Providers topic+samples update (#7708)

--- aspnetcore/fundamentals/file-providers/sample/src/FileProviderSample/wwwroot/js/site.js
+++
@@ -1,2 +0,0 @@
-﻿// Write your Javascript code.
-   
---
+++ aspnetcore/fundamentals/file-providers/samples/1.x/FileProviderSample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+namespace FileProviderSample.Controllers
+{
+    #region snippet1
+    public class HomeController : Controller
+    {
+        private readonly IFileProvider _fileProvider;
+
+        public HomeController(IFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+        }
+
+        public IActionResult Index()
+        {
+            var contents = _fileProvider.GetDirectoryContents(string.Empty);
+
+            return View(contents);
+        }
+    }
+    #endregion
+}
