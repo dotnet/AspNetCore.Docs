72fadad569cb86ed24d8abfd6756505097453c30
Derek <KrunchMuffin@users.noreply.github.com>
2018-08-15 00:52:17 -0400

    Update sample2.cs
    The Twilio codes no longer works and has been updated.

--- aspnet/mvc/overview/security/aspnet-mvc-5-app-with-sms-and-email-two-factor-authentication/samples/sample2.cs
+++ aspnet/mvc/overview/security/aspnet-mvc-5-app-with-sms-and-email-two-factor-authentication/samples/sample2.cs
@@ -2,7 +2,27 @@ public class SmsService : IIdentityMessageService
 {
     public Task SendAsync(IdentityMessage message)
     {
-        // Twilio Begin
+        //// Twilio Begin NEW
+        //var accountSid = ConfigurationManager.AppSettings["SMSAccountIdentification"];
+        //var authToken = ConfigurationManager.AppSettings["SMSAccountPassword"];
+        //var fromNumber = ConfigurationManager.AppSettings["SMSAccountFrom"];
+
+        //TwilioClient.Init(accountSid, authToken);
+
+        //MessageResource result = MessageResource.Create(
+            //new PhoneNumber(message.Destination),
+            //from: new PhoneNumber(fromNumber),
+           //body: message.Body
+        //);
+
+        ////Status is one of Queued, Sending, Sent, Failed or null if the number is not valid
+         //Trace.TraceInformation(result.Status.ToString());
+        ////Twilio doesn't currently have an async API, so return success.
+         //return Task.FromResult(0);
+        //// Twilio End  NEW
+        
+        // below code is old and no longer functions. use code above.
+        // Twilio Begin OLD
         // var Twilio = new TwilioRestClient(
         //   System.Configuration.ConfigurationManager.AppSettings["SMSAccountIdentification"],
         //   System.Configuration.ConfigurationManager.AppSettings["SMSAccountPassword"]);
@@ -14,7 +34,7 @@ public class SmsService : IIdentityMessageService
         // Trace.TraceInformation(result.Status);
         // Twilio doesn't currently have an async API, so return success.
         // return Task.FromResult(0);
-        // Twilio End
+        // Twilio End OLD
 
         // ASPSMS Begin 
         // var soapSms = new MvcPWx.ASPSMSX2.ASPSMSX2SoapClient("ASPSMSX2Soap");
