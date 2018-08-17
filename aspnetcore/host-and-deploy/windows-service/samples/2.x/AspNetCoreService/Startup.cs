b6e855ff24b164e142b20fcc72650b4456a6c4ba
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-08-16 19:44:31 -0500

    Go with a field for DI config in Startup

--- aspnetcore/host-and-deploy/windows-service/samples/2.x/AspNetCoreService/Startup.cs
+++ aspnetcore/host-and-deploy/windows-service/samples/2.x/AspNetCoreService/Startup.cs
@@ -9,12 +9,12 @@ namespace AspNetCoreService
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
         public Startup(IConfiguration config)
         {
-            Configuration = config;
+            _config = config;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
