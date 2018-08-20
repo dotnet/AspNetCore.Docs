1179bb8c3b4b76c0649de2c4fdd74d2fd7e275ac
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-08-20 14:55:49 -0500

    Routing updates
    Updates
    
    Update

--- aspnetcore/fundamentals/routing/samples/1.x/RoutingSample/Startup.cs
+++ aspnetcore/fundamentals/routing/samples/1.x/RoutingSample/Startup.cs
@@ -8,14 +8,16 @@ namespace RoutingSample
 {
     public class Startup
     {
+        #region snippet_ConfigureServices
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRouting();
         }
+        #endregion
 
-        // Routes must configured in Configure
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
+            #region snippet_RouteHandler
             var trackPackageRouteHandler = new RouteHandler(context =>
             {
                 var routeValues = context.GetRouteData().Values;
@@ -32,7 +34,7 @@ namespace RoutingSample
             routeBuilder.MapGet("hello/{name}", context =>
             {
                 var name = context.GetRouteValue("name");
-                // This is the route handler when HTTP GET "hello/<anything>"  matches
+                // The route handler when HTTP GET "hello/<anything>" matches
                 // To match HTTP GET "hello/<anything>/<anything>, 
                 // use routeBuilder.MapGet("hello/{*name}"
                 return context.Response.WriteAsync($"Hi, {name}!");
@@ -40,8 +42,9 @@ namespace RoutingSample
 
             var routes = routeBuilder.Build();
             app.UseRouter(routes);
+            #endregion
 
-            // Show link generation when no routes match.
+            #region snippet_Dictionary
             app.Run(async (context) =>
             {
                 var dictionary = new RouteValueDictionary
@@ -50,14 +53,16 @@ namespace RoutingSample
                     { "id", 123}
                 };
 
-                var vpc = new VirtualPathContext(context, null, dictionary, "Track Package Route");
+                var vpc = new VirtualPathContext(context, null, dictionary, 
+                    "Track Package Route");
                 var path = routes.GetVirtualPath(vpc).VirtualPath;
 
                 context.Response.ContentType = "text/html";
                 await context.Response.WriteAsync("Menu<hr/>");
-                await context.Response.WriteAsync($"<a href='{path}'>Create Package 123</a><br/>");
+                await context.Response.WriteAsync(
+                    $"<a href='{path}'>Create Package 123</a><br/>");
             });
-            // End of app.Run
+            #endregion
         }
     }
 }
