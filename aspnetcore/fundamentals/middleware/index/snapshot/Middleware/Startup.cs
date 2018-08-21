024410f4ff212147f8d12d741a48e0a694bd9c18
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-08-21 13:37:51 -0500

    Middleware topic+sample update

--- aspnetcore/fundamentals/middleware/index/snapshot/Middleware/Startup.cs
+++ aspnetcore/fundamentals/middleware/index/snapshot/Middleware/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
+#region snippet1
 public class Startup
 {
     public void Configure(IApplicationBuilder app)
@@ -12,4 +13,4 @@ public class Startup
         });
     }
 }
-
+#endregion
