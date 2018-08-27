6ac0d79fb0459adfeb925b90f3011abb818b90f6
Rick Anderson <riande@microsoft.com>
2018-08-27 12:31:50 -0700

    2.1 update of secure user data (#7651)
    - [x] Blocked until https://github.com/aspnet/Identity/issues/1813 bits (https://github.com/aspnet/Identity/pull/1832) are surfaced in 2.2 preview
    
    Fixes #6290 
    Fixes #7108
    [Review URL](https://review.docs.microsoft.com/en-us/aspnet/core/security/authorization/secure-data?view=aspnetcore-2.1&branch=pr-en-us-7651)
    
    @blowdart this is an upgrade to 2.1 with the following security changes:
    
    * The project was generated with the 2.1 templates:
    	* *Startup.cs* is based on the 2.1 version.
    	* `AddDefaultIdentity` is used, replaced the template generated Identity 2.0 UI with the Identity 2.1 RCL.
    * I fixed in the previous version where an unauthorized person could see details by hacking the URL. See [new code here](https://github.com/aspnet/Docs/blob/51f4712af66ff563827e8de5d87e7596551d3122/aspnetcore/security/authorization/secure-data/samples/final2.1/Pages/Contacts/Details.cshtml.cs#L34-L44).
    * Uses 2.2 preview-1 because of https://github.com/aspnet/Identity/issues/1813
    
    So it shouldn't require a full audit.
    cc @joeaudette

---
+++ aspnetcore/security/authorization/secure-data/samples/final2.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using ContactManager.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ContactManager
+{
+    #region snippet
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+
+                // requires using Microsoft.Extensions.Configuration;
+                var config = host.Services.GetRequiredService<IConfiguration>();
+                // Set password with the Secret Manager tool.
+                // dotnet user-secrets set SeedUserPW <pw>
+
+                var testUserPw = config["SeedUserPW"];
+                try
+                {
+                    SeedData.Initialize(services, testUserPw).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex.Message, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+    #endregion
+}
