85d768d1ec2a45c323373ee3f2b0089b5b8daee9
Scott Addie <scott.addie@microsoft.com>
2018-09-17 16:16:34 -0500

    Tag Helper Components doc edit pass

---
+++ aspnetcore/mvc/views/tag-helpers/th-components/samples/RazorPagesSample/TagHelpers/AddressStyleTagHelperComponent.cs
@@ -0,0 +1,31 @@
+﻿namespace RazorPagesSample.TagHelpers
+{
+    #region snippet_AddressStyleTagHelperComponentClass
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    public class AddressStyleTagHelperComponent : ITagHelperComponent
+    {
+        private readonly string _style = 
+            @"<link rel=""stylesheet"" href=""/css/address.css"" />";
+
+        public int Order => 1;
+
+        public void Init(TagHelperContext context)
+        {
+        }
+
+        public Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.Equals(context.TagName, "head", 
+                              StringComparison.OrdinalIgnoreCase))
+            {
+                output.PostContent.AppendHtml(_style);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+    #endregion
+}
