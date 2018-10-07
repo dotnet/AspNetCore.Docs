455e253bcdbc2796fcc329e81b6235bf1aab1bc9
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2018-10-07 20:29:18 +0300

    Use CreateWebHostBuilder

--- aspnetcore/fundamentals/websockets/sample/Program.cs
+++ aspnetcore/fundamentals/websockets/sample/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace EchoApp
@@ -11,14 +7,11 @@ namespace EchoApp
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+              .UseStartup<Startup>();
     }
 }
