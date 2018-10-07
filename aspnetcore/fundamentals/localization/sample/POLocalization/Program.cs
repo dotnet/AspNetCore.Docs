b0e9ee31c5cb8abcca573994448ab828ebf84cc8
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2018-10-07 06:09:16 +0300

    Use  CreateWebHostBuilder in localization samples (#8882)

--- aspnetcore/fundamentals/localization/sample/POLocalization/Program.cs
+++ aspnetcore/fundamentals/localization/sample/POLocalization/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace POLocalization
 {
@@ -14,12 +7,11 @@ namespace POLocalization
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>();
     }
 }
