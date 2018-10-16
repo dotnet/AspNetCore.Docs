50863ad503faa71911446eca9f759a2085404cc7
Tom Dykstra <tdykstra@microsoft.com>
2018-10-16 12:16:57 -0700

    Logging article updates (#8908)
    Fixes #5161
    Fixes #8338 
    Fixes #8620
    Fixes #8803 
    Fixes #8207
    
    [Internal review link](https://review.docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/index?view=aspnetcore-2.2&branch=pr-en-us-8908)

---
+++ aspnetcore/fundamentals/logging/index/samples/2.x/TodoApiSample/Pages/About.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApiSample.Pages
+{
+    #region snippet_LoggerDI
+    public class AboutModel : PageModel
+    {
+        private readonly ILogger _logger;
+
+        public AboutModel(ILogger<AboutModel> logger)
+        {
+            _logger = logger;
+        }
+        #endregion
+        public string Message { get; set; }
+
+        #region snippet_CallLogMethods
+        public void OnGet()
+        {
+            Message = $"About page visited at {DateTime.UtcNow.ToLongTimeString()}";
+            _logger.LogInformation("Message displayed: {Message}", Message);
+        }
+        #endregion
+    }
+}
