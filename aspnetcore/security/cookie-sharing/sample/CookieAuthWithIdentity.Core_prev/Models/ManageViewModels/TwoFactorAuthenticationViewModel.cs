6b9a33aab3047a946ba3b52f1ac115ea8764a3a8
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-10-18 17:13:39 -0500

    Remove previous cookie sharing sample app

--- aspnetcore/security/cookie-sharing/sample/CookieAuthWithIdentity.Core_prev/Models/ManageViewModels/TwoFactorAuthenticationViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CookieAuthWithIdentityCore.Models.ManageViewModels
-{
-    public class TwoFactorAuthenticationViewModel
-    {
-        public bool HasAuthenticator { get; set; }
-        public int RecoveryCodesLeft { get; set; }
-        public bool Is2faEnabled { get; set; }
-    }
-}
