2fe9ca8744b85dfceeb9b51a456f1ae5804b351c
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-10-18 17:35:06 -0500

    Remove previous Identity Core sample app (#9115)
    Fixes #9114 
    
    We're going to keep the cookie sharing auth samples over here on the docs repo and **not** move them to the AuthSamples repo. We don't need to keep the previous Identity Core sample (and it's not linked to anything).

--- aspnetcore/security/cookie-sharing/sample/CookieAuthWithIdentity.Core_prev/Models/ManageViewModels/RemoveLoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CookieAuthWithIdentityCore.Models.ManageViewModels
-{
-    public class RemoveLoginViewModel
-    {
-        public string LoginProvider { get; set; }
-        public string ProviderKey { get; set; }
-    }
-}
