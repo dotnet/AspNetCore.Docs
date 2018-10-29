c5f0d8d2ad499e614adb4e78ef249d25d2f0fb49
Danilo Dantas <dannevesdantas@outlook.com>
2018-10-29 00:47:46 -0300

    Updating MvcMovieContext to seed data by overriding the OnModelCreating method
    Seeding data by overriding the OnModelCreating method of the MvcMovieContext class.

--- aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie21/Data/MvcMovieContextRating.cs
+++ aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie21/Data/MvcMovieContextRating.cs
@@ -1,7 +1,4 @@
-﻿//#define MvcMovieContext
-#if MvcMovieContext
-#region snippet_1
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -21,22 +18,25 @@ namespace MvcMovie.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Movie>().HasData(
+            #region snippet1
                 new Movie
                 {
                     ID = 1,
                     Title = "When Harry Met Sally",
-                    ReleaseDate = DateTime.Parse("1989-1-11"),
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
-                    Price = 7.99M
+                    Price = 7.99M,
+                    Rating = "R"
                 },
-
+            #endregion
                 new Movie
                 {
                     ID = 2,
                     Title = "Ghostbusters ",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
-                    Price = 8.99M
+                    Price = 8.99M,
+                    Rating = "G"
                 },
 
                 new Movie
@@ -45,7 +45,8 @@ namespace MvcMovie.Models
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
-                    Price = 9.99M
+                    Price = 9.99M,
+                    Rating = "G"
                 },
 
                 new Movie
@@ -54,11 +55,10 @@ namespace MvcMovie.Models
                     Title = "Rio Bravo",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
-                    Price = 3.99M
+                    Price = 3.99M,
+                    Rating = "NA"
                 }
             );
         }
     }
 }
-#endregion
-#endif
