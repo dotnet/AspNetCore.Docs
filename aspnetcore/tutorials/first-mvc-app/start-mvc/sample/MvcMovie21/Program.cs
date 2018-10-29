3b4de984153c2fa8f8d45bda57a8e3c28d809b6a
Danilo Dantas <dannevesdantas@outlook.com>
2018-10-29 01:08:17 -0300

    Removing references to the SeedData class from Program.cs
    With application data being sown at MvcMovieContext, calling the SeedData.Initialize() in Program.cs is not required anymore.

--- aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie21/Program.cs
+++ aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie21/Program.cs
@@ -13,26 +13,7 @@ namespace MvcMovie
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    var context = services.GetRequiredService<MvcMovieContext>();
-                    context.Database.Migrate();
-                    SeedData.Initialize(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
-            }
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
