d14ed9186188188c180c3b9137e589857fd612e9
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-10-30 15:09:41 -0500

    Windows Services updates

--- aspnetcore/host-and-deploy/windows-service/samples/2.x/AspNetCoreService/Startup.cs
+++ aspnetcore/host-and-deploy/windows-service/samples/2.x/AspNetCoreService/Startup.cs
@@ -19,12 +19,6 @@ namespace AspNetCoreService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
