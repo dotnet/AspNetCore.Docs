cb407a29a128ca81606decc77752ea740cfeef93
fiyazbinhasan <fiyazhasan@live.com>
2018-11-04 06:02:27 +0600

    better formatting

--- aspnetcore/tutorials/first-web-api/samples/2.1/TodoApi/wwwroot/site.js
+++ aspnetcore/tutorials/first-web-api/samples/2.1/TodoApi/wwwroot/site.js
@@ -1,117 +1,133 @@
-﻿const uri = 'api/todo';
+﻿const uri = "api/todo";
 let todos = null;
 function getCount(data) {
-    const el = $('#counter');
-    let name = 'to-do';
-    if (data) {
-        if (data > 1) {
-            name = 'to-dos';
-        }
-        el.text(data + ' ' + name);
-    } else {
-        el.text('No ' + name);
+  const el = $("#counter");
+  let name = "to-do";
+  if (data) {
+    if (data > 1) {
+      name = "to-dos";
     }
+    el.text(data + " " + name);
+  } else {
+    el.text("No " + name);
+  }
 }
 
-$(document).ready(function () {
-    getData();
+$(document).ready(function() {
+  getData();
 });
 
 function getData() {
-    $.ajax({
-        type: 'GET',
-        url: uri,
-        cache: false,
-        success: function (data) {
-            var tBody = $('#todos');
+  $.ajax({
+    type: "GET",
+    url: uri,
+    cache: false,
+    success: function(data) {
+      var tBody = $("#todos");
 
-            $(tBody).empty();
+      $(tBody).empty();
 
-            getCount(data.length);
+      getCount(data.length);
 
-            $.each(data, function (key, item) {
-                var tr = $('<tr></tr>')
-                    .append($('<td></td>')
-                        .append($('<input/>', { type: 'checkbox', disabled: true, checked: item.isComplete })))
-                    .append($('<td></td>')
-                        .text(item.name))
-                    .append($('<td></td>')
-                        .append('<button>Edit</button>').on("click", function () { editItem(item.id); }))
-                    .append($('<td></td>')
-                        .append('<button>Delete</button>').on("click", function () { deleteItem(item.id); }));
+      $.each(data, function(key, item) {
+        var tr = $("<tr></tr>")
+          .append(
+            $("<td></td>").append(
+              $("<input/>", {
+                type: "checkbox",
+                disabled: true,
+                checked: item.isComplete
+              })
+            )
+          )
+          .append($("<td></td>").text(item.name))
+          .append(
+            $("<td></td>").append(
+              $("<button>Edit</button>").on("click", function() {
+                editItem(item.id);
+              })
+            )
+          )
+          .append(
+            $("<td></td>").append(
+              $("<button>Delete</button>").on("click", function() {
+                deleteItem(item.id);
+              })
+            )
+          );
 
-                tr.appendTo(tBody);
-            });
+        tr.appendTo(tBody);
+      });
 
-            todos = data;
-        }
-    });
+      todos = data;
+    }
+  });
 }
 
 function addItem() {
-    const item = {
-        'name': $('#add-name').val(),
-        'isComplete': false
-    };
+  const item = {
+    name: $("#add-name").val(),
+    isComplete: false
+  };
 
-    $.ajax({
-        type: 'POST',
-        accepts: 'application/json',
-        url: uri,
-        contentType: 'application/json',
-        data: JSON.stringify(item),
-        error: function (jqXHR, textStatus, errorThrown) {
-            alert('Something went wrong!');
-        },
-        success: function (result) {
-            getData();
-            $('#add-name').val('');
-        }
-    });
+  $.ajax({
+    type: "POST",
+    accepts: "application/json",
+    url: uri,
+    contentType: "application/json",
+    data: JSON.stringify(item),
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert("Something went wrong!");
+    },
+    success: function(result) {
+      getData();
+      $("#add-name").val("");
+    }
+  });
 }
 
 function deleteItem(id) {
-    $.ajax({
-        url: uri + '/' + id,
-        type: 'DELETE',
-        success: function (result) {
-            getData();
-        }
-    });
+  $.ajax({
+    url: uri + "/" + id,
+    type: "DELETE",
+    success: function(result) {
+      getData();
+    }
+  });
 }
 
 function editItem(id) {
-    $.each(todos, function (key, item) {
-        if (item.id === id) {
-            $('#edit-name').val(item.name);
-            $('#edit-id').val(item.id);
-            $('#edit-isComplete')[0].checked = item.isComplete;
-        }
-    });
-    $('#spoiler').css({ 'display': 'block' });
+  $.each(todos, function(key, item) {
+    if (item.id === id) {
+      $("#edit-name").val(item.name);
+      $("#edit-id").val(item.id);
+      $("#edit-isComplete")[0].checked = item.isComplete;
+    }
+  });
+  $("#spoiler").css({ display: "block" });
 }
 
-$('.my-form').on('submit', function () {
-    const item = {
-        'name': $('#edit-name').val(),
-        'isComplete': $('#edit-isComplete').is(':checked'),
-        'id': $('#edit-id').val()
-    };
+$(".my-form").on("submit", function() {
+  const item = {
+    name: $("#edit-name").val(),
+    isComplete: $("#edit-isComplete").is(":checked"),
+    id: $("#edit-id").val()
+  };
 
-    $.ajax({
-        url: uri + '/' + $('#edit-id').val(),
-        type: 'PUT',
-        accepts: 'application/json',
-        contentType: 'application/json',
-        data: JSON.stringify(item),
-        success: function (result) {
-            getData();
-        }
-    });
-    closeInput();
-    return false;
+  $.ajax({
+    url: uri + "/" + $("#edit-id").val(),
+    type: "PUT",
+    accepts: "application/json",
+    contentType: "application/json",
+    data: JSON.stringify(item),
+    success: function(result) {
+      getData();
+    }
+  });
+  closeInput();
+  return false;
 });
 
 function closeInput() {
-    $('#spoiler').css({ 'display': 'none' });
+  $("#spoiler").css({ display: "none" });
 }
