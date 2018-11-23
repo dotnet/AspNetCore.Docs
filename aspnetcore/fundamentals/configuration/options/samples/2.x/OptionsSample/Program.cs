78a70539de2fc1fc4016e8435314167e2859ce50
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-11-23 14:25:31 -0600

    Emphasize IOptionsMonitor in Options topic

---
+++ aspnetcore/fundamentals/configuration/options/samples/2.x/OptionsSample/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SampleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+            }
+}
