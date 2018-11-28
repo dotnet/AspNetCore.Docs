dda13500cf30d835396e36bd2972a2377acdde3c
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-11-27 20:24:13 -0600

    Enhance WS topic with FDD scenario (#9667)
    * Enhance WS topic with FDD scenario
    
    * React to feedback
    
    * React to feedback
    
    * React to feedback
    
    Update

--- aspnetcore/host-and-deploy/windows-service/samples/2.x/AspNetCoreService/Pages/Index.cshtml.cs
+++ aspnetcore/host-and-deploy/windows-service/samples/2.x/AspNetCoreService/Pages/Index.cshtml.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace AspNetCoreService.Pages
+namespace SampleApp.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+            _logger.LogInformation("Logged from the IndexModel.OnGet method at {TIME}", DateTimeOffset.Now);
         }
     }
 }
