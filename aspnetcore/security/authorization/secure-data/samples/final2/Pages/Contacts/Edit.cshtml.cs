5d633e4d85b3fd4325ec11a01497d3d0702f0c47
Rick Anderson <riande@microsoft.com>
2018-12-06 17:22:52 -1000

    Update Edit.cshtml.cs (#9850)
    * Update Edit.cshtml.cs
    
    * Update Edit.cshtml.cs

--- aspnetcore/security/authorization/secure-data/samples/final2/Pages/Contacts/Edit.cshtml.cs
+++ aspnetcore/security/authorization/secure-data/samples/final2/Pages/Contacts/Edit.cshtml.cs
@@ -55,7 +55,6 @@ namespace ContactManager.Pages.Contacts
 
             // Fetch Contact from DB to get OwnerID.
             var contact = await Context
-                .Contact.AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ContactId == id);
 
             if (contact == null)
@@ -72,8 +71,6 @@ namespace ContactManager.Pages.Contacts
             }
 
             Contact.OwnerID = contact.OwnerID;
-
-            Context.Attach(Contact).State = EntityState.Modified;
 
             if (contact.Status == ContactStatus.Approved)
             {
