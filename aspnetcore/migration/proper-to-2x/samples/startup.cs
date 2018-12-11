bf366ba17abae761ad2cbb2e8211ca441e731409
Scott Addie <10702007+scottaddie@users.noreply.github.com>
2018-12-11 16:58:40 -0600

    Remove comments from Startup.cs

--- aspnetcore/migration/proper-to-2x/samples/startup.cs
+++ aspnetcore/migration/proper-to-2x/samples/startup.cs
@@ -1,4 +1,3 @@
-// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 {
     if (env.IsDevelopment())
@@ -7,7 +6,6 @@ public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     }
     else
     {
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
     }
 
