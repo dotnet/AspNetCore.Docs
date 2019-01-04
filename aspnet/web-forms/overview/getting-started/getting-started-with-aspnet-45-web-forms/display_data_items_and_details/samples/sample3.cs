abab1fce231c3951accc07627022a6be543dbf1c
pgoldman5699 <v-pagold@microsoft.com>
2019-01-04 06:19:04 -0800

    edits

--- aspnet/web-forms/overview/getting-started/getting-started-with-aspnet-45-web-forms/display_data_items_and_details/samples/sample3.cs
+++ aspnet/web-forms/overview/getting-started/getting-started-with-aspnet-45-web-forms/display_data_items_and_details/samples/sample3.cs
@@ -6,25 +6,36 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using WingtipToys.Models;
 using System.Web.ModelBinding;
+using System.Web.Routing;
 
 namespace WingtipToys
 {
-    public partial class ProductList : System.Web.UI.Page
+  public partial class ProductList : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
 
-        }
-
-        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
-        {
-            var _db = new WingtipToys.Models.ProductContext();
-                IQueryable<Product> query = _db.Products;
-                if (categoryId.HasValue && categoryId > 0)
-                {
-                    query = query.Where(p => p.CategoryID == categoryId);
-                }
-                return query;
-        }
     }
+
+    public IQueryable<Product> GetProducts(
+                        [QueryString("id")] int? categoryId,
+                        [RouteData] string categoryName)
+    {
+      var _db = new WingtipToys.Models.ProductContext();
+      IQueryable<Product> query = _db.Products;
+
+      if (categoryId.HasValue && categoryId > 0)
+      {
+        query = query.Where(p => p.CategoryID == categoryId);
+      }
+
+      if (!String.IsNullOrEmpty(categoryName))
+      {
+        query = query.Where(p =>
+                            String.Compare(p.Category.CategoryName,
+                            categoryName) == 0);
+      }
+      return query;
+    }
+  }
 }
--- aspnet/web-forms/overview/getting-started/getting-started-with-aspnet-45-web-forms/display_data_items_and_details/samples/sample6.cs
+++ aspnet/web-forms/overview/getting-started/getting-started-with-aspnet-45-web-forms/display_data_items_and_details/samples/sample6.cs
@@ -9,26 +9,33 @@ using System.Web.ModelBinding;
 
 namespace WingtipToys
 {
-    public partial class ProductDetails : System.Web.UI.Page
+  public partial class ProductDetails : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
 
-        }
-
-        public IQueryable<Product> GetProduct([QueryString("productID")] int? productId)
-        {
-            var _db = new WingtipToys.Models.ProductContext();
-                IQueryable<Product> query = _db.Products;
-                if (productId.HasValue && productId > 0)
-                {
-                    query = query.Where(p => p.ProductID == productId);
-                }
-                else
-                {
-                    query = null;
-                }
-                return query;
-        }
     }
+
+    public IQueryable<Product> GetProduct(
+                        [QueryString("ProductID")] int? productId,
+                        [RouteData] string productName)
+    {
+      var _db = new WingtipToys.Models.ProductContext();
+      IQueryable<Product> query = _db.Products;
+      if (productId.HasValue && productId > 0)
+      {
+        query = query.Where(p => p.ProductID == productId);
+      }
+      else if (!String.IsNullOrEmpty(productName))
+      {
+        query = query.Where(p =>
+                  String.Compare(p.ProductName, productName) == 0);
+      }
+      else
+      {
+        query = null;
+      }
+      return query;
+    }
+  }
 }
