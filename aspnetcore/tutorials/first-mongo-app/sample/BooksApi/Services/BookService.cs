2a2cb9d5093f2ed810bf217c2d760c7f6cbd5373
Scott Addie <10702007+scottaddie@users.noreply.github.com>
2019-01-22 10:14:08 -0600

    Remove unused using statement

--- aspnetcore/tutorials/first-mongo-app/sample/BooksApi/Services/BookService.cs
+++ aspnetcore/tutorials/first-mongo-app/sample/BooksApi/Services/BookService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using BooksApi.Models;
 using Microsoft.Extensions.Configuration;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace BooksApi.Services
