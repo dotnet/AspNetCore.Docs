1c63b5b4fdb3c53fd0016547a1c36c2031772de0
Rick Anderson <riande@microsoft.com>
2019-02-04 17:56:54 -1000

    work

--- aspnetcore/security/authentication/identity/sample/WebApp1/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ aspnetcore/security/authentication/identity/sample/WebApp1/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -73,8 +73,10 @@ namespace WebApp1.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                // To enable password failures to trigger account lockout, 
+                // set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, 
+                    Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
