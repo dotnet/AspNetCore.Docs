2bc40e8fa67932306729f5c1fd42575c4ef9f72c
Kirk Larkin <serpent5@pm.me>
2019-02-15 21:07:06 +0000

    Copied Services/Requirements/* and Services/Handlers/*.

---
+++ aspnetcore/security/authorization/policies/samples/PoliciesAuthApp1/Services/Handlers/BadgeEntryHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace PoliciesAuthApp1.Services.Handlers
+{
+    #region snippet_BadgeEntryHandlerClass
+    using Microsoft.AspNetCore.Authorization;
+    using PoliciesAuthApp1.Services.Requirements;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    public class BadgeEntryHandler : AuthorizationHandler<BuildingEntryRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                                                       BuildingEntryRequirement requirement)
+        {
+            if (context.User.HasClaim(c => c.Type == ClaimTypes.BadgeId &&
+                                           c.Issuer == "http://microsoftsecurity"))
+            {
+                context.Succeed(requirement);
+            }
+
+            //TODO: Use the following if targeting a version of
+            //.NET Framework older than 4.6:
+            //      return Task.FromResult(0);
+            return Task.CompletedTask;
+        }
+    }
+    #endregion
+}
---
+++ aspnetcore/security/authorization/policies/samples/PoliciesAuthApp1/Services/Requirements/EditPermission.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace PoliciesAuthApp1.Services.Requirements
+{
+    public class EditPermission : IAuthorizationRequirement
+    {
+        // Code omitted for brevity
+    }
+}
