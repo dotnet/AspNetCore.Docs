3fd8d9d0aca1c8ea29e17c136566db1a852a8094
Rick Anderson <riande@microsoft.com>
2019-02-26 10:50:04 -1000

    CORS update

---
+++ aspnetcore/security/cors/sample/Cors/WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    #region snippet
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            switch (id)
+            {
+                case 1:
+                    return "1st Val";
+                case 2:
+                    return "2nd val";
+                default:
+                    return NotFound();
+            }
+        }
+       
+    }
+    #endregion
+}
---
+++ aspnetcore/security/cors/sample/Cors/WebAPI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<StartupTest>();
+                //.UseStartup<StartupTestMultiPolicy>();
+    }
+}
