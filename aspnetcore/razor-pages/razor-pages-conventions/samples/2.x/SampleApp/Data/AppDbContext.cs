8a15a6e94e67a021b24cd8af4b75df224577fc61
Luke Latham <1622880+guardrex@users.noreply.github.com>
2019-02-27 15:24:58 -0600

    RP route and app conventions update (#11194)
    Fixes #6801 
    
    * Add example code for `IPageHandlerModelConvention` to the sample app (but it's not wired up to do anything really ... just shows how to implement it generally).
    * Take sample app to 2.2.
    * Drop the *Replace the default page app model provider* and sample code for it per instructions from engineering (see the issue).
    * Take topic to 2.1+ (shed 2.0 content).
    * XREF links (I really suspect that a VSC Extension could do 90% of the conversion work ... I should consider creating it.)

---
+++ aspnetcore/razor-pages/razor-pages-conventions/samples/2.x/SampleApp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}
