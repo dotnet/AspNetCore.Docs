250f514a971830cc9201d5c442ae8e3a15b58740
谭九鼎 <109224573@qq.com>
2019-03-06 22:54:12 +0800

    Course -> Courses

--- aspnetcore/data/ef-rp/intro/samples/cu21/Data/SchoolContext.cs
+++ aspnetcore/data/ef-rp/intro/samples/cu21/Data/SchoolContext.cs
@@ -15,7 +15,7 @@ namespace ContosoUniversity.Models
 
         public DbSet<Student> Student { get; set; }
         public DbSet<Enrollment> Enrollment { get; set; }
-        public DbSet<Course> Course { get; set; }
+        public DbSet<Course> Courses { get; set; }
     }
 }
 #endregion
