b4eaf279dc9fd9b97d69af58e684564609e757a0
Luke Latham <1622880+guardrex@users.noreply.github.com>
2019-03-13 15:21:31 -0500

    Razor Components: Build first Preview3 updates

--- aspnetcore/tutorials/build-your-first-razor-components-app/samples/3.x/RazorComponents/Program.cs
+++ aspnetcore/tutorials/build-your-first-razor-components-app/samples/3.x/RazorComponents/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace RazorComponents.Server
+namespace RazorComponents
 {
     public class Program
     {
