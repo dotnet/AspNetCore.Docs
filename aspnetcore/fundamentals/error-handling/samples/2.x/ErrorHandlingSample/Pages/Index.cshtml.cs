5f5bd93f8f15ad2f45921aa5e2abae14272dbb7a
Tom Dykstra <tdykstra@microsoft.com>
2019-04-09 17:30:36 -0700

    Change sample app to RP (#11899)

---
+++ aspnetcore/fundamentals/error-handling/samples/2.x/ErrorHandlingSample/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ErrorHandlingSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            if (HttpContext.Request.Query.ContainsKey("throw"))
+            {
+                throw new FileNotFoundException("File not found exception thrown in index.chtml");
+            }
+        }
+    }
+}
