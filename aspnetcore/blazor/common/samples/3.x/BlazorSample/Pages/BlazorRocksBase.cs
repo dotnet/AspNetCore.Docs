8c2942d6150797082749c9f5a942103b7411d87c
Luke Latham <1622880+guardrex@users.noreply.github.com>
2019-04-18 20:12:34 -0500

    Revert "Fix BlazorRocksBase code-behind experience example"
    This reverts commit e23d59fa1de199dcdb7a5d94a44233b2fd75007c.

--- aspnetcore/blazor/common/samples/3.x/BlazorSample/Pages/BlazorRocksBase.cs
+++ aspnetcore/blazor/common/samples/3.x/BlazorSample/Pages/BlazorRocksBase.cs
@@ -1,10 +1,9 @@
+/* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 using Microsoft.AspNetCore.Components;
 
-namespace BlazorSample
+public class BlazorRocksBase : ComponentBase
 {
-    public class BlazorRocksBase : ComponentBase
-    {
-        public string BlazorRocksText { get; set; } = 
-            "Blazor rocks the browser!";
-    }
+    public string BlazorRocksText { get; set; } = 
+        "Blazor rocks the browser!";
 }
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */
