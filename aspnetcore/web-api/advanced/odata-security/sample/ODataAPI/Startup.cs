6b7a25bca04bacea6cc255825fc72fdb3bdc1a48
hamed Mohammedi <hammedmohamadi@gmail.com>
2019-04-24 10:40:53 +0430

    todo context

--- aspnetcore/web-api/advanced/odata-security/sample/ODataAPI/Startup.cs
+++ aspnetcore/web-api/advanced/odata-security/sample/ODataAPI/Startup.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ODataAPI.Models;
 
 namespace ODataAPI
 {
@@ -26,6 +28,8 @@ namespace ODataAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddOData();
         }
