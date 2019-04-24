39facdfc1af371f0dab58b66c08903d157ea0586
hamed mohammadi <hammedmohamadi@gmail.com>
2019-04-24 21:53:28 +0430

    Odata-security (#12137)
    * Adding Project files
    
    * Updating startup cs
    
    * adding todoitem
    
    * todo context
    
    * TodoController
    
    * OData-OrderByValidator
    
    * OData-MyEnableQuery
    
    * Update controller

---
+++ aspnetcore/web-api/advanced/odata-security/sample/ODataAPI/Models/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODataAPI.Models
+{
+    public class TodoItem
+    {
+        #region OldProps
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        #endregion
+
+        #region  NewProps
+        public string Type { get; set; }
+        public int priority { get; set; }
+        public System.DateTime DueDate { get; set; }
+        #endregion
+    }
+}
