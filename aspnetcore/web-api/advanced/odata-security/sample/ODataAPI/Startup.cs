b7c96a066c1ed6a41de499ad54380b7283bdedab
hamed Mohammedi <hammedmohamadi@gmail.com>
2019-04-24 10:32:40 +0430

    Updating startup cs

--- aspnetcore/web-api/advanced/odata-security/sample/ODataAPI/Startup.cs
+++ aspnetcore/web-api/advanced/odata-security/sample/ODataAPI/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace ODataAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddOData();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +44,11 @@ namespace ODataAPI
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvc(routeBuilder =>
+            {
+                routeBuilder.EnableDependencyInjection();
+                routeBuilder.Select().Count().OrderBy().Filter();
+            });
         }
     }
 }
