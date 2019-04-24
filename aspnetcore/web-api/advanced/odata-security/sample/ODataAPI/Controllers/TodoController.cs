6ce322adca3fd62f16a29eaa1537ec65b467cc57
hamed Mohammedi <hammedmohamadi@gmail.com>
2019-04-24 10:46:11 +0430

    TodoController

---
+++ aspnetcore/web-api/advanced/odata-security/sample/ODataAPI/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ODataAPI.Models;
+
+namespace ODataAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext context;
+        public TodoController(TodoContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IQueryable<TodoItem>> GetTodoItems()
+        {
+            return this.context.TodoItems;
+        }
+    }
+}
