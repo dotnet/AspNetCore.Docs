81621f96886963f6a2d5a4c874539d0c9f972198
hamed Mohammedi <hammedmohamadi@gmail.com>
2019-04-24 11:27:45 +0430

    Update controller

--- aspnetcore/web-api/advanced/odata-security/sample/ODataAPI/Controllers/TodoController.cs
+++ aspnetcore/web-api/advanced/odata-security/sample/ODataAPI/Controllers/TodoController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.OData;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ODataAPI.Models;
+using ODataAPI.ODataAttribute;
 
 namespace ODataAPI.Controllers
 {
@@ -20,9 +21,32 @@ namespace ODataAPI.Controllers
         }
 
         [HttpGet]
+        [MyEnableQuery()]
         public ActionResult<IQueryable<TodoItem>> GetTodoItems()
         {
             return this.context.TodoItems;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await this.context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            this.context.TodoItems.Add(item);
+            await this.context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
         }
     }
 }
