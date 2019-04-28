ca46be6c10df5c1c6bc8788e66dc53e699904720
hamed Mohammedi <hammedmohamadi@gmail.com>
2019-04-29 00:30:58 +0430

    validator

--- aspnetcore/web-api/advanced/odata-advanced/sample/odata-expand/Program.cs
+++ aspnetcore/web-api/advanced/odata-advanced/sample/odata-expand/Program.cs
@@ -107,7 +107,7 @@ namespace ContosoUniversity
     public class Program
     {
         public static void Main(string[] args)
-        {
+        {           
             var host = CreateWebHostBuilder(args).Build();
 
             using (var scope = host.Services.CreateScope())
