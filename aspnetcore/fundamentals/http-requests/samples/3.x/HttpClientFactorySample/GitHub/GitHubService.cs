fcfa1c60d3baad2796cc3ff91de389c3468d9ca9
Scott Addie <10702007+scottaddie@users.noreply.github.com>
2019-05-10 20:22:28 -0500

    Address removal of WebApi.Client from shared framework (#12371)
    * Address removal of WebApi.Client from shared framework
    
    * Update IHttpClientFactory sample code
    
    * IHttpClientFactory doc updates

---
+++ aspnetcore/fundamentals/http-requests/samples/3.x/HttpClientFactorySample/GitHub/GitHubService.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientFactorySample.GitHub
+{
+    /// <summary>
+    /// Exposes methods to return GitHub API data
+    /// </summary>
+    #region snippet1
+    public class GitHubService
+    {
+        public HttpClient Client { get; }
+
+        public GitHubService(HttpClient client)
+        {
+            client.BaseAddress = new Uri("https://api.github.com/");
+            // GitHub API versioning
+            client.DefaultRequestHeaders.Add("Accept", 
+                "application/vnd.github.v3+json");
+            // GitHub requires a user-agent
+            client.DefaultRequestHeaders.Add("User-Agent", 
+                "HttpClientFactory-Sample");
+
+            Client = client;
+        }
+
+        public async Task<IEnumerable<GitHubIssue>> GetAspNetDocsIssues()
+        {
+            var response = await Client.GetAsync(
+                "/repos/aspnet/AspNetCore.Docs/issues?state=open&sort=created&direction=desc");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content
+                .ReadAsAsync<IEnumerable<GitHubIssue>>();
+
+            return result;
+        }
+    }
+    #endregion
+}
