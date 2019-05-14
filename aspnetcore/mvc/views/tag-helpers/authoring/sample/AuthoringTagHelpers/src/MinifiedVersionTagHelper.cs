f603e04127646548e6733e3136c8c623e2622838
Rick Anderson <riande@microsoft.com>
2019-05-14 07:27:39 -1000

    update Author TH (#12394)
    * update Author TH
    
    * update Author TH
    
    * update Author TH
    
    * update Author TH
    
    * update Author TH
    
    * Update aspnetcore/mvc/views/tag-helpers/authoring.md
    
    Co-Authored-By: reZach <reZach@users.noreply.github.com>
    
    * Update aspnetcore/mvc/views/tag-helpers/authoring.md
    
    Co-Authored-By: Luke Latham <1622880+guardrex@users.noreply.github.com>
    
    * Update authoring.md

---
+++ aspnetcore/mvc/views/tag-helpers/authoring/sample/AuthoringTagHelpers/src/MinifiedVersionTagHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Threading.Tasks;
+
+namespace AuthoringTagHelpers.TagHelpers
+{
+    /// <summary>
+    ///     The minified-partial tag helper loads
+    ///     minified partials depending on the value
+    ///     of ASPNETCORE_ENVIRONMENT.
+    /// </summary>
+    #region snippet
+    public class MinifiedVersionPartialTagHelper : PartialTagHelper
+        {
+            public MinifiedVersionPartialTagHelper(ICompositeViewEngine viewEngine, 
+                                    IViewBufferScope viewBufferScope)
+                                   : base(viewEngine, viewBufferScope)
+            {
+
+            }
+
+            public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+            {
+                // Append ".min" to load the minified partial view.
+                if (!IsDevelopment())
+                {
+                    Name += ".min";
+                }
+
+                return base.ProcessAsync(context, output);
+            }
+
+            private bool IsDevelopment()
+            {
+                return Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") 
+                                                     == EnvironmentName.Development;
+            }
+        }
+    #endregion
+}
