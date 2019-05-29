5870e42ecb5b3adde3d7d691bf9c1969c9808ff6
Rick Anderson <riande@microsoft.com>
2019-05-29 07:49:16 -1000

    Combine gRPC tutorials (#12522)
    * Combine gRPC tutorials
    
    * Combine gRPC tutorials
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * react to feedback
    
    * react to feedback
    
    * react to feedback
    
    * react to feedback

--- aspnetcore/tutorials/grpc/grpc-start/sample/GrpcGreeter/Startup.cs
+++ aspnetcore/tutorials/grpc/grpc-start/sample/GrpcGreeter/Startup.cs
@@ -9,14 +9,16 @@ using Microsoft.Extensions.Hosting;
 
 namespace GrpcGreeter
 {
-    #region snippet
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -34,5 +36,4 @@ namespace GrpcGreeter
             });
         }
     }
-    #endregion
 }
