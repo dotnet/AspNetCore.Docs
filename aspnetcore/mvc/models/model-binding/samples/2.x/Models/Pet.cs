fb4f991c8742a1e8066a6b248fa0ddb2e68f3faa
Tom Dykstra <tdykstra@microsoft.com>
2019-06-06 07:43:46 -0700

    Model binding doc updates (#12462)
    * draft of rewrite
    
    * show how to make cusom val prov first
    
    * correct errors
    
    * more corrections
    
    * fix typo
    
    * more small fixes
    
    * dict int,string to string,string
    
    * feedback
    
    * feedback
    
    * fix links
    
    * feedback
    
    * feedback
    
    * clean up sample app
    
    * convert inline code to snippet refs
    
    * acrolinx
    
    * fix snippet
    
    * changes that were accidentally not saved
    
    * proofread fixes
    
    * corrections
    
    * use asp-for
    
    * fix date
    
    * feedback

---
+++ aspnetcore/mvc/models/model-binding/samples/2.x/Models/Pet.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiSample.DataAccess.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+
+        public string Breed { get; set; }
+        public string Name { get; set; }
+
+        public PetType PetType { get; set; }
+    }
+
+    public enum PetType
+    {
+        Dog = 0,
+        Cat = 1
+    }
+}
