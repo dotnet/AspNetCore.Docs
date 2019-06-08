ee133378dabf2af0d59efe88ff312b9c7d79cb07
James Newton-King <james@newtonking.com>
2019-06-08 22:34:43 +1200

    Revert port

--- aspnetcore/tutorials/grpc/grpc-start/sample/GrpcGreeterClient/Program.cs
+++ aspnetcore/tutorials/grpc/grpc-start/sample/GrpcGreeterClient/Program.cs
@@ -17,7 +17,7 @@ namespace GrpcGreeterClient
                 true);
             var httpClient = new HttpClient();
             // The port number(50051) must match the port of the gRPC server.
-            httpClient.BaseAddress = new Uri("http://localhost:49989");
+            httpClient.BaseAddress = new Uri("http://localhost:50051");
             var client = GrpcClient.Create<Greeter.GreeterClient>(httpClient);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
