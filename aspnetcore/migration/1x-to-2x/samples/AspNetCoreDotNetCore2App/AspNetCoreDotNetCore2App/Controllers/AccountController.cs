2efb4554f8f659be97ee7cd5dd6143b871b330a5
Scott Addie <10702007+scottaddie@users.noreply.github.com>
2019-06-18 13:29:24 -0500

    Mention namespace changes in Identity 2.0 (#12868)
    * Mention namespace changes in Identity 2.0
    
    * More edits

--- aspnetcore/migration/1x-to-2x/samples/AspNetCoreDotNetCore2App/AspNetCoreDotNetCore2App/Controllers/AccountController.cs
+++ aspnetcore/migration/1x-to-2x/samples/AspNetCoreDotNetCore2App/AspNetCoreDotNetCore2App/Controllers/AccountController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+#region snippet_AuthenticationImport
 using Microsoft.AspNetCore.Authentication;
+#endregion
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
