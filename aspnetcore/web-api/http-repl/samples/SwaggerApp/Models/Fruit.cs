3683fc4caea0ea12cce07b27216365defd50594e
Scott Addie <scott.addie@microsoft.com>
2019-06-27 15:25:06 -0500

    HTTP REPL doc patch

---
+++ aspnetcore/web-api/http-repl/samples/SwaggerApp/Models/Fruit.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SwaggerApp.Models
+{
+    public class Fruit
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
