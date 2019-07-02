dddbf2d1768f7c766ca969f6cd730c58d220bbda
James Newton-King <james@newtonking.com>
2019-07-03 11:49:07 +1200

    Update with latest template and packages

--- aspnetcore/tutorials/grpc/grpc-start/sample/GrpcGreeterClient/Program.cs
+++ aspnetcore/tutorials/grpc/grpc-start/sample/GrpcGreeterClient/Program.cs
@@ -13,8 +13,8 @@ namespace GrpcGreeterClient
         static async Task Main(string[] args)
         {
             var httpClient = new HttpClient();
-            // The port number(50051) must match the port of the gRPC server.
-            httpClient.BaseAddress = new Uri("https://localhost:50051");
+            // The port number(5001) must match the port of the gRPC server.
+            httpClient.BaseAddress = new Uri("https://localhost:5001");
             var client = GrpcClient.Create<Greeter.GreeterClient>(httpClient);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
