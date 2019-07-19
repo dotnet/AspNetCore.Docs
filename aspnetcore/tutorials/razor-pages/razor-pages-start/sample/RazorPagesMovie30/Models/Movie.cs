4f6db660c37efb1cfb875ff9a14a39f8a426045e
Rick Anderson <riande@microsoft.com>
2019-07-19 16:36:17 -0700

    Update RP intro to 3.0 (#13391)
    * Update RP intro to 3.0
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work

---
+++ aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie30/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
