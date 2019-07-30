a1acf900fe429aec3163be52e4f17e72dfcf33fd
Rick Anderson <riande@microsoft.com>
2019-07-30 17:58:12 -0600

    work

---
+++ aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie3/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
