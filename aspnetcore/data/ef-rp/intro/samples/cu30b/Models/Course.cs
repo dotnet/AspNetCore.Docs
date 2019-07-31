f8d480170598ba4d4f50fd5bf5fa3910c2a4efbf
Tom Dykstra <tdykstra@microsoft.com>
2019-07-30 18:18:52 -0700

    corrections

---
+++ aspnetcore/data/ef-rp/intro/samples/cu30b/Models/Course.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
--- aspnetcore/data/ef-rp/intro/samples/cu30b/Startup.cs
+++ aspnetcore/data/ef-rp/intro/samples/cu30b/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
 
 namespace ContosoUniversity
 {
@@ -26,6 +28,9 @@ namespace ContosoUniversity
 
 
             services.AddRazorPages();
+
+            services.AddDbContext<SchoolContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SchoolContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
