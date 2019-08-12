37c549d4eb162fcebc2c58064940c7ee8c7b7def
Luke Latham <1622880+guardrex@users.noreply.github.com>
2019-08-12 16:17:44 -0500

    Configuration topic+sample 3.0 updates (#13670)

--- aspnetcore/fundamentals/configuration/index/samples/3.x/ConfigurationSample/Program.cs
+++ aspnetcore/fundamentals/configuration/index/samples/3.x/ConfigurationSample/Program.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.IO;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using ConfigurationSample.Extensions;
 
 namespace ConfigurationSample
@@ -23,11 +23,11 @@ namespace ConfigurationSample
 
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
                     config.SetBasePath(Directory.GetCurrentDirectory());
@@ -42,7 +42,10 @@ namespace ConfigurationSample
                         options => options.UseInMemoryDatabase("InMemoryDb"));
                     config.AddCommandLine(args);
                 })
-                .UseStartup<Startup>();
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
     #endregion
 }
