4014254b2f2aaff81f9d61582be4fbc50432fc33
Luke Latham <1622880+guardrex@users.noreply.github.com>
2019-08-15 13:35:11 -0500

    Integration Testing topic+sample update for 3.0
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates

---
+++ aspnetcore/test/integration-tests/samples/3.x/IntegrationTestsSample/tests/RazorPagesProject.Tests/IntegrationTests/BasicTests.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using AngleSharp.Html.Dom;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using RazorPagesProject.Services;
+using RazorPagesProject.Tests.Helpers;
+
+namespace RazorPagesProject.Tests.IntegrationTests
+{
+    #region snippet1
+    public class BasicTests 
+        : IClassFixture<WebApplicationFactory<RazorPagesProject.Startup>>
+    {
+        private readonly WebApplicationFactory<RazorPagesProject.Startup> _factory;
+
+        public BasicTests(WebApplicationFactory<RazorPagesProject.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Index")]
+        [InlineData("/About")]
+        [InlineData("/Privacy")]
+        [InlineData("/Contact")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8", 
+                response.Content.Headers.ContentType.ToString());
+        }
+    #endregion
+
+        #region snippet2
+        [Fact]
+        public async Task Get_SecurePageRequiresAnAuthenticatedUser()
+        {
+            // Arrange
+            var client = _factory.CreateClient(
+                new WebApplicationFactoryClientOptions
+                {
+                    AllowAutoRedirect = false
+                });
+
+            // Act
+            var response = await client.GetAsync("/SecurePage");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.StartsWith("http://localhost/Identity/Account/Login", 
+                response.Headers.Location.OriginalString);
+        }
+        #endregion
+
+        #region snippet3
+        [Fact]
+        public async Task CanGetAGithubUser()
+        {
+            // Arrange
+            void ConfigureTestServices(IServiceCollection services) =>
+                services.AddSingleton<IGithubClient>(new TestGithubClient());
+            var client = _factory
+                .WithWebHostBuilder(builder => 
+                    builder.ConfigureTestServices(ConfigureTestServices))
+                .CreateClient();
+
+            // Act
+            var profile = await client.GetAsync("/GithubProfile");
+            Assert.Equal(HttpStatusCode.OK, profile.StatusCode);
+            var profileHtml = await HtmlHelpers.GetDocumentAsync(profile);
+
+            var profileWithUserName = await client.SendAsync(
+                (IHtmlFormElement)profileHtml.QuerySelector("#user-profile"),
+                new Dictionary<string, string> { ["Input_UserName"] = "user" });
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, profileWithUserName.StatusCode);
+            var profileWithUserHtml = 
+                await HtmlHelpers.GetDocumentAsync(profileWithUserName);
+            var userLogin = profileWithUserHtml.QuerySelector("#user-login");
+            Assert.Equal("user", userLogin.TextContent);
+        }
+
+        public class TestGithubClient : IGithubClient
+        {
+            public Task<GithubUser> GetUserAsync(string userName)
+            {
+                if (userName == "user")
+                {
+                    return Task.FromResult(
+                        new GithubUser
+                        {
+                            Login = "user",
+                            Company = "Contoso Blockchain",
+                            Name = "John Doe"
+                        });
+                }
+                else
+                {
+                    return Task.FromResult<GithubUser>(null);
+                }
+            }
+        }
+        #endregion
+    }
+}
