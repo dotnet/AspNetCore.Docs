47415a51ef49723a999265ec29af5468c917194f
Luke Latham <1622880+guardrex@users.noreply.github.com>
2019-08-26 13:27:00 -0500

    File Providers topic update for 3.0

---
+++ aspnetcore/fundamentals/file-providers/samples/3.x/FileProviderSample/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.FileProviders;
+
+namespace FileProviderSample.Pages
+{
+    #region snippet1
+    public class IndexModel : PageModel
+    {
+        private readonly IFileProvider _fileProvider;
+
+        public IndexModel(IFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+        }
+
+        public IDirectoryContents DirectoryContents { get; private set; }
+
+        public void OnGet()
+        {
+            DirectoryContents = _fileProvider.GetDirectoryContents(string.Empty);
+        }
+    }
+    #endregion
+}
