5cfc8540e19e877671618351b1c8b47bd4329a2f
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-09-03 10:53:30 +0100

    Update BlazorSample to preview 9

--- aspnetcore/blazor/common/samples/3.x/BlazorSample/JsInteropClasses/ExampleJsInterop.cs
+++ aspnetcore/blazor/common/samples/3.x/BlazorSample/JsInteropClasses/ExampleJsInterop.cs
@@ -17,12 +17,12 @@ namespace BlazorSample.JsInteropClasses
             _jsRuntime = jsRuntime;
         }
 
-        public Task CallHelloHelperSayHello(string name)
+        public ValueTask<object> CallHelloHelperSayHello(string name)
         {
             // sayHello is implemented in wwwroot/exampleJsInterop.js
             return _jsRuntime.InvokeAsync<object>(
                 "exampleJsFunctions.sayHello",
-                DotNetObjectRef.Create(new HelloHelper(name)));
+                DotNetObjectReference.Create(new HelloHelper(name)));
         }
     }
     #endregion
