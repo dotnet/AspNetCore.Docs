d385e9959b5f8bbfd2feb1bcf5326456b06a8d4b
Luke Latham <1622880+guardrex@users.noreply.github.com>
2019-09-22 12:57:37 -0500

    MVC Test Controller Logic update to 3.0

---
+++ aspnetcore/mvc/controllers/testing/samples/3.x/TestingControllersSample/src/TestingControllersSample/Infrastructure/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestingControllersSample.Core.Model;
+
+namespace TestingControllersSample.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) :
+            base(dbContextOptions)
+        {
+        }
+
+        public DbSet<BrainstormSession> BrainstormSessions { get; set; }
+    }
+}
