3173c083001b265a8246a28530b9db3467a9bd12
Rick Anderson <riande@microsoft.com>
2019-09-22 18:06:46 -1000

    Revert "Removed MetadataReferenceFeature from the Application Parts sample. (#14464)"
    This reverts commit 1e63a2e53f30baeba8dec91370f4d9f772489af5.

--- aspnetcore/mvc/advanced/app-parts/sample/AppPartsSample/ViewModels/FeaturesViewModel.cs
+++ aspnetcore/mvc/advanced/app-parts/sample/AppPartsSample/ViewModels/FeaturesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
 using TypeInfo = System.Reflection.TypeInfo;
 
 namespace AppPartsSample.ViewModels
@@ -6,6 +7,8 @@ namespace AppPartsSample.ViewModels
     public class FeaturesViewModel
     {
         public List<TypeInfo> Controllers { get; set; }
+
+        public List<MetadataReference> MetadataReferences { get; set; }
 
         public List<TypeInfo> TagHelpers { get; set; }
 
