d559577c4e009414fd91aa3b5bbd58fe290b11d1
Rick Anderson <riande@microsoft.com>
2019-09-22 18:12:05 -1000

    Update Features article (#12376)
    * Update Features article
    
    * Update Features article
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * Update docfx.json (#12414)

---
+++ aspnetcore/mvc/advanced/app-parts/sample2/AppPartsSample/GenericControllerNameConvention.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+
+namespace AppPartsSample
+{
+    // Used to set the controller name for routing purposes. Without this convention the
+    // names is 'GenericController`1[Widget]' rather than 'Widget'.
+    //
+    // Conventions can be applied as attributes or added to MvcOptions.Conventions.
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class GenericControllerNameConvention : Attribute, IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            if (controller.ControllerType.GetGenericTypeDefinition() != 
+                typeof(GenericController<>))
+            {
+                // Not a GenericController, ignore.
+                return;
+            }
+
+            var entityType = controller.ControllerType.GenericTypeArguments[0];
+            controller.ControllerName = entityType.Name;
+        }
+    }
+}
