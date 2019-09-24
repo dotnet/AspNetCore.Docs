259dcfeca278103b55875fc8a503f16b5a9bd6ce
Luke Latham <1622880+guardrex@users.noreply.github.com>
2019-09-24 14:31:50 -0500

    DI topic updates for 3.0 (#14471)
    * DI topic updates for 3.0
    
    Updates
    
    * React to feedback
    
    * AddControllersWithViews over UseMvc for 3.0 Autofac example
    
    * Nits
    
    * Link out for container replacement scenarios
    
    * Content for Generic Host Startup injection

---
+++ aspnetcore/fundamentals/dependency-injection/samples/3.x/DependencyInjectionSample/Interfaces/IOperation.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace DependencyInjectionSample.Interfaces
+{
+    #region snippet1
+    public interface IOperation
+    {
+        Guid OperationId { get; }
+    }
+
+    public interface IOperationTransient : IOperation
+    {
+    }
+
+    public interface IOperationScoped : IOperation
+    {
+    }
+
+    public interface IOperationSingleton : IOperation
+    {
+    }
+
+    public interface IOperationSingletonInstance : IOperation
+    {
+    }
+    #endregion
+}
