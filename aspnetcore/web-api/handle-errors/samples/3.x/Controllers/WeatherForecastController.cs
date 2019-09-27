5155bf85b183f220d7093922ebd914ee126b2d09
Scott Addie <scott.addie@microsoft.com>
2019-09-27 12:15:48 -0500

    Add more detail to Developer Exception Page with web API

--- aspnetcore/web-api/handle-errors/samples/3.x/Controllers/WeatherForecastController.cs
+++ aspnetcore/web-api/handle-errors/samples/3.x/Controllers/WeatherForecastController.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace WebApiSample.Controllers
 {
@@ -11,11 +10,6 @@ namespace WebApiSample.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -24,16 +18,37 @@ namespace WebApiSample.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecast> Get() =>
+            GetWeather();
+
+        #region snippet_GetByCity
+        [HttpGet("{city}")]
+        public WeatherForecast Get(string city)
         {
+            if (!string.Equals(city?.TrimEnd(), "Redmond", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new ArgumentException(
+                    $"We don't offer a weather forecast for {city}.", nameof(city));
+            }
+            
+            return GetWeather().First();
+        }
+        #endregion
+
+        private IEnumerable<WeatherForecast> GetWeather()
+        {
+            string[] summaries = new[]
+            {
+                "Rainy", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
             var rng = new Random();
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                Summary = summaries[rng.Next(summaries.Length)]
+            }).ToArray();
         }
     }
 }
