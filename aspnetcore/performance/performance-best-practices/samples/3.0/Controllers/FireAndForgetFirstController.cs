1e14273e536c04986227265ebcff8c53fc3a7af7
Rick Anderson <riande@microsoft.com>
2019-10-09 11:13:38 -1000

    Performance best practice update with davidfowl samples (#14734)
    * Migrated asp.net core best practice to performance article moved code to samples
    
    * Fixed link validation issues for performance-best-practice.md
    
    * Fixing validation error for bookmark link for performance-best-practices.md
    
    * Another link fix on performance-best-practices.md and moved hot definition
    
    * fixed code snippet regions.
    
    * Perf best practice by fowler
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * add back comment The following line will crash the process because ...
    
    * react to fowler feedback
    
    * react to fowler feedback
    
    * param left alignment
    
    * param left alignment
    
    * react to fowler feedback
    
    * react to fowler feedback
    
    * react to halter73 feedback
    
    * Apply Tratcher suggestions from code review
    
    Co-Authored-By: Chris Ross <Tratcher@Outlook.com>
    
    * react to tratcher feeedback
    
    * my changes
    
    * react to Tratcher feedback

---
+++ aspnetcore/performance/performance-best-practices/samples/3.0/Controllers/FireAndForgetFirstController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+
+namespace performance_best_practices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FireAndForgetBadController : Controller
+    {
+        #region snippet1
+        [HttpGet("/fire-and-forget-1")]
+        public IActionResult BadFireAndForget()
+        {
+            _ = Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+
+                var path = HttpContext.Request.Path;
+                Log(path);
+            });
+
+            return Accepted();
+        }
+        #endregion
+
+        private void Log(PathString path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FireAndForgetGoodController : Controller
+    {
+        #region snippet2
+        [HttpGet("/fire-and-forget-3")]
+        public IActionResult GoodFireAndForget()
+        {
+            string path = HttpContext.Request.Path;
+            _ = Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+
+                Log(path);
+            });
+
+            return Accepted();
+        }
+        #endregion
+        private void Log(PathString path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
