fd29d6aae540c62de61d27b02b6c69c5b2627147
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2019-10-30 11:44:35 +0000

    Fix google authentication main code fragment missing

--- aspnetcore/security/authentication/social/social-code/3.x/StartupGoogle3x.cs
+++ aspnetcore/security/authentication/social/social-code/3.x/StartupGoogle3x.cs
@@ -3,8 +3,9 @@ public void ConfigureServices(IServiceCollection services)
     services.AddDbContext<ApplicationDbContext>(options =>
         options.UseSqlServer(
             Configuration.GetConnectionString("DefaultConnection")));
-    services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-        .AddEntityFrameworkStores<ApplicationDbContext>();
+    services.AddDefaultIdentity<IdentityUser>(options =>
+        options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
     services.AddRazorPages();
 
     services.AddAuthentication()
@@ -16,5 +17,4 @@ public void ConfigureServices(IServiceCollection services)
             options.ClientId = googleAuthNSection["ClientId"];
             options.ClientSecret = googleAuthNSection["ClientSecret"];
         });
-
 }
