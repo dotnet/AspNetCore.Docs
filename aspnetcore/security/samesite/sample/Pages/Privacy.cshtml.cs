df033410561cd70fff93019145d5164cc19b3ff3
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2019-12-02 21:54:32 -0800

    SameSiite (#15618)
    * DRAFT NO COMMENTS PLEAZE
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * never
    
    * shold b nothing
    
    * Tratcher outline started
    
    * Tratcher outline started
    
    * Tratcher outline started
    
    * Tratcher outline started
    
    * Tratcher outline started
    
    * Tratcher outline started
    
    * Tratcher outline started
    
    * Tratcher outline started
    
    * Tratcher outline started
    
    * Tratcher outline started
    
    * Tratcher outline started
    
    * Update aspnetcore/security/samesite.md
    
    Co-Authored-By: Chris Ross <Tratcher@Outlook.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: Chris Ross <Tratcher@Outlook.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: Chris Ross <Tratcher@Outlook.com>
    
    * Say the default changed from Lax to (-1)
    
    * react to Trather feedback
    
    * Apply suggestions from code review
    
    Co-Authored-By: Chris Ross <Tratcher@Outlook.com>
    
    * Update aspnetcore/security/samesite/sample/Startup.cs
    
    Co-Authored-By: Chris Ross <Tratcher@Outlook.com>

---
+++ aspnetcore/security/samesite/sample/Pages/Privacy.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebSameSite.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        private CultureInfo culture;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
