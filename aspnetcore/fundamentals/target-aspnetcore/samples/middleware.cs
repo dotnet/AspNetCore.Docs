8c41e7a1949d37b7a2fa9b02886f73f162cb9b52
Scott Addie <scott.addie@microsoft.com>
2019-12-16 15:06:35 -0600

    Simplify middleware code snippet

--- aspnetcore/fundamentals/target-aspnetcore/samples/middleware.cs
+++ aspnetcore/fundamentals/target-aspnetcore/samples/middleware.cs
@@ -1,57 +1,29 @@
-// code omitted for brevity
-
-#if NETCOREAPP3_0 || NETCOREAPP5_0
-using System.Text.Json;
-using System.Text.Json.Serialization;
-#else
-using Newtonsoft.Json;
-#endif
-
-namespace Benchmarks.Middleware
+public async Task Invoke(HttpContext httpContext)
 {
-    public class JsonMiddleware
+    if (httpContext.Request.Path.StartsWithSegments(_path, StringComparison.Ordinal))
     {
-        private static readonly PathString _path = new PathString(Scenarios.GetPath(s => s.Json));
-        private static readonly UTF8Encoding _encoding = new UTF8Encoding(false);
-        private const int _bufferSize = 27;
-#if !NETCOREAPP3_0 && !NETCOREAPP5_0
-        private static readonly JsonSerializer _json = new JsonSerializer();
-#endif
-        private readonly RequestDelegate _next;
+        httpContext.Response.StatusCode = 200;
+        httpContext.Response.ContentType = "application/json";
+        httpContext.Response.ContentLength = _bufferSize;
 
-        public JsonMiddleware(RequestDelegate next)
+#if !NETCOREAPP3_0 && !NETCOREAPP5_0
+        var syncIOFeature = httpContext.Features.Get<IHttpBodyControlFeature>();
+        if (syncIOFeature != null)
         {
-            _next = next;
+            syncIOFeature.AllowSynchronousIO = true;
         }
 
-        public async Task Invoke(HttpContext httpContext)
+        using (var sw = new StreamWriter(
+            httpContext.Response.Body, _encoding, bufferSize: _bufferSize))
         {
-            if (httpContext.Request.Path.StartsWithSegments(_path, StringComparison.Ordinal))
-            {
-                httpContext.Response.StatusCode = 200;
-                httpContext.Response.ContentType = "application/json";
-                httpContext.Response.ContentLength = _bufferSize;
-
-#if !NETCOREAPP3_0 && !NETCOREAPP5_0
-                var syncIOFeature = httpContext.Features.Get<IHttpBodyControlFeature>();
-                if (syncIOFeature != null)
-                {
-                    syncIOFeature.AllowSynchronousIO = true;
-                }
-
-                using (var sw = new StreamWriter(httpContext.Response.Body, _encoding, bufferSize: _bufferSize))
-                {
-                    _json.Serialize(sw, new JsonMessage { message = "Hello, World!" });
-                }
+            _json.Serialize(sw, new JsonMessage { message = "Hello, World!" });
+        }
 #else
-                await JsonSerializer.SerializeAsync<JsonMessage>(httpContext.Response.Body, new JsonMessage { message = "Hello, World!" });
+        await JsonSerializer.SerializeAsync<JsonMessage>(
+            httpContext.Response.Body, new JsonMessage { message = "Hello, World!" });
 #endif
-                return;
-            }
-
-            await _next(httpContext);
-        }
+        return;
     }
 
-    // code omitted for brevity
+    await _next(httpContext);
 }
