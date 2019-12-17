194c3823f2328291dd57f0ad77f819337c19efed
Scott Addie <10702007+scottaddie@users.noreply.github.com>
2019-12-17 10:15:49 -0600

    Add guidance for using ASP.NET Core APIs in libraries (#15741)
    * Add guidance for using ASP.NET Core APIs in libraries
    
    * remove stray text
    
    * refactoring
    
    * refactoring
    
    * edits
    
    * more edits
    
    * more edits
    
    * simplification
    
    * React to feedback
    
    * React to most feedback
    
    * Simplify middleware code snippet
    
    * React to feedback

---
+++ aspnetcore/fundamentals/target-aspnetcore/samples/middleware.cs
@@ -0,0 +1,29 @@
+public async Task Invoke(HttpContext httpContext)
+{
+    if (httpContext.Request.Path.StartsWithSegments(_path, StringComparison.Ordinal))
+    {
+        httpContext.Response.StatusCode = 200;
+        httpContext.Response.ContentType = "application/json";
+        httpContext.Response.ContentLength = _bufferSize;
+
+#if !NETCOREAPP3_0 && !NETCOREAPP5_0
+        var syncIOFeature = httpContext.Features.Get<IHttpBodyControlFeature>();
+        if (syncIOFeature != null)
+        {
+            syncIOFeature.AllowSynchronousIO = true;
+        }
+
+        using (var sw = new StreamWriter(
+            httpContext.Response.Body, _encoding, bufferSize: _bufferSize))
+        {
+            _json.Serialize(sw, new JsonMessage { message = "Hello, World!" });
+        }
+#else
+        await JsonSerializer.SerializeAsync<JsonMessage>(
+            httpContext.Response.Body, new JsonMessage { message = "Hello, World!" });
+#endif
+        return;
+    }
+
+    await _next(httpContext);
+}
