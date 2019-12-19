3e03600980ada6f239ff2ecb35df8f2fac5d0b70
Kirk Larkin <serpent5@pm.me>
2019-12-19 16:15:03 +0000

    Additional explanation of middleware pipeline configuration options.

---
+++ aspnetcore/fundamentals/middleware/index/snapshot/Chain/StartupUseWhen.cs
@@ -0,0 +1,31 @@
+﻿public class Startup
+{
+    private readonly ILogger<Startup> _logger;
+
+    public Startup(ILogger<Startup> logger)
+    {
+        _logger = logger;
+    }
+
+    private void HandleBranchAndRejoin(IApplicationBuilder app)
+    {
+        app.Use(async (context, next) =>
+        {
+            var branchVer = context.Request.Query["branch"];
+            _logger.LogInformation("Branch used = {branchVer}", branchVer);
+
+            await next();
+        });
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseWhen(context => context.Request.Query.ContainsKey("branch"),
+                               HandleBranchAndRejoin);
+
+        app.Run(async context =>
+        {
+            await context.Response.WriteAsync("Hello from main pipeline.");
+        });
+    }
+}
