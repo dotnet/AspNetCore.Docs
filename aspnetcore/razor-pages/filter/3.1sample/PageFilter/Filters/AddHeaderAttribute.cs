d941f8603c6a8375ed01538b5e44f879be6d804c
Rick Anderson <riande@microsoft.com>
2019-12-18 15:02:34 -1000

    3.1 update of RP filters

---
+++ aspnetcore/razor-pages/filter/3.1sample/PageFilter/Filters/AddHeaderAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace PageFilter.Filters
+{
+    public class AddHeaderAttribute  : ResultFilterAttribute
+    {
+        private readonly string _name;
+        private readonly string _value;
+
+        public AddHeaderAttribute (string name, string value)
+        {
+            _name = name;
+            _value = value;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            context.HttpContext.Response.Headers.Add(_name, new string[] { _value });
+        }
+    }
+}
