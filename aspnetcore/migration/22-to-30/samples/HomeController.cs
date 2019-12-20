c543e7229ab01ce5ec2d8c2582fa9faaf89773d5
Pranav K <prkrishn@hotmail.com>
2019-12-19 17:38:06 -0800

    Add global auth filter + policy migration example (#16092)
    * Update 22-to-30.md
    
    * Update 22-to-30.md
    
    * Merge into Pranav's PR (#16129)
    
    * Update 22-to-30.md
    
    * add snippet
    
    * add snippet
    
    * add snippet
    
    * add snippet
    
    * add snippet
    
    * Update aspnetcore/migration/22-to-30/samples/Startup.cs
    
    Co-Authored-By: Kirk Larkin <6025110+serpent5@users.noreply.github.com>
    
    * react to serpent5 feedback
    
    * react to serpent5 feedback
    
    Co-authored-by: Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>

---
+++ aspnetcore/migration/22-to-30/samples/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication66.Models;
+
+namespace WebApplication66.Controllers
+{
+    #region snippet
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        #endregion
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
