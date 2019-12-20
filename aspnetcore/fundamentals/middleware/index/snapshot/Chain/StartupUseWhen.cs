e9d933e991eb10ec6e7cef35cf8494194121416a
Kirk Larkin <serpent5@pm.me>
2019-12-20 09:34:23 +0000

    Inline comments for UseWhen's pipeline.

--- aspnetcore/fundamentals/middleware/index/snapshot/Chain/StartupUseWhen.cs
+++ aspnetcore/fundamentals/middleware/index/snapshot/Chain/StartupUseWhen.cs
@@ -14,7 +14,9 @@
             var branchVer = context.Request.Query["branch"];
             _logger.LogInformation("Branch used = {branchVer}", branchVer);
 
+            // Do work that doesn't write to the Response.
             await next();
+            // Do other work that doesn't write to the Response.
         });
     }
 
