176c1ed55894881ec37146f052cebfb8a900fb97
Ryan Nowak <nowakra@gmail.com>
2019-12-27 13:32:27 -0800

    Pivot Controller routing content based on version

---
+++ aspnetcore/mvc/controllers/routing/sample-3.0/main/StartupAreas1.cs
@@ -0,0 +1,10 @@
+﻿#if Never
+#region snippet_1
+app.UseMvc(routes =>
+{
+    routes.MapAreaRoute("zebra_route", "Zebra",
+        "Manage/{controller}/{action}/{ id ?}");
+    routes.MapRoute("default_route", "{controller}/{action}/{id?}");
+});
+#endregion
+#endif
---
+++ aspnetcore/mvc/controllers/routing/sample-3.0/main/StartupUseMvc.cs
@@ -0,0 +1,9 @@
+﻿/*
+#region snippet_1
+// In Startup class
+public void Configure(IApplicationBuilder app)
+{
+    app.UseMvc();
+}
+#endregion
+*/
