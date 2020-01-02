a547aaf08ce1cddb430a3bd9b5c515bdc18532cd
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2020-01-02 14:03:26 -0800

    Update Filters to 3.1 (#16294)
    * Update Filters to 3.1
    
    * Update Filters to 3.1
    
    * Update Filters to 3.1
    
    * Update Filters to 3.1
    
    * Update Filters to 3.1
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work

---
+++ aspnetcore/mvc/controllers/filters/3.1sample/FiltersSample/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using FiltersSample.Filters;
+using FiltersSample.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FiltersSample.Controllers
+{
+    #region snippet
+public class TestController : Controller
+{
+    [SampleActionFilter(Order = int.MinValue)]
+    public IActionResult FilterTest2()
+    {
+            var m = MethodBase.GetCurrentMethod();
+            MyDebug.Write(m, HttpContext.Request.Path);
+            return Content(m.ReflectedType.Name + "." + m.Name);
+        }
+
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        // Do something before the action executes.
+        MyDebug.Write(MethodBase.GetCurrentMethod(), HttpContext.Request.Path);
+        base.OnActionExecuting(context);
+    }
+
+    public override void OnActionExecuted(ActionExecutedContext context)
+    {
+        // Do something after the action executes.
+        MyDebug.Write(MethodBase.GetCurrentMethod(), HttpContext.Request.Path);
+        base.OnActionExecuted(context);
+    }
+}
+#endregion
+}
