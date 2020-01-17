7eaa30066f787be64129572ecd5f775928c4f36f
Rick Anderson <riande@microsoft.com>
2020-01-17 12:53:44 -1000

    fix duplicate startup compile error

--- aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie3/StartupDevProd.cs
+++ aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie3/StartupDevProd.cs
@@ -1,17 +1,12 @@
-using MvcMovie.Data;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovie.Data;
 
-namespace MvcMovie
+namespace MvcMovie_Production
 {
     #region snippet_StartupClass
     public class Startup
