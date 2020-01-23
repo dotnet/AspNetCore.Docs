cd9dc7b190cd198f68e019403e900606863c7933
Wade Pickett <wpickett@microsoft.com>
2020-01-23 13:08:08 -0800

    patch: Loc updates: moved code to cs files & commenting to list.

---
+++ aspnetcore/grpc/browser/sample/Startup.cs
@@ -0,0 +1,18 @@
+﻿#region snippet
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddGrpc();
+}
+
+public void Configure(IApplicationBuilder app)
+{
+    app.UseRouting();
+
+    app.UseGrpcWeb();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapGrpcService<GreeterService>().EnableGrpcWeb();
+    });
+}
+#endregion
