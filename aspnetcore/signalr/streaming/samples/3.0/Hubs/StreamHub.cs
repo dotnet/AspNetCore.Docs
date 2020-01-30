52997c6ec3d42fc19bfa10127e08d493046f2ed8
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-01-30 14:08:09 -0800

    feedback

--- aspnetcore/signalr/streaming/samples/3.0/Hubs/StreamHub.cs
+++ aspnetcore/signalr/streaming/samples/3.0/Hubs/StreamHub.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reactive.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
@@ -31,7 +30,6 @@ namespace SignalRChat.Hubs
             ChannelWriter<int> writer,
             int count,
             int delay,
-            [EnumeratorCancellation]
             CancellationToken cancellationToken)
         {
             Exception localException = null;
