48350e32e0de0eee5bee5e4ebe477bcea9b08b10
Rick Anderson <riande@microsoft.com>
2020-01-30 20:03:51 -1000

    work

---
+++ aspnetcore/mvc/controllers/routing/samples/3.x/AreasRouting/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace3
+{
+    // Matches { area = string.Empty, controller = Users, action = AddUser }
+    // Matches { area = null, controller = Users, action = AddUser }
+    // Matches { controller = Users, action = AddUser }
+    public class UsersController : Controller
+    {
+        public IActionResult AddUser()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ aspnetcore/mvc/controllers/routing/samples/3.x/AreasRouting/Startup3.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AreasRouting
+{
+    public class Startup3
+    {
+        public Startup3(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            #region snippet3
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapAreaControllerRoute("duck_route", "Duck",
+                    "Manage/{controller}/{action}/{id?}");
+                endpoints.MapControllerRoute("default", "Manage/{controller=Home}/{action=Index}/{id?}");
+            });
+            #endregion
+        }
+    }
+}
