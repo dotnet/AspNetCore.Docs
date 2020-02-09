7543af23559d0746e9b517878e0808220f0afc62
Wade Pickett <wpickett@microsoft.com>
2020-02-09 13:48:30 -0800

    Update Startup.cs
    Changed snippet regions

--- aspnetcore/tutorials/signalr-typescript-webpack/sample/3.x/Startup.cs
+++ aspnetcore/tutorials/signalr-typescript-webpack/sample/3.x/Startup.cs
@@ -7,7 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+#region snippet_HubsNamespace
 using SignalRWebPack.Hubs;
+#endregion
 
 namespace SignalRWebPack
 {
@@ -17,25 +19,32 @@ namespace SignalRWebPack
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            #region snippet_AddSignalR
             services.AddSignalR();
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        #region snippet_UseStaticDefaultFiles
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             app.UseRouting();
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
+            
+            #region snippet_UseSignalR
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapHub<ChatHub>("/hub");
             });
+            #endregion
+                
         }
+        #endregion
     }
 }
