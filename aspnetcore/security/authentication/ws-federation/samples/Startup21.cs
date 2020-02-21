8fd2b116beeed2d765c87ade7ed5bf2fac1d98da
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2020-02-21 14:02:18 -0800

    Identity Fed (#17080)
    * Identity Fed
    
    * Identity Fed
    
    * Identity Fed
    
    * Identity Fed
    
    * Identity Fed
    
    * Identity Fed
    
    * Update aspnetcore/security/authentication/ws-federation/samples/StartupNon31.cs
    
    Co-Authored-By: Chris Ross <Tratcher@Outlook.com>
    
    * work
    
    Co-authored-by: Chris Ross <Tratcher@Outlook.com>

---
+++ aspnetcore/security/authentication/ws-federation/samples/Startup21.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp21.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp21
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        #region snippet
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddWsFederation(options =>
+                {
+                    // MetadataAddress represents the Active Directory instance used to authenticate users.
+                    options.MetadataAddress = "https://<ADFS FQDN or AAD tenant>/FederationMetadata/2007-06/FederationMetadata.xml";
+            
+                    // Wtrealm is the app's identifier in the Active Directory instance.
+                    // For ADFS, use the relying party's identifier, its WS-Federation Passive protocol URL:
+                    options.Wtrealm = "https://localhost:44307/";
+            
+                    // For AAD, use the App ID URI from the app registration's Properties blade:
+                    options.Wtrealm = "https://wsfedsample.onmicrosoft.com/bf0e7e6d-056e-4e37-b9a6-2c36797b9f01";
+                });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+        #endregion
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
