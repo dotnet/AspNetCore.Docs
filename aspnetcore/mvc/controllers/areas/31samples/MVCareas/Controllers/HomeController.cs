25646599b56d798c913ab3947588348200e43024
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2020-02-25 23:15:15 -0800

    Update areas to 3.1 (#17094)
    * Update areas to 3.1
    
    * Update areas to 3.1
    
    * Update areas to 3.1
    
    * work
    
    * work

---
+++ aspnetcore/mvc/controllers/areas/31samples/MVCareas/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCareas.Models;
+using System.Diagnostics;
+
+namespace MVCareas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["routeInfo"] = ControllerContext.ToCtxString();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewData["routeInfo"] = ControllerContext.ToCtxString();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["routeInfo"] = ControllerContext.ToCtxString();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ aspnetcore/mvc/controllers/areas/31samples/RPareas/Areas/Services/Pages/Manage/Index.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RPareas.Areas.Services.Pages.Manage
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            ViewData["routeInfo"] = PageContext.ToCtxStringP();
+        }
+    }
+}
