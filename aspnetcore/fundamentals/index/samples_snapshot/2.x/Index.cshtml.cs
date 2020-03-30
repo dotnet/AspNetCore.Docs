ba17b01ba53e525e6214025cffabb6dece897daf
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2020-03-30 23:50:41 +0100

    Update Fundamentals/Index to 3.1 (#17321)
    * Update Fundamentals/Index to 3.1
    
    * Apply suggestions from code review
    
    Co-Authored-By: Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
    
    * React to feedback
    
    Co-authored-by: Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>

--- aspnetcore/fundamentals/index/samples_snapshot/2.x/Index.cshtml.cs
+++ aspnetcore/fundamentals/index/samples_snapshot/2.x/Index.cshtml.cs
@@ -6,11 +6,11 @@ public class IndexModel : PageModel
     {
         _context = context;
     }
+
     // ...
+
     public async Task OnGetAsync()
     {
-        var movies = from m in _context.Movies
-                        select m;
-        Movies = await movies.ToListAsync();
+        Movies = await _context.Movies.ToListAsync();
     }
 }
