1acadb9be83d0671c689dcf98d0e4de92d2d7b8d
Rick Anderson <riande@microsoft.com>
2020-03-31 18:01:09 -1000

    Javascript

--- aspnetcore/security/cors/3.1sample/Cors/WebAPI/wwwroot/js/MyJS.js
+++ aspnetcore/security/cors/3.1sample/Cors/WebAPI/wwwroot/js/MyJS.js
@@ -1,31 +1,11 @@
-﻿function MyTestCors(host, uri, methodName) {
+﻿function MyTestCors3(host, uri, methodName, includeHeaders=false) {
     const resultSpan = document.getElementById('result');
+    const myHeaders = includeHeaders ? { 'Content-Type': 'x-custom-header' } : {};
 
     fetch(host + uri,
         {
             method: methodName,
-        }).then(response => {
-            if (response.ok) {
-                response.text().then(text => {
-                    resultSpan.innerText = text;
-                });
-            }
-            else {
-                resultSpan.innerText = response.status;
-            }
-        })
-        .catch(error => resultSpan.innerText = 'See F12 Console for error');
-}
-
-function MyTestCorsX(host, uri, methodName) {
-    const resultSpan = document.getElementById('result');
-
-    fetch(host + uri,
-        {
-            method: methodName,
-            headers: {
-                "Content-Type": "x-custom-header"
-            },
+            headers: myHeaders,
 
         }).then(response => {
             if (response.ok) {
