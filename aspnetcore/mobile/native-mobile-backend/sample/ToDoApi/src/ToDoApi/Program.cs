44e1e0e50525a90700ef983372750a4b81f867e7
Hamid Molareza <kitm.hamid@gmail.com>
2020-04-05 20:31:44 +0430

    Config to run on http://0.0.0.0:5001

--- aspnetcore/mobile/native-mobile-backend/sample/ToDoApi/src/ToDoApi/Program.cs
+++ aspnetcore/mobile/native-mobile-backend/sample/ToDoApi/src/ToDoApi/Program.cs
@@ -1,16 +1,30 @@
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace ToDoApi {
     public class Program {
         public static void Main (string[] args) {
-            CreateHostBuilder (args).Build ().Run ();
-        }
+            // use this to allow command line parameters in the config
+            var configuration = new ConfigurationBuilder ()
+                .AddCommandLine (args)
+                .Build ();
 
-        public static IHostBuilder CreateHostBuilder (string[] args) =>
-            Host.CreateDefaultBuilder (args)
-            .ConfigureWebHostDefaults (webBuilder => {
-                webBuilder.UseStartup<Startup> ();
-            });
+            var hostUrl = configuration["hosturl"];
+            if (string.IsNullOrEmpty (hostUrl))
+                hostUrl = "http://0.0.0.0:5001";
+
+            var host = new WebHostBuilder ()
+                .UseKestrel ()
+                .UseUrls (hostUrl)
+                .UseContentRoot (Directory.GetCurrentDirectory ())
+                .UseIISIntegration ()
+                .UseStartup<Startup> ()
+                .UseConfiguration (configuration)
+                .Build ();
+
+            host.Run ();
+        }
     }
 }
