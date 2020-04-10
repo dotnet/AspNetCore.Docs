03bc444f27f4c180452d827af4e0492313097274
Hamid Molareza <kitm.hamid@gmail.com>
2020-04-10 23:14:19 +0430

    Create Tutorial-WebApi Project.

---
+++ aspnetcore/mobile/native-mobile-backend/sample/TodoApi/TodoApi/Models/TodoItemDTO.cs
@@ -0,0 +1,7 @@
+namespace TodoApi.Models {
+    public class TodoItemDTO {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
