437db076abbeba2836e134932336016108be9c44
Hamid Molareza <kitm.hamid@gmail.com>
2020-04-11 18:42:30 +0430

    Update Models.

--- aspnetcore/mobile/native-mobile-backend/sample/TodoApi/TodoApi/Models/TodoItem.cs
+++ aspnetcore/mobile/native-mobile-backend/sample/TodoApi/TodoApi/Models/TodoItem.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoApi.Models {
     public class TodoItem {
-        public long Id { get; set; }
+        [Required]
+        public string Id { get; set; }
         public string Name { get; set; }
-        public bool IsComplete { get; set; }
+        public string Notes { get; set; }
+        public bool Done { get; set; }
         public string Secret { get; set; }
     }
 }
--- aspnetcore/mobile/native-mobile-backend/sample/TodoApi/TodoApi/Models/TodoItemDTO.cs
+++ aspnetcore/mobile/native-mobile-backend/sample/TodoApi/TodoApi/Models/TodoItemDTO.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoApi.Models {
     public class TodoItemDTO {
-        public long Id { get; set; }
+        [Required]
+        public string Id { get; set; }
         public string Name { get; set; }
-        public bool IsComplete { get; set; }
+        public string Notes { get; set; }
+        public bool Done { get; set; }
     }
 }
