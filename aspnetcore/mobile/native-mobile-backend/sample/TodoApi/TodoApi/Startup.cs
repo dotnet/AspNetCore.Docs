754ce3fc7a24e26603b8063e9f531d699d20cee5
Hamid Molareza <kitm.hamid@gmail.com>
2020-04-11 17:10:21 +0430

    Delete unnecessary files. (WeatherForecast)

--- aspnetcore/mobile/native-mobile-backend/sample/TodoApi/TodoApi/Startup.cs
+++ aspnetcore/mobile/native-mobile-backend/sample/TodoApi/TodoApi/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using TodoApi.Models;
 
 namespace TodoApi {
