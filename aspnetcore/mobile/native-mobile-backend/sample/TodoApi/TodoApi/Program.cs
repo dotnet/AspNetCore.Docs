2697d356423c3c82f126f498c2aeedd4f5529324
Hamid Molareza <kitm.hamid@gmail.com>
2020-04-11 17:15:14 +0430

    Config Main method to run on http://*:5000

--- aspnetcore/mobile/native-mobile-backend/sample/TodoApi/TodoApi/Program.cs
+++ aspnetcore/mobile/native-mobile-backend/sample/TodoApi/TodoApi/Program.cs
@@ -1,20 +1,19 @@
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace TodoApi
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+namespace TodoApi {
+    public class Program {
+        public static void Main (string[] args) {
+            var host = new WebHostBuilder ()
+                .UseKestrel ()
+                .UseUrls ("http://*:5000")
+                .UseContentRoot (Directory.GetCurrentDirectory ())
+                .UseIISIntegration ()
+                .UseStartup<Startup> ()
+                .Build ();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            host.Run ();
+        }
     }
 }
