bb77636c9b404bd48c8e8b3bef8e1de09ebbe46b
Rick Anderson <riande@microsoft.com>
2020-04-28 13:44:07 -1000

    work

--- aspnetcore/fundamentals/logging/index/samples/3.x/MyMain/Program.cs
+++ aspnetcore/fundamentals/logging/index/samples/3.x/MyMain/Program.cs
@@ -1,10 +1,5 @@
 #define FilterFunction
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
