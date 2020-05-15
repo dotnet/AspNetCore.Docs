4176c2fca3e7979975bbdcf416db1c8289d0afc5
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-05-15 19:57:03 +0300

    Update AccountController.cs
    There is no need  to put user id in callback url that is way  i removed user id and i tested and it works.

--- aspnetcore/migration/1x-to-2x/samples/AspNetCoreDotNetFx2.0App/AspNetCoreDotNetFx2.0App/Controllers/AccountController.cs
+++ aspnetcore/migration/1x-to-2x/samples/AspNetCoreDotNetFx2.0App/AspNetCoreDotNetFx2.0App/Controllers/AccountController.cs
@@ -371,7 +371,7 @@ namespace AspNetCoreDotNetFx2._0App.Controllers
                 // For more information on how to enable account confirmation and password reset please
                 // visit https://go.microsoft.com/fwlink/?LinkID=532713
                 var code = await _userManager.GeneratePasswordResetTokenAsync(user);
-                var callbackUrl = Url.ResetPasswordCallbackLink(user.Id, code, Request.Scheme);
+                var callbackUrl = Url.ResetPasswordCallbackLink(code, Request.Scheme);
                 await _emailSender.SendEmailAsync(model.Email, "Reset Password",
                    $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
                 return RedirectToAction(nameof(ForgotPasswordConfirmation));
