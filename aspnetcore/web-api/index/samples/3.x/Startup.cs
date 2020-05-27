e8e5c8be32df28c715469e1760da595267d46d25
Ian Kemp <IanKemp@users.noreply.github.com>
2020-05-27 16:41:43 +0100

    alphabetize usings

--- aspnetcore/web-api/index/samples/3.x/Startup.cs
+++ aspnetcore/web-api/index/samples/3.x/Startup.cs
@@ -9,10 +9,10 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Http;
 
 namespace WebApiSample
 {
