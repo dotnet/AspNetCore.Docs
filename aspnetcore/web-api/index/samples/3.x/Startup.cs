d89957fb8dc76c3b3409af089d7b75977406abe5
Ian Kemp <IanKemp@users.noreply.github.com>
2020-05-27 22:19:47 +0100

    Use strongly-typed status code (#18508)
    * use strongly-typed constants for status code
    
    * alphabetize usings

--- aspnetcore/web-api/index/samples/3.x/Startup.cs
+++ aspnetcore/web-api/index/samples/3.x/Startup.cs
@@ -9,6 +9,7 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -35,7 +36,7 @@ namespace WebApiSample
                     options.SuppressInferBindingSourcesForParameters = true;
                     options.SuppressModelStateInvalidFilter = true;
                     options.SuppressMapClientErrors = true;
-                    options.ClientErrorMapping[404].Link =
+                    options.ClientErrorMapping[StatusCodes.Status404NotFound].Link =
                         "https://httpstatuses.com/404";
                 });
             #endregion
