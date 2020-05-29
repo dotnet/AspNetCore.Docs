4aef1af17fc9b1bff246a19eaf75d22fec0a17d6
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-05-29 04:11:24 +0300

    Add 3.x localization sample

---
+++ aspnetcore/fundamentals/localization/sample/3.x/Localization/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Localization
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
