9f7f696cda9daef9d404c71b9a5a4f186c902425
Pranav K <prkrishn@hotmail.com>
2020-06-06 21:40:17 -0700

    Update AuthorizeIndexPageHandlerFilter.cs

--- aspnetcore/security/authorization/simple/samples/3.1/PageHandlerAuth/AuthorizeIndexPageHandlerFilter.cs
+++ aspnetcore/security/authorization/simple/samples/3.1/PageHandlerAuth/AuthorizeIndexPageHandlerFilter.cs
@@ -35,13 +35,7 @@ namespace WebApplication22
                 return;
             }
 
-            var authorizeAttribute = new AuthorizeAttribute(attribute.Policy)
-            {
-               Roles = attribute.Roles,
-               AuthenticationSchemes = attribute.AuthenticationSchemes,
-            };
-            
-            var policy = await AuthorizationPolicy.CombineAsync(policyProvider, new[] { authorizeAttribute });
+            var policy = await AuthorizationPolicy.CombineAsync(policyProvider, new[] { attribute });
             if (policy is null)
             {
                 return;
@@ -93,3 +87,19 @@ namespace WebApplication22
     }
     #endregion
 }
+
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class AuthorizePageHandlerAttribute : Attribute, IAuthorizeData
+    {
+        public AuthorizePageHandlerAttribute(string policy = null)
+        {
+            Policy = policy;
+        }
+
+        public string Policy { get; set; }
+        
+        public string Roles { get; set; }
+        
+        public string AuthenticationSchemes { get; set; }
+    }
