413a30f6712949b3e0074f7be776eac825392460
Rick Anderson <riande@microsoft.com>
2020-06-07 16:38:21 -1000

    work

--- aspnetcore/security/authorization/simple/samples/3.1/PageHandlerAuth/AuthorizeIndexPageHandlerFilter.cs
+++ aspnetcore/security/authorization/simple/samples/3.1/PageHandlerAuth/AuthorizeIndexPageHandlerFilter.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace WebApplication22
