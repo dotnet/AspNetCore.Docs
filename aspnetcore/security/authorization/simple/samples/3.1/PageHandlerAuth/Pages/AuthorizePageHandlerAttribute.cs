dd2f0caf0f9d18d319fd004089c096241f7adfb5
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2020-06-09 13:43:57 -1000

    Can't apply [Authorize] on page handler methods (#18676)
    * Can't apply [Authorize] on page handler methods
    
    * Can't apply [Authorize] on page handler methods
    
    * add compose warning
    
    * add compose warning
    
    * add compose warning
    
    * add sample
    
    * add sample
    
    * add sample
    
    * add sample
    
    * add sample
    
    * Update AuthorizeIndexPageHandlerFilter.cs
    
    * Update AuthorizeIndexPageHandlerFilter.cs
    
    * Update Index.cshtml.cs
    
    * Update Startup.cs
    
    * work
    
    * work
    
    * work
    
    * Update aspnetcore/security/authorization/simple.md
    
    Co-authored-by: Pranav K <prkrishn@hotmail.com>
    
    Co-authored-by: Pranav K <prkrishn@hotmail.com>

---
+++ aspnetcore/security/authorization/simple/samples/3.1/PageHandlerAuth/Pages/AuthorizePageHandlerAttribute.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace WebApplication22.Pages
+{
+}
---
+++ aspnetcore/security/authorization/simple/samples/3.1/PageHandlerAuth/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication22.Pages
+{
+    #region snippet
+    [TypeFilter(typeof(AuthorizeIndexPageHandlerFilter))]
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+
+        [AuthorizePageHandler]
+        public void OnPostAuthorized()
+        {
+
+        }
+    }
+    #endregion
+}
