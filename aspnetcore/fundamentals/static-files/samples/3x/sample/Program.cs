268135bc109aaa672cbc4d82a28064a5263302ce
Rick Anderson <riande@microsoft.com>
2020-06-29 13:55:47 -1000

    Replace Directory.GetCurrentDirectory() with xplat env versions

--- aspnetcore/fundamentals/static-files/samples/3x/sample/Program.cs
+++ aspnetcore/fundamentals/static-files/samples/3x/sample/Program.cs
@@ -16,15 +16,15 @@ namespace sample
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     //webBuilder.UseStartup<Startup>();
-                   // webBuilder.UseStartup<StartupRose>();
+                    // webBuilder.UseStartup<StartupRose>();
                     // webBuilder.UseStartup<StartupAddHeader>();
-                    webBuilder.UseStartup<StartupBrowse>();
+                    //webBuilder.UseStartup<StartupBrowse>();
                     //webBuilder.UseStartup<StartupEmpty>();
                     //webBuilder.UseStartup<StartupEmpty2>();
                     //webBuilder.UseStartup<StartupEmpty3>();
                     //webBuilder.UseStartup<StartupDefault>();
                     // webBuilder.UseStartup<StartupDefault2>();
-                    //webBuilder.UseStartup<StartupUseFileServer>();
+                    webBuilder.UseStartup<StartupUseFileServer>();
                     //webBuilder.UseStartup<StartupFileExtensionContentTypeProvider>();
                     //webBuilder.UseStartup<StartupServeUnknownFileTypes>();
 
