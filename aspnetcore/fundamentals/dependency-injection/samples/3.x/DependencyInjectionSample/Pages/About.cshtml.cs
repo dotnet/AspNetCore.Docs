aa44ca01005c51c688c850c3592cdcbe8d21282f
Rick Anderson <riande@microsoft.com>
2020-07-30 18:12:40 -1000

    DI Antipatterns

--- aspnetcore/fundamentals/dependency-injection/samples/3.x/DependencyInjectionSample/Pages/About.cshtml.cs
+++ aspnetcore/fundamentals/dependency-injection/samples/3.x/DependencyInjectionSample/Pages/About.cshtml.cs
@@ -1,16 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace DependencyInjectionSample.Pages
 {
+    #region snippet
     public class AboutModel : PageModel
     {
+        private readonly ILogger _logger;
+
+        public AboutModel(ILogger<AboutModel> logger)
+        {
+            _logger = logger;
+        }
+        public string Message { get; set; }
+
         public void OnGet()
         {
+            Message = $"About page visited at {DateTime.UtcNow.ToLongTimeString()}";
+            _logger.LogInformation(Message);
         }
     }
+    #endregion
 }
