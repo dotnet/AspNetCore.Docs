8ab65ce39ad822d0ed872707cd6de54c3fa5e2c7
Patrick Goode <pgoode75@sbcglobal.net>
2020-08-17 00:24:32 -0500

    Update example for EF6 and .Net Core 3.1

---
+++ aspnetcore/data/entity-framework-6/3.xsample/MVCCore/DbContext/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(string connString) : base(connString)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ aspnetcore/data/entity-framework-6/3.xsample/MVCCore/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ContosoUniversity.SchoolContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ContosoUniversity.SchoolContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
