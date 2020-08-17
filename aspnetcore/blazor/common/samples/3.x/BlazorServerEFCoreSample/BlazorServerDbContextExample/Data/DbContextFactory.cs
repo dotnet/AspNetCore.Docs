74c30928d978af7b3c81ae700241f4e3e701764a
guardrex <1622880+guardrex@users.noreply.github.com>
2020-08-17 09:36:32 -0500

    Blazor EF Core topic updates

--- aspnetcore/blazor/common/samples/3.x/BlazorServerEFCoreSample/BlazorServerDbContextExample/Data/DbContextFactory.cs
+++ aspnetcore/blazor/common/samples/3.x/BlazorServerEFCoreSample/BlazorServerDbContextExample/Data/DbContextFactory.cs
@@ -1,43 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace BlazorServerDbContextExample.Data
 {
-    /// <summary>
-    /// Factory to generate instances of <see cref="TContext"/>.
-    /// </summary>
-    /// <typeparam name="TContext">The <see cref="DbContext"/> type to provide.</typeparam>
-    public class DbContextFactory<TContext> : IDbContextFactory<TContext> where TContext : DbContext
+    public class DbContextFactory<TContext> 
+        : IDbContextFactory<TContext> where TContext : DbContext
     {
-        /// <summary>
-        /// Service provider.
-        /// </summary>
-        private readonly IServiceProvider _provider;
+        private readonly IServiceProvider provider;
 
-        /// <summary>
-        /// Inject the service provider.
-        /// </summary>
-        /// <param name="provider">The <see cref="IServiceProvider"/> instance.</param>
         public DbContextFactory(IServiceProvider provider)
         {
-            _provider = provider;
+            this.provider = provider;
         }
 
-        /// <summary>
-        /// Create a newly created context.
-        /// </summary>
-        /// <returns>A new instance of <see cref="TContext"/>.</returns>
-        // rename to Create
         public TContext CreateDbContext()
         {
-            if (_provider == null)
+            if (provider == null)
             {
-                throw new InvalidOperationException($"You must configure an instance of IServiceProvider");
+                throw new InvalidOperationException(
+                    $"You must configure an instance of IServiceProvider");
             }
 
-            // satisfies any dependencies via the provider
-            return ActivatorUtilities.CreateInstance<TContext>(_provider);
+            return ActivatorUtilities.CreateInstance<TContext>(provider);
         }
     }
 }
