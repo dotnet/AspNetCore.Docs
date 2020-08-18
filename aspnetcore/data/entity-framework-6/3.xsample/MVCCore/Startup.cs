4529fb2503fbba849f05e64bd8991d43f069d51e
Rick Anderson <riande@microsoft.com>
2020-08-17 14:21:33 -1000

    EF6 and ASP.NET Core

--- aspnetcore/data/entity-framework-6/3.xsample/MVCCore/Startup.cs
+++ aspnetcore/data/entity-framework-6/3.xsample/MVCCore/Startup.cs
@@ -16,14 +16,13 @@ namespace MVCCore
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddScoped<SchoolContext>(_ => new SchoolContext(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<SchoolContext>(_ => 
+                new SchoolContext(Configuration.GetConnectionString("DefaultConnection")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -33,7 +32,6 @@ namespace MVCCore
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
