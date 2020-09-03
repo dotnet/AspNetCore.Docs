ae81a15022f84144728217b08536584034fcbd43
Rick Anderson <riande@microsoft.com>
2020-09-02 17:23:08 -1000

    work

---
+++ aspnetcore/fundamentals/error-handling/samples/5.x/ErrorHandlingSample/Pages/Error.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.IO;
+
+namespace ErrorHandlingSample.Pages
+{
+    #region snippet
+    [ResponseCache(Duration=0, Location=ResponseCacheLocation.None, NoStore=true)]
+    [IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string ExceptionMessage { get; set; }
+        private readonly ILogger<ErrorModel> _logger;
+
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+            var exceptionHandlerPathFeature =
+            HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+            {
+                ExceptionMessage = "File error thrown";
+                _logger.LogError(ExceptionMessage);
+            }
+            if (exceptionHandlerPathFeature?.Path == "/index")
+            {
+                ExceptionMessage += " from home page";
+            }
+        }
+    }
+    #endregion
+}
