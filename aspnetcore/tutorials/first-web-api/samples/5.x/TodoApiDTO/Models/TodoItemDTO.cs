84b365e87678bf952a633e1a1f410b9966bc0444
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2020-09-21 13:33:22 -1000

    Webapi update to 5.0/ra (#19781)
    * Update first Web API to 5.0 RC1
    
    * Update first Web API to .NET 5 RC1
    
    * Update first Web API to .NET 5 RC1
    
    * Update first Web API to .NET 5 RC1
    
    * work
    
    * work
    
    * work
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up

---
+++ aspnetcore/tutorials/first-web-api/samples/5.x/TodoApiDTO/Models/TodoItemDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Models
+{
+    #region snippet
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+    #endregion
+}
