cf5c8849e75facc6879b1892ee99970140f703d7
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2020-10-01 15:01:57 -1000

    5.0 update for EF/RP (#20064)
    * 5.0 update for EF/RP
    
    * 5.0 update for EF/RP
    
    * 5.0 update for EF/RP
    
    * 5.0 update for EF/RP

--- aspnetcore/data/ef-rp/intro/samples/cu50/Pages/Students/Index.cshtml.cs
+++ aspnetcore/data/ef-rp/intro/samples/cu50/Pages/Students/Index.cshtml.cs
@@ -1,34 +1,73 @@
 ﻿using ContosoUniversity.Data;
 using ContosoUniversity.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ContosoUniversity.Pages.Students
 {
-    #region snippet
     public class IndexModel : PageModel
     {
         private readonly SchoolContext _context;
-        private readonly MvcOptions _mvcOptions;
 
-        public IndexModel(SchoolContext context, IOptions<MvcOptions> mvcOptions)
+        public IndexModel(SchoolContext context)
         {
             _context = context;
-            _mvcOptions = mvcOptions.Value;
         }
 
-        public IList<Student> Student { get;set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public PaginatedList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
-            Student = await _context.Students.Take(
-                _mvcOptions.MaxModelBindingCollectionSize).ToListAsync();
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                             select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstMidName.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            int pageSize = 3;
+            Students = await PaginatedList<Student>.CreateAsync(
+                studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
-    #endregion
 }
