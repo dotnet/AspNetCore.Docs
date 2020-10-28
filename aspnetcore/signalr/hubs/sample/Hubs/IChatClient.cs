0e25d12b9c0a988d4986ad29da9b28904ff31596
Brennan <brecon@microsoft.com>
2020-10-28 11:36:39 -0700

    Update IChatClient.cs

--- aspnetcore/signalr/hubs/sample/Hubs/IChatClient.cs
+++ aspnetcore/signalr/hubs/sample/Hubs/IChatClient.cs
@@ -5,7 +5,6 @@ namespace SignalRChat.Hubs
     #region snippet_IChatClient
     public interface IChatClient
     {
-        Task ReceiveMessage(string user, string message);
         Task ReceiveMessage(string message);
     }
     #endregion
