6d53f3ff20e2a4f6f9399f17728a451a983753aa
Rick Anderson <riande@microsoft.com>
2020-11-04 17:05:09 -1000

    work

--- aspnetcore/data/ef-mvc/intro/samples/5cu-snap/Program.cs
+++ aspnetcore/data/ef-mvc/intro/samples/5cu-snap/Program.cs
@@ -26,7 +26,7 @@ namespace ContosoUniversity
                 try
                 {
                     var context = services.GetRequiredService<SchoolContext>();
-                    context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context);
                 }
                 catch (Exception ex)
                 {
