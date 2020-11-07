d9e53d2940671eb455ba05dd857ce08ad09574a5
Rick Anderson <riande@microsoft.com>
2020-11-06 15:29:22 -1000

    work

--- aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie5/Data/MvcMovieContext.cs
+++ aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie5/Data/MvcMovieContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using MvcMovie.Models;
 
 namespace MvcMovie.Data
