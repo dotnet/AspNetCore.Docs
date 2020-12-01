2969df0300dac747276f62b7dac051134483f89e
Luke Latham <llatham@aquent.com>
2020-12-01 07:14:59 -0600

    Blazor SignalR tutorial sample update for 5.0 (#20822)

--- aspnetcore/tutorials/signalr-blazor-webassembly/samples/5.x/BlazorSignalRApp/Server/Startup.cs
+++ aspnetcore/tutorials/signalr-blazor-webassembly/samples/5.x/BlazorSignalRApp/Server/Startup.cs
@@ -12,11 +12,19 @@ namespace BlazorSignalRApp.Server
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         #region snippet_ConfigureServices
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSignalR();
             services.AddControllersWithViews();
+            services.AddRazorPages();
             services.AddResponseCompression(opts =>
             {
                 opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
@@ -49,6 +57,7 @@ namespace BlazorSignalRApp.Server
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllers();
                 endpoints.MapHub<ChatHub>("/chathub");
                 endpoints.MapFallbackToFile("index.html");
