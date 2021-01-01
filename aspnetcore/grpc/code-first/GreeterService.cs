17c5e268bbabc262c02acdc160fe7e74c1637360
Luke Latham <llatham@aquent.com>
2021-01-01 05:09:06 -0600

    Update GreeterService.cs

--- aspnetcore/grpc/code-first/GreeterService.cs
+++ aspnetcore/grpc/code-first/GreeterService.cs
@@ -1,7 +1,9 @@
 public class GreeterService : IGreeterService
 {
-    public Task<HelloReply> SayHelloAsync(HelloRequest request, CallContext context = default)
+    public Task<HelloReply> SayHelloAsync(HelloRequest request, 
+        CallContext context = default)
     {
-        return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
+        return Task.FromResult(new HelloReply { Message = "Hello " + 
+            request.Name });
     }
 }
