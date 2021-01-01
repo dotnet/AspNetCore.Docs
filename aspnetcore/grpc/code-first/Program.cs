cbbebf1f104280631d364c33b2d6909ead99b2a4
Luke Latham <llatham@aquent.com>
2021-01-01 05:10:05 -0600

    Update Program.cs

--- aspnetcore/grpc/code-first/Program.cs
+++ aspnetcore/grpc/code-first/Program.cs
@@ -1,5 +1,6 @@
 using var channel = GrpcChannel.ForAddress("https://localhost:5001");
 var client = channel.CreateGrpcService<IGreeterService>();
 
-var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+var reply = await client.SayHelloAsync(
+  new HelloRequest { Name = "GreeterClient" });
 Console.WriteLine("Greeting: " + reply.Message);
