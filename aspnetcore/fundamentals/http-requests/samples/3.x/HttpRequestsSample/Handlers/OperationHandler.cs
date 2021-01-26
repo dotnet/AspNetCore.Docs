0ac6a03a6fa786443dee916d2198d36581a9ea89
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2021-01-26 10:32:40 +1000

    Serpent5 scoped di/ra (#21275)
    * Explain scoped DI in HttpClient handlers
    
    * Include relevant sample code in the topic
    
    * Explain scoped DI in HttpClient handlers by serpent5
    
    * Explain scoped DI in HttpClient handlers by serpent5
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * Apply suggestions from code review
    
    Co-authored-by: Kirk Larkin <serpent5@pm.me>
    
    * Update aspnetcore/fundamentals/http-requests/samples/3.x/HttpRequestsSample/Startup.cs
    
    Co-authored-by: Kirk Larkin <serpent5@pm.me>
    
    Co-authored-by: Kirk Larkin <serpent5@pm.me>

---
+++ aspnetcore/fundamentals/http-requests/samples/3.x/HttpRequestsSample/Handlers/OperationHandler.cs
@@ -0,0 +1,27 @@
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using HttpRequestsSample.Models;
+
+namespace HttpRequestsSample.Handlers
+{
+    #region snippet_Class
+    public class OperationHandler : DelegatingHandler
+    {
+        private readonly IOperationScoped _operationService;
+
+        public OperationHandler(IOperationScoped operationScoped)
+        {
+            _operationService = operationScoped;
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("X-OPERATION-ID", _operationService.OperationId);
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+    #endregion
+}
