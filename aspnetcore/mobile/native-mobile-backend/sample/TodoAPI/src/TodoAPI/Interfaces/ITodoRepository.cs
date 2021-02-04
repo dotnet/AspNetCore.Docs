139b3899da0c035c0c7d1e9841a7dba3b2ce1ab7
James Montemagno <james.montemagno@gmail.com>
2021-02-03 18:29:37 -0800

    Add sample back in

---
+++ aspnetcore/mobile/native-mobile-backend/sample/TodoAPI/src/TodoAPI/Interfaces/ITodoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using TodoAPI.Models;
+
+namespace TodoAPI.Interfaces
+{
+    public interface ITodoRepository
+    {
+        bool DoesItemExist(string id);
+        IEnumerable<TodoItem> All { get; }
+        TodoItem Find(string id);
+        void Insert(TodoItem item);
+        void Update(TodoItem item);
+        void Delete(string id);
+    }
+}
---
+++ aspnetcore/mobile/native-mobile-backend/sample/TodoAPI/src/TodoAPI/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPI.Models
+{
+    public class TodoItem
+    {
+        [Required]
+        public string ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Notes { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
