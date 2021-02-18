6500808073aae2dfd8113214aeed966c7b1bdba8
David Guida <david.guida.03@gmail.com>
2021-02-18 17:36:42 -0500

    addressed review comments

--- aspnetcore/fundamentals/host/hosted-services/samples/3.x/BackgroundTasksSample/Services/BackgroundTaskQueue.cs
+++ aspnetcore/fundamentals/host/hosted-services/samples/3.x/BackgroundTasksSample/Services/BackgroundTaskQueue.cs
@@ -10,15 +10,13 @@ namespace BackgroundTasksSample.Services
     {
         ValueTask QueueBackgroundWorkItemAsync(Func<CancellationToken, ValueTask> workItem);
 
-        Task<Func<CancellationToken, ValueTask>> DequeueAsync(
+        ValueTask<Func<CancellationToken, ValueTask>> DequeueAsync(
             CancellationToken cancellationToken);
     }
 
     public class BackgroundTaskQueue : IBackgroundTaskQueue
     {
         private readonly Channel<Func<CancellationToken, ValueTask>> _queue = Channel.CreateUnbounded<Func<CancellationToken, ValueTask>>();
-
-        private readonly SemaphoreSlim _signal = new SemaphoreSlim(0);
 
         public async ValueTask QueueBackgroundWorkItemAsync(
             Func<CancellationToken, ValueTask> workItem)
@@ -29,16 +27,12 @@ namespace BackgroundTasksSample.Services
             }
 
             await _queue.Writer.WriteAsync(workItem);
-            
-            _signal.Release();
         }
 
-        public async Task<Func<CancellationToken, ValueTask>> DequeueAsync(
+        public async ValueTask<Func<CancellationToken, ValueTask>> DequeueAsync(
             CancellationToken cancellationToken)
         {
-            await _signal.WaitAsync(cancellationToken);
-
-            _queue.Reader.TryRead(out var workItem);
+            var workItem = await _queue.Reader.ReadAsync(cancellationToken);
 
             return workItem;
         }
