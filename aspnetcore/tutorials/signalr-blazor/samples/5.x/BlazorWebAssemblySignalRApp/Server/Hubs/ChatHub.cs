d799b38108ac2576fbcaa7a807d4fcdd2fabcc79
guardrex <1622880+guardrex@users.noreply.github.com>
2021-04-01 04:33:52 -0500

    Blazor-SignalR tutorial updates

--- aspnetcore/tutorials/signalr-blazor/samples/5.x/BlazorWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
+++ aspnetcore/tutorials/signalr-blazor/samples/5.x/BlazorWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
 namespace BlazorWebAssemblySignalRApp.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessageAsync(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
