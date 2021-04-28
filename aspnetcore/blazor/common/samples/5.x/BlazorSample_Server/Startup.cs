ae04911703a4c804e6dd2371517c7f2b392d6485
guardrex <1622880+guardrex@users.noreply.github.com>
2021-04-28 10:28:36 -0500

    Blazor File Uploads topic UE pass

--- aspnetcore/blazor/common/samples/5.x/BlazorSample_Server/Startup.cs
+++ aspnetcore/blazor/common/samples/5.x/BlazorSample_Server/Startup.cs
@@ -30,6 +30,7 @@ namespace BlazorSample
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddScoped<Notifier>();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
