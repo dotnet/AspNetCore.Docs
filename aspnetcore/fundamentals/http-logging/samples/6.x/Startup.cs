64544742548896e296a487ea69135092adcbef11
Sourabh Shirhatti <shirhatti@outlook.com>
2021-05-25 15:40:14 -0700

    Fix usings

--- aspnetcore/fundamentals/http-logging/samples/6.x/Startup.cs
+++ aspnetcore/fundamentals/http-logging/samples/6.x/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
