a6617ada1cf16c9687132bf3de3154cf31efcd7d
Fiyaz Bin Hasan <fiyazhasan@live.com>
2021-07-24 02:39:36 +0600

    Update ui-class.md (#22808)
    * Update ui-class.md
    
    * Create Startup.cs
    
    * use code snippet
    
    * new startup created and snippet reference added to doc
    
    * Delete Startup.cs
    
    * new line in the end
    
    * move startup out of the webapp1
    
    * Update Startup.cs
    
    * Update aspnetcore/razor-pages/ui-class.md
    
    * Update aspnetcore/razor-pages/ui-class.md
    
    Co-authored-by: Kirk Larkin <kirk.larkin@pm.me>
    
    Co-authored-by: Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
    Co-authored-by: Kirk Larkin <kirk.larkin@pm.me>

--- aspnetcore/razor-pages/ui-class/samples/Startup.cs
+++ aspnetcore/razor-pages/ui-class/samples/Startup.cs
@@ -4,40 +4,37 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace WebApp1
+namespace MvcApp
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
+        
+        #region snippet
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapRazorPages();
             });
         }
+        #endregion
     }
 }
