bcef903d0f1f7bec6789915c4fe130ba65b6c98c
guardrex <1622880+guardrex@users.noreply.github.com>
2021-08-05 04:58:06 -0500

    Integration Testing updates

--- aspnetcore/test/integration-tests/samples/2.x/IntegrationTestsSample/src/RazorPagesProject/Services/GithubUser.cs
+++
@@ -1,9 +0,0 @@
-namespace RazorPagesProject
-{
-    public class GithubUser
-  {
-      public string Login { get; set; }
-      public string Name { get; set; }
-      public string Company { get; set; }
-  }
-}
