9a9d53a8c6025fc7b635cb470d5660c3b25e5d1b
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2021-08-11 09:21:36 -0700

    Update CacheKeys.cs

--- aspnetcore/performance/caching/memory/3.0sample/WebCacheSample/CacheKeys.cs
+++ aspnetcore/performance/caching/memory/3.0sample/WebCacheSample/CacheKeys.cs
@@ -1,13 +1,13 @@
-﻿public static class CacheKeys
+public static class CacheKeys
 {
-    public static string Entry { get { return "_Entry"; } }
-    public static string CallbackEntry { get { return "_Callback"; } }
-    public static string CallbackMessage { get { return "_CallbackMessage"; } }
-    public static string Parent { get { return "_Parent"; } }
-    public static string Child { get { return "_Child"; } }
-    public static string DependentMessage { get { return "_DependentMessage"; } }
-    public static string DependentCTS { get { return "_DependentCTS"; } }
-    public static string Ticks { get { return "_Ticks"; } }
-    public static string CancelMsg { get { return "_CancelMsg"; } }
-    public static string CancelTokenSource { get { return "_CancelTokenSource"; } }
+    public static string Entry => "_Entry";
+    public static string CallbackEntry => "_Callback";
+    public static string CallbackMessage => "_CallbackMessage";
+    public static string Parent => "_Parent";
+    public static string Child => "_Child";
+    public static string DependentMessage => "_DependentMessage";
+    public static string DependentCTS => "_DependentCTS";
+    public static string Ticks => "_Ticks";
+    public static string CancelMsg => "_CancelMsg";
+    public static string CancelTokenSource => "_CancelTokenSource";
 }
