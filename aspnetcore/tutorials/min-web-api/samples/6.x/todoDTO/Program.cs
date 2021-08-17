d2200e967feaa3b314bc6b1261188cdac5ca847f
Rick Anderson <riande@microsoft.com>
2021-08-17 11:54:09 -0700

    start of DTO

---
+++ aspnetcore/tutorials/min-web-api/samples/6.x/todoDTO/Program.cs
@@ -0,0 +1,105 @@
+#region snippet_all
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+#region snippet_get
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/todoitems", async (TodoDb db) =>
+    await db.Todos.Select(x => TodoItemDTO.ItemToDTO(x)).ToListAsync());
+
+app.MapGet("/todoitems/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(TodoItemDTO.ItemToDTO(todo))
+            : Results.NotFound());
+#endregion
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+#region snippet_put
+app.MapPut("/todoitems/{id}", async (int id, TodoItemDTO todoItemDTO, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = todoItemDTO.Name;
+    todo.IsComplete = todoItemDTO.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+#endregion
+
+#region snippet_delete
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.Ok(todo);
+    }
+
+    return Results.NotFound();
+});
+
+#endregion
+
+app.Run();
+
+public class Todo
+{
+    public int Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
+
+#region snippet_DTO
+public class TodoItemDTO
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+
+    public static TodoItemDTO ItemToDTO(Todo todoItem) =>
+    new TodoItemDTO
+    {
+        Id = todoItem.Id,
+        Name = todoItem.Name,
+        IsComplete = todoItem.IsComplete
+    };
+}
+#endregion
+
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
+#endregion
