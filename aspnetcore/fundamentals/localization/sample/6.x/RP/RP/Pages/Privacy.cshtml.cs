5c7461e66148f42cb12ec39c1f24b6117362ac9a
Ponant <19961806+Ponant@users.noreply.github.com>
2021-08-26 11:28:41 +0200

    Sample for 6.x

---
+++ aspnetcore/fundamentals/localization/sample/6.x/RP/RP/Pages/Privacy.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace RP.Pages;
+public class PrivacyModel : PageModel
+{
+    [BindProperty]
+    public InputModel Input { get; set; } = null!;
+
+    public void OnGet()
+    {
+    }
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            return LocalRedirect("/");
+        }
+        return Page();
+    }
+}
+
+public class InputModel
+{
+    [Required(ErrorMessage = "Required")]
+    [MinLength(3, ErrorMessage = "{1} characters or more")]
+    [Display(Prompt = "Username", Name = "Your Username")]
+    public string UserName { get; set; } = null!;
+}
