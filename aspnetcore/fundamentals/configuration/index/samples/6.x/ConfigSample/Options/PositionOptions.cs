5376b7bda74585b9b803d606107e8dac6bb4c29e
Rick Anderson <riande@microsoft.com>
2021-09-04 10:21:13 -0700

    V6 update to config /5

--- aspnetcore/fundamentals/configuration/index/samples/6.x/ConfigSample/Options/PositionOptions.cs
+++ aspnetcore/fundamentals/configuration/index/samples/6.x/ConfigSample/Options/PositionOptions.cs
@@ -6,7 +6,7 @@
         public const string Position = "Position";
 
         public string Title { get; set; } = String.Empty;
-        public string Name { get; set; }= String.Empty;
+        public string Name { get; set; } = String.Empty;
     }
     #endregion
 }
