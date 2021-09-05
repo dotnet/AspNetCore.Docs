c83e5821d19733800050ebbec10538e7f6aba3d9
Rick Anderson <riande@microsoft.com>
2021-09-05 14:38:15 -0700

    V6 of DI /5

--- aspnetcore/fundamentals/dependency-injection/samples/6.x/DependencyInjectionSample/ProgramEF.cs
+++ aspnetcore/fundamentals/dependency-injection/samples/6.x/DependencyInjectionSample/ProgramEF.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
