7428aec1dfbbd72df593733ba37186edf6853f36
Rick Anderson <riande@microsoft.com>
2021-09-05 15:12:15 -0700

    .NET 6 CORS /6

---
+++ aspnetcore/security/cors/6.0sample/Cors/WebAPI/Program.cs
@@ -0,0 +1,35 @@
+#define FIRST // FIRST
+#if FIRST
+#region snippet
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://example.com",
+                                              "http://www.contoso.com");
+                      });
+});
+
+// services.AddResponseCaching();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+#endregion
+#endif
