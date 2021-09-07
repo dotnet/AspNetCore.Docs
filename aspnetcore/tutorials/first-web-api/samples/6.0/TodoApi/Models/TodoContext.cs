515ad00b6b63427c3a560da1282cb236e56ec9ec
Tom Dykstra <tdykstra@microsoft.com>
2021-09-07 16:10:13 -0700

    draft

---
+++ aspnetcore/tutorials/first-web-api/samples/6.0/TodoApi/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        [NotNull]
+        public DbSet<TodoItem>? TodoItems { get; set; }
+    }
+}
