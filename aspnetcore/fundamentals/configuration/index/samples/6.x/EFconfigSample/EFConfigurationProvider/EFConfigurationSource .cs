b490582c63c83862dc92f3835396f20403123fdb
Fiyaz Bin Hasan <fiyazhasan@live.com>
2021-09-08 08:41:47 +0600

    custom config provider sample for dotnet 6 (#23221)
    * sample for dotnet 6
    
    * newline endings

---
+++ aspnetcore/fundamentals/configuration/index/samples/6.x/EFconfigSample/EFConfigurationProvider/EFConfigurationSource .cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace EFConfigSample;
+#region snippet1
+public class EFConfigurationSource : IConfigurationSource
+{
+    private readonly Action<DbContextOptionsBuilder> _optionsAction;
+
+    public EFConfigurationSource(Action<DbContextOptionsBuilder> optionsAction) => _optionsAction = optionsAction;
+
+    public IConfigurationProvider Build(IConfigurationBuilder builder) => new EFConfigurationProvider(_optionsAction);
+}
+#endregion
