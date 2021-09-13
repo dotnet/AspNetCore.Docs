0f1985b01118f8b2ca39c1b1f9e7ded371450093
Tom Dykstra <tdykstra@microsoft.com>
2021-09-13 12:28:36 -0700

    draft

--- aspnetcore/fundamentals/host/hosted-services/samples/2.x/BackgroundTasksSample/Data/Message.cs
+++
@@ -1,8 +0,0 @@
-namespace BackgroundTasksSample.Data
-{
-    public class Message
-    {
-        public int Id { get; set; }
-        public string Text { get; set; }
-    }
-}
---
+++ aspnetcore/fundamentals/host/hosted-services/samples/6.0/BackgroundTasksSample/Services/TimedHostedService.cs
@@ -0,0 +1,48 @@
+namespace BackgroundTasksSample.Services
+{
+    #region snippet1
+    public class TimedHostedService : IHostedService, IDisposable
+    {
+        private int executionCount = 0;
+        private readonly ILogger<TimedHostedService> _logger;
+        private Timer _timer = null!;
+
+        public TimedHostedService(ILogger<TimedHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service running.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, 
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object? state)
+        {
+            var count = Interlocked.Increment(ref executionCount);
+
+            _logger.LogInformation(
+                "Timed Hosted Service is working. Count: {Count}", count);
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+    #endregion
+}
