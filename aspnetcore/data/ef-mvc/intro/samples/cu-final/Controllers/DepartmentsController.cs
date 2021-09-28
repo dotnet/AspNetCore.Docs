742f654a74194d0298ce89011f47e9adf41f76e1
Joan Pujol <91483947+Joan-Pujol@users.noreply.github.com>
2021-09-28 12:01:49 -0300

    Update DepartmentsController.cs
    FromSql is no longer supported. You must replace it with FromSqlRaw

--- aspnetcore/data/ef-mvc/intro/samples/cu-final/Controllers/DepartmentsController.cs
+++ aspnetcore/data/ef-mvc/intro/samples/cu-final/Controllers/DepartmentsController.cs
@@ -36,7 +36,7 @@ namespace ContosoUniversity.Controllers
 
             string query = "SELECT * FROM Department WHERE DepartmentID = {0}";
             var department = await _context.Departments
-                .FromSql(query, id)
+                .FromSqlRaw(query, id)
                 .Include(d => d.Administrator)
                 .AsNoTracking()
                 .FirstOrDefaultAsync();
