acd554a19f6d3f093815425a305676277f74fe35
Rick Anderson <riande@microsoft.com>
2021-11-04 18:53:28 -1000

    V6 Options /4

--- aspnetcore/fundamentals/configuration/options/samples/6.x/OptionsValidationSample/Program.cs
+++ aspnetcore/fundamentals/configuration/options/samples/6.x/OptionsValidationSample/Program.cs
@@ -1,4 +1,4 @@
-#define P2   // DEFAULT MC XM P1  P2
+#define P3   // DEFAULT MC XM P1  P2 P3
 #if NEVER
 #elif DEFAULT
 #region snippet
@@ -161,6 +161,44 @@ builder.Services.AddOptions<MyConfigOptions>()
                 .Bind(builder.Configuration.GetSection(MyConfigOptions.MyConfig));
 
 builder.Services.PostConfigure<MyConfigOptions>("MyConfig", myOptions =>
+{
+    myOptions.Key1 = "post_configured_key1_value";
+});
+#endregion
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+#elif P3
+#region snippet_p3
+using OptionsValidationSample.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddOptions<MyConfigOptions>()
+                .Bind(builder.Configuration.GetSection(MyConfigOptions.MyConfig));
+
+builder.Services.PostConfigureAll<MyConfigOptions>(myOptions =>
 {
     myOptions.Key1 = "post_configured_key1_value";
 });
