91506fd52f05ce508afe0aaf84592d48999d4757
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2021-11-05 14:36:56 -1000

    What's new in ASP.NET Core 6.0 /3 (#23640)
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * Apply suggestions from code review
    
    Co-authored-by: Luke Latham <1622880+guardrex@users.noreply.github.com>
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * Apply suggestions from code review
    
    Co-authored-by: Luke Latham <1622880+guardrex@users.noreply.github.com>
    
    * What's new in ASP.NET Core 6.0 /3
    
    * What's new in ASP.NET Core 6.0 /3
    
    * react to feedback
    
    * react to feedback
    
    * react to feedback
    
    * react to feedback
    
    * react to feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: Artak <34246760+mkArtakMSFT@users.noreply.github.com>
    Co-authored-by: Pranav K <prkrishn@hotmail.com>
    
    Co-authored-by: Luke Latham <1622880+guardrex@users.noreply.github.com>
    Co-authored-by: Artak <34246760+mkArtakMSFT@users.noreply.github.com>
    Co-authored-by: Pranav K <prkrishn@hotmail.com>

---
+++ aspnetcore/release-notes/aspnetcore-6.0/samples/WebMvcEF/Controllers/MoviesController.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebMvcEF.Models;
+
+namespace WebMvcEF.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MvcMovieContext _context;
+
+        public MoviesController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+        #region snippet1
+        public IActionResult GetMovies()
+        {
+            return Ok(_context.Movie);
+        }
+        #endregion
+
+        #region snippet2
+        public async Task<IActionResult> GetMovies2()
+        {
+            return Ok(await _context.Movie.ToListAsync());
+        }
+        #endregion
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Movie.ToListAsync());
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            Movie movie = new Movie
+            {
+                Genre = "western",
+                Price = 1.0M,
+                ReleaseDate = DateTime.Now,
+                Title = "Good Bqa"
+            };
+            return View(movie);
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movie.FindAsync(id);
+            _context.Movie.Remove(movie);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movie.Any(e => e.Id == id);
+        }
+    }
+}
