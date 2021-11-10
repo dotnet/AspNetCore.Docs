07197d0189b0df803fed9f7982584bcaf4a857c9
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2021-11-10 12:10:39 -1000

    clean up unused folders for EF/RP /3 (#23853)

--- aspnetcore/data/ef-rp/intro/samples/cuSQLite/Models/SchoolViewModels/CourseViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿public class CourseViewModel
-{
-    public int CourseID { get; set; }
-    public string? Title { get; set; }
-    public int Credits { get; set; }
-    public string? DepartmentName { get; set; }
-}
