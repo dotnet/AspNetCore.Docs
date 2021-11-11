a7732a5ca6125804e6233e31f9f28980811dd9f6
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2021-11-11 15:28:58 +0000

    Update Cache In-Memory to 6.0

---
+++ aspnetcore/performance/caching/memory/samples/6.0/CachingMemorySample/CacheKeys.cs
@@ -0,0 +1,25 @@
+﻿namespace CachingMemorySample
+{
+    public static class CacheKeys
+    {
+        public const string Entry = "_Entry";
+
+        public const string CallbackEntry = "_CallbackEntry";
+
+        public const string CallbackMessage = "_CallbackMessage";
+
+        public const string Parent = "_Parent";
+
+        public const string Child = "_Child";
+
+        // public const string DependentMessage = "_DependentMessage";
+
+        public const string DependentCancellationTokenSource = "_DependentCancellationTokenSource";
+
+        // public const string Ticks = "_Ticks";
+
+        // public const string CancelMessage = "_CancelMessage";
+
+        // public const string CancelTokenSource = "_CancelTokenSource";
+    }
+}
---
+++ aspnetcore/performance/caching/memory/samples/6.0/CachingMemorySample/Snippets/Pages/MyMemoryCache.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CachingMemorySample.Snippets.Pages
+{
+    public class MyMemoryCacheModel : PageModel
+    {
+        private readonly MemoryCache _memoryCache;
+
+        public MyMemoryCacheModel(MyMemoryCache myMemoryCache) =>
+            _memoryCache = myMemoryCache.Cache;
+
+        public void OnGetCacheSizeSetSize()
+        {
+            #region snippet_OnGetCacheSizeSetSize
+            if (!_memoryCache.TryGetValue(CacheKeys.Entry, out DateTime cacheValue))
+            {
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSize(1);
+
+                // cacheEntryOptions.Size = 1;
+
+                _memoryCache.Set(CacheKeys.Entry, cacheValue, cacheEntryOptions);
+            }
+            #endregion
+        }
+
+        public void OnGetCacheCompact()
+        {
+            #region snippet_OnGetCacheCompact
+            _memoryCache.Remove(CacheKeys.Entry);
+            _memoryCache.Compact(.25);
+            #endregion
+        }
+    }
+}
