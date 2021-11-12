88fe376e0c9d5d50ba8666c5ccb62c0f5c523a28
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2021-11-12 13:18:53 -1000

    Use startup new hosting model/ra/2 (#23893)
    * fix snippets in what's new
    
    * Migrate to 6.0, add shim to Startup
    
    * Migrate to 6.0, add shim to Startup
    
    * Migrate to 6.0, add shim to Startup
    
    * Migrate to 6.0, add shim to Startup
    
    * Migrate to 6.0, add shim to Startup

---
+++ aspnetcore/migration/50-to-60/samples/WebRP31to60/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebRP31to60.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
