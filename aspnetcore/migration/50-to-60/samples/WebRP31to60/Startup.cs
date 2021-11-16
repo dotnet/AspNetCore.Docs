010335d0fe58e2dd62c9810f870257f7972bb276
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2021-11-16 09:10:04 -1000

    Shim startup/ra/2 (#23898)
    * fix snippets in what's new
    
    * Remove UseEndpoints/UseRouting from StartUp shim/2
    
    * Remove UseEndpoints/UseRouting from StartUp shim/2
    
    * Remove UseEndpoints/UseRouting from StartUp shim/2
    
    * Update aspnetcore/migration/50-to-60.md
    
    Co-authored-by: Stephen Halter <halter73@gmail.com>
    
    * Remove UseEndpoints/UseRouting from StartUp shim/2
    
    Co-authored-by: Stephen Halter <halter73@gmail.com>

--- aspnetcore/migration/50-to-60/samples/WebRP31to60/Startup.cs
+++ aspnetcore/migration/50-to-60/samples/WebRP31to60/Startup.cs
@@ -123,5 +123,40 @@ public class Startup
     }
 }
 #endregion
+#elif V60E
+#region snippet_60e
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRazorPages();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (!env.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        //app.UseRouting();
+
+        //app.UseEndpoints(endpoints =>
+        //{
+        //    endpoints.MapRazorPages();
+        //});
+    }
+}
+#endregion
 #endif
 
