00ef151adb635551efa40fde59c7bbb2c82ae412
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2021-11-17 12:22:13 +0000

    Restore < 6.0 version

--- aspnetcore/mvc/controllers/filters/samples/6.0/FiltersSample/Controllers/ResponseHeaderController.cs
+++ aspnetcore/mvc/controllers/filters/samples/6.0/FiltersSample/Controllers/ResponseHeaderController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FiltersSample.Controllers;
 
+// <snippet_Class>
 // <snippet_ClassIndex>
 [ResponseHeader("X-Filter-Header", "Filter Value")]
 public class ResponseHeaderController : ControllerBase
@@ -12,4 +13,9 @@ public class ResponseHeaderController : ControllerBase
 
     // ...
     // </snippet_ClassIndex>
+
+    [ResponseHeader("X-Another-Filter-Header", "Another Filter Value")]
+    public IActionResult Multiple() =>
+        Content("Examine the response headers using the F12 developer tools.");
 }
+// </snippet_Class>
