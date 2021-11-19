fa3dee7844b06a19ba08030aa6d6f1e75762e481
Rick Anderson <riande@microsoft.com>
2021-11-19 08:37:30 -1000

    fix sample

--- aspnetcore/fundamentals/dependency-injection/samples/6.x/DIsample2/DIsample2/Program.cs
+++ aspnetcore/fundamentals/dependency-injection/samples/6.x/DIsample2/DIsample2/Program.cs
@@ -1,4 +1,4 @@
-#define SECOND // FIRST SECOND 
+#define THRD // FIRST SECOND THRD
 #if FIRST
 #region snippet
 using DIsample2.Services;
@@ -40,6 +40,37 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddSingleton<Service1>();
 builder.Services.AddSingleton<Service2>();
+#endregion
+
+var myKey = builder.Configuration["MyKey"];
+builder.Services.AddSingleton<IService3>(sp => new Service3(myKey));
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+     app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+#elif THRD
+using DIsample2.Services;
+#region snippet3
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton(new Service1());
+builder.Services.AddSingleton(new Service2());
 #endregion
 
 var myKey = builder.Configuration["MyKey"];
