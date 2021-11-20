dea887123569fde0541e5453345d8afe58641cf9
Rick Anderson <riande@microsoft.com>
2021-11-19 15:26:50 -1000

    Update Win Auth to V6 /3

---
+++ aspnetcore/security/authentication/windowsauth/6.0samples/WebRPwinAuth/Program.cs
@@ -0,0 +1,38 @@
+#define DEFAULT
+#if NEVER
+#elif DEFAULT
+#region snippet1
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+#endregion
+#endif
