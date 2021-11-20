f4b2703ca6f921e80fc4c9dc5a2fb74da977c8cb
Rick Anderson <riande@microsoft.com>
2021-11-19 16:16:57 -1000

    Update Win Auth to V6 /3

--- aspnetcore/security/authentication/windowsauth/6.0samples/WebRPwinAuth/Program.cs
+++ aspnetcore/security/authentication/windowsauth/6.0samples/WebRPwinAuth/Program.cs
@@ -1,4 +1,4 @@
-#define DEFAULT
+#define IMP  // DEFAULT RBAC LDAP HTS IMP
 #if NEVER
 #elif DEFAULT
 #region snippet1
@@ -35,4 +35,199 @@ app.MapRazorPages();
 
 app.Run();
 #endregion
+#elif RBAC
+#region snippet_rbac
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using System.Runtime.InteropServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+    .AddNegotiate(options =>
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            options.EnableLdap("contoso.com");
+        }
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+#endregion
+#elif LDAP
+#region snippet_ldap
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using System.Runtime.InteropServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+        .AddNegotiate(options =>
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                options.EnableLdap(settings =>
+                {
+                    settings.Domain = "contoso.com";
+                    settings.MachineAccountName = "machineName";
+                    settings.MachineAccountPassword =
+                                      builder.Configuration["Password"];
+                });
+            }
+        });
+#endregion
+
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+#elif HTS
+#region snippet_hts
+using Microsoft.AspNetCore.Server.HttpSys;
+using System.Runtime.InteropServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(HttpSysDefaults.AuthenticationScheme);
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    builder.WebHost.UseHttpSys(options =>
+        {
+            options.Authentication.Schemes =
+                AuthenticationSchemes.NTLM |
+                AuthenticationSchemes.Negotiate;
+            options.Authentication.AllowAnonymous = false;
+        });
+}
+#endregion
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+#elif IMP
+#pragma warning disable CA1416 // Validate platform compatibility
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using System.Security.Principal;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+#region snippet_imp
+app.Run(async (context) =>
+{
+    try
+    {
+        var user = (WindowsIdentity)context.User.Identity!;
+
+        await context.Response
+            .WriteAsync($"User: {user.Name}\tState: {user.ImpersonationLevel}\n");
+
+        WindowsIdentity.RunImpersonated(user.AccessToken, () =>
+        {
+            var impersonatedUser = WindowsIdentity.GetCurrent();
+            var message =
+                $"User: {impersonatedUser.Name}\t" +
+                $"State: {impersonatedUser.ImpersonationLevel}";
+
+            var bytes = Encoding.UTF8.GetBytes(message);
+            context.Response.Body.Write(bytes, 0, bytes.Length);
+        });
+    }
+    catch (Exception e)
+    {
+        await context.Response.WriteAsync(e.ToString());
+    }
+});
+
+#endregion
+#pragma warning restore CA1416 // Validate platform compatibility
 #endif
