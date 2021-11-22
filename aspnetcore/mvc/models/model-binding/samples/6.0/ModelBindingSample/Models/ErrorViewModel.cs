f9b309e594c676f015390fb4d425b9b916632d42
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2021-11-22 15:19:47 +0000

    Use Razor Pages

--- aspnetcore/mvc/models/model-binding/samples/6.0/ModelBindingSample/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ModelBindingSample.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- aspnetcore/mvc/models/model-binding/samples/6.0/ModelBindingSample/Program.cs
+++ aspnetcore/mvc/models/model-binding/samples/6.0/ModelBindingSample/Program.cs
@@ -1,14 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -20,8 +20,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
