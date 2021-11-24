90604217624fd4308c0477f813dcd2d4734d0bc5
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2021-11-24 10:29:49 +0000

    Upgrade Model-Binding to 6.0 (#24019)
    Co-authored-by: Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>

---
+++ aspnetcore/mvc/models/model-binding/samples/6.x/ModelBindingSample/CookieValueProviderFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ModelBindingSample;
+
+public class CookieValueProviderFactory : IValueProviderFactory
+{
+    public Task CreateValueProviderAsync(ValueProviderFactoryContext context)
+    {
+        _ = context ?? throw new ArgumentNullException(nameof(context));
+
+        var cookies = context.ActionContext.HttpContext.Request.Cookies;
+        if (cookies is not null && cookies.Count > 0)
+        {
+            var valueProvider = new CookieValueProvider(
+                BindingSource.ModelBinding,
+                cookies,
+                CultureInfo.InvariantCulture);
+
+            context.ValueProviders.Add(valueProvider);
+        }
+
+        return Task.CompletedTask;
+    }
+}
---
+++ aspnetcore/mvc/models/model-binding/samples/6.x/ModelBindingSample/Snippets/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+
+namespace ModelBindingSample.Snippets;
+
+public static class Program
+{
+    public static void AddValueProviderFactory(WebApplicationBuilder builder)
+    {
+        // <snippet_AddValueProviderFactory>
+        builder.Services.AddControllers(options =>
+        {
+            options.ValueProviderFactories.Add(new CookieValueProviderFactory());
+        });
+        // </snippet_AddValueProviderFactory>
+    }
+
+    public static void ReplaceQueryStringValueProviderFactory(WebApplicationBuilder builder)
+    {
+        // <snippet_ReplaceQueryStringValueProviderFactory>
+        builder.Services.AddControllers(options =>
+        {
+            var index = options.ValueProviderFactories.IndexOf(
+                options.ValueProviderFactories.OfType<QueryStringValueProviderFactory>()
+                    .Single());
+
+            options.ValueProviderFactories[index] =
+                new CultureQueryStringValueProviderFactory();
+        });
+        // </snippet_ReplaceQueryStringValueProviderFactory>
+    }
+
+    public static void AddXmlSerializerFormatters(WebApplicationBuilder builder)
+    {
+        // <snippet_AddXmlSerializerFormatters>
+        builder.Services.AddControllers()
+            .AddXmlSerializerFormatters();
+        // </snippet_AddXmlSerializerFormatters>
+    }
+
+    public static void ExcludeSuppressModelBinding(WebApplicationBuilder builder)
+    {
+        // <snippet_ModelMetadataDetailsProviders>
+        builder.Services.AddRazorPages()
+            .AddMvcOptions(options =>
+            {
+                options.ModelMetadataDetailsProviders.Add(
+                    new ExcludeBindingMetadataProvider(typeof(Version)));
+                options.ModelMetadataDetailsProviders.Add(
+                    new SuppressChildValidationMetadataProvider(typeof(Guid)));
+            });
+        // </snippet_ModelMetadataDetailsProviders>
+    }
+}
