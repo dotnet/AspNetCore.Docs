7461435e94076300a2726e35beed4372995d55e7
Rick Anderson <riande@microsoft.com>
2021-11-27 09:50:05 -1000

    update Roles for V6

--- aspnetcore/security/authorization/roles/samples/6_0/WebAll/Controllers/SalaryController.cs
+++ aspnetcore/security/authorization/roles/samples/6_0/WebAll/Controllers/SalaryController.cs
@@ -12,26 +12,3 @@ namespace WebAll.Controllers
     }
     #endregion
 }
-
-/*
- * using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-
-namespace WebAll.Controllers
-{
-    #region snippet2
-    #region snippet
-    [Authorize(Roles = "HRManager,Finance")]
-    public class SalaryController : Controller
-    {
-        public IActionResult Payslip() =>
-                        Content("Payslip");
-        #endregion
-
-        [Authorize(Policy = "HumanResources")]
-        public IActionResult UpdateSalary() =>
-                    Content("HumanResources");
-    }
-    #endregion
-}
-*/
