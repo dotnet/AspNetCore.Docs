ce646202963e9c88ac7aa7d5a7d6011fe12e7754
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2021-11-29 11:51:50 +0000

    Update Handle Errors to 6.0

---
+++ aspnetcore/fundamentals/error-handling/samples/6.x/ErrorHandlingSample/Snippets/Program.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ErrorHandlingSample.Snippets;
+
+public static class Program
+{
+    public static void UseExceptionHandlerInline(WebApplication app)
+    {
+        // <snippet_UseExceptionHandlerInline>
+        app.UseExceptionHandler(exceptionHandlerApp =>
+        {
+            exceptionHandlerApp.Run(async context =>
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                // using static System.Net.Mime.MediaTypeNames;
+                context.Response.ContentType = Text.Plain;
+
+                await context.Response.WriteAsync("An exception was thrown.");
+
+                var exceptionHandlerPathFeature =
+                    context.Features.Get<IExceptionHandlerPathFeature>();
+
+                if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+                {
+                    await context.Response.WriteAsync(" The file was not found.");
+                }
+
+                if (exceptionHandlerPathFeature?.Path == "/")
+                {
+                    await context.Response.WriteAsync(" Page: Home.");
+                }
+            });
+        });
+        // </snippet_UseExceptionHandlerInline>
+    }
+
+    public static void UseStatusCodePages(WebApplicationBuilder builder)
+    {
+        // <snippet_UseStatusCodePages>
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseStatusCodePages();
+        // </snippet_UseStatusCodePages>
+    }
+
+    public static void UseStatusCodePagesContent(WebApplicationBuilder builder)
+    {
+        // <snippet_UseStatusCodePagesContent>
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        // using static System.Net.Mime.MediaTypeNames;
+        app.UseStatusCodePages(Text.Plain, "Status Code Page: {0}");
+        // </snippet_UseStatusCodePagesContent>
+    }
+
+    public static void UseStatusCodePagesInline(WebApplicationBuilder builder)
+    {
+        // <snippet_UseStatusCodePagesInline>
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseStatusCodePages(async statusCodeContext =>
+        {
+            // using static System.Net.Mime.MediaTypeNames;
+            statusCodeContext.HttpContext.Response.ContentType = Text.Plain;
+
+            await statusCodeContext.HttpContext.Response.WriteAsync(
+                $"Status Code Page: {statusCodeContext.HttpContext.Response.StatusCode}");
+        });
+        // </snippet_UseStatusCodePagesInline>
+    }
+
+    public static void UseStatusCodePagesRedirect(WebApplicationBuilder builder)
+    {
+        // <snippet_UseStatusCodePagesRedirect>
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseStatusCodePagesWithRedirects("/StatusCode/{0}");
+        // </snippet_UseStatusCodePagesRedirect>
+    }
+
+    public static void UseStatusCodePagesReExecute(WebApplicationBuilder builder)
+    {
+        // <snippet_UseStatusCodePagesReExecute>
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseStatusCodePagesWithReExecute("/StatusCode/{0}");
+        // </snippet_UseStatusCodePagesReExecute>
+    }
+}
