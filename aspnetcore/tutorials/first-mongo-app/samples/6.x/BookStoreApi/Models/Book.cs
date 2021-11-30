ef58f38709e1d1dabea394306ae2e61b8f8061aa
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2021-11-30 12:36:00 +0000

    Update MongoDB Web Api to 6.0

---
+++ aspnetcore/tutorials/first-mongo-app/samples/6.x/BookStoreApi/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿// THIS FILE USES SNIPPET RANGES.
+
+// <snippet_UsingSystemTextJsonSerialization>
+using System.Text.Json.Serialization;
+// </snippet_UsingSystemTextJsonSerialization>
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStoreApi.Models;
+
+public class Book
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = null!;
+
+    // <snippet_BookName>
+    [BsonElement("Name")]
+    [JsonPropertyName("Name")]
+    public string BookName { get; set; } = null!;
+    // </snippet_BookName>
+
+    public decimal Price { get; set; }
+
+    public string Category { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+}
