69503bfd757ccc43d7ecb09acfed20a27d20b4f9
suresh mandia <sureshmandia@yahoo.in>
2021-12-09 21:13:50 +0100

    updated the code in program.cs and deleted the StartupJavaScript.cs

--- aspnetcore/tutorials/first-web-api/samples/6.0/TodoApi/Program.cs
+++ aspnetcore/tutorials/first-web-api/samples/6.0/TodoApi/Program.cs
@@ -1,28 +1,22 @@
+#region snippet
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
-
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
-//});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    //app.UseSwagger();
-    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
@@ -31,3 +25,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+#endregion
