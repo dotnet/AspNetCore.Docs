e58210dce9d69d7e36ccf50cb8187638ffc2c702
Fiyaz Hasan <fiyazhasan@live.com>
2021-12-14 09:06:46 +0600

    Update Program60UseExtensions.cs

--- aspnetcore/fundamentals/middleware/index/snapshot/Program60UseExtensions.cs
+++ aspnetcore/fundamentals/middleware/index/snapshot/Program60UseExtensions.cs
@@ -1,25 +1,5 @@
-using System.Globalization;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-/* next is type of Func<Task> */
-
-#region snippet1
-app.Use(async (context, next) =>
-{
-    var cultureQuery = context.Request.Query["culture"];
-    if (!string.IsNullOrWhiteSpace(cultureQuery))
-    {
-        var culture = new CultureInfo(cultureQuery);
-
-        CultureInfo.CurrentCulture = culture;
-        CultureInfo.CurrentUICulture = culture;
-    }
-
-    await next();
-});
-#endregion
 
 /* 
     1. next is type of RequestDelegate which requires passing the HttpContext
@@ -27,37 +7,11 @@ app.Use(async (context, next) =>
     3. Saves two per request allocations over the previous Use extension
 */
 
-#region snippet2
+#region snippet1
 app.Use(async (context, next) =>
 {
-    var cultureQuery = context.Request.Query["culture"];
-    if (!string.IsNullOrWhiteSpace(cultureQuery))
-    {
-        var culture = new CultureInfo(cultureQuery);
-
-        CultureInfo.CurrentCulture = culture;
-        CultureInfo.CurrentUICulture = culture;
-    }
-
-    await next(context);
-});
-#endregion
-
-/*
-    1. next is not used i.e. it is a terminal middleware
-    2. Every other middleware after a terminal middleware is skipped
-    1. Use should be replaced with Run or it will throw a compile time error (CS0121: ambiguous)
-
-    app.Use(async (context, next) => {
-        await context.Response.WriteAsync(
-            $"Hello {CultureInfo.CurrentCulture.DisplayName}");
-    });
-*/
-
-#region snippet3
-app.Run(async (context) =>
-{
-    await context.Response.WriteAsync(
-        $"Hello {CultureInfo.CurrentCulture.DisplayName}");
+    // Do work that doesn't write to the Response.
+    await next.Invoke(context);
+    // Do logging or other work that doesn't write to the Response.
 });
 #endregion
