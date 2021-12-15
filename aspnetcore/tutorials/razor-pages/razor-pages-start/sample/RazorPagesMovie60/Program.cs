b435c9cdad5e7af7b943e9c4f41f73c21cb139e4
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2021-12-14 16:56:59 -1000

    Update Program.cs

--- aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie60/Program.cs
+++ aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie60/Program.cs
@@ -54,11 +54,7 @@ builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
