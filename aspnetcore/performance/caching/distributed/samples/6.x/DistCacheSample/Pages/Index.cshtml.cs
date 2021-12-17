05675b9578b24e29c222679f392718cb2bb041b2
suresh mandia <sureshmandia@yahoo.in>
2021-12-18 00:21:48 +0100

    updated the program.cs for  distributed caching cases.

--- aspnetcore/performance/caching/distributed/samples/6.x/DistCacheSample/Pages/Index.cshtml.cs
+++ aspnetcore/performance/caching/distributed/samples/6.x/DistCacheSample/Pages/Index.cshtml.cs
@@ -1,20 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
 
 namespace SampleApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly IDistributedCache _cache;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IDistributedCache cache)
         {
-            _logger = logger;
+            _cache = cache;
         }
 
-        public void OnGet()
-        {
+        public string? CachedTimeUTC { get; set; }
+        public string? ASP_Environment { get; set; }
 
+        public async Task OnGetAsync()
+        {
+            CachedTimeUTC = "Cached Time Expired";
+            var encodedCachedTimeUTC = await _cache.GetAsync("cachedTimeUTC");
+
+            if (encodedCachedTimeUTC != null)
+            {
+                CachedTimeUTC = Encoding.UTF8.GetString(encodedCachedTimeUTC);
+            }
+
+            ASP_Environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (String.IsNullOrEmpty(ASP_Environment))
+            {
+                ASP_Environment = "Null, so Production";
+            }
+        }
+
+        public async Task<IActionResult> OnPostResetCachedTime()
+        {
+            var currentTimeUTC = DateTime.UtcNow.ToString();
+            byte[] encodedCurrentTimeUTC = Encoding.UTF8.GetBytes(currentTimeUTC);
+            var options = new DistributedCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(20));
+            await _cache.SetAsync("cachedTimeUTC", encodedCurrentTimeUTC, options);
+
+            return RedirectToPage();
         }
     }
 }
