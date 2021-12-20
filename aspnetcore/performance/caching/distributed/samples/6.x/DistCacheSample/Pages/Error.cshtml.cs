21466dd8d618f463cbf726096e93530e21b0fd84
smandia <sureshmandia@yahoo.in>
2021-12-20 20:04:55 +0100

    new solution added for distributing caching for  (#24363)
    * Added the dotnet 6 sample app for distributed cache in folder 6.x
    
    * updated the program.cs for  distributed caching cases.

---
+++ aspnetcore/performance/caching/distributed/samples/6.x/DistCacheSample/Pages/Error.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace SampleApp.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
