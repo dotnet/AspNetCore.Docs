5c7591322d87e01e5f5160b2f2f0dbdf7f825fb7
smandia <sureshmandia@yahoo.in>
2021-12-28 22:54:15 +0100

    fixes #24121 , documentation update distributed caching (#24400)
    * fixes #24121 , documentation update distributed caching
    
    * reverted the below dot net 6 moniker range
    
    * moved the NCache related code in program.cs
    
    * removed the extra DI

--- aspnetcore/performance/caching/distributed/samples/6.x/DistCacheSample/Program.cs
+++ aspnetcore/performance/caching/distributed/samples/6.x/DistCacheSample/Program.cs
@@ -1,4 +1,5 @@
-#define SQLServer // Redis
+#define SQLServer //NCache, // Redis
+using Alachisoft.NCache.Caching.Distributed;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.SqlServer;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
@@ -8,12 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-
 if (builder.Environment.IsDevelopment())
 {
     #region snippet_AddDistributedMemoryCache
     builder.Services.AddDistributedMemoryCache();
-    builder.Services.AddSingleton<IDistributedCache, MemoryDistributedCache>();
     #endregion
 }
 else
@@ -26,23 +25,31 @@ else
         options.SchemaName = "dbo";
         options.TableName = "TestCache";
     });
-    builder.Services.AddSingleton<IDistributedCache, SqlServerCache>();
     #endregion
-#else
+
+#elif Redis
     #region snippet_AddStackExchangeRedisCache
-                builder.Services.AddStackExchangeRedisCache(options =>
+    builder.Services.AddStackExchangeRedisCache(options =>
                 {
                     options.Configuration = builder.Configuration.GetConnectionString("MyRedisConStr");
                     options.InstanceName = "SampleInstance";
                 });
-                builder.Services.AddSingleton<IDistributedCache, RedisCache>();
+    #endregion
+#else
+    #region snippet_AddNCache_Cache
+    builder.Services.AddNCacheDistributedCache(configuration =>
+    {
+        configuration.CacheName = "democache";
+        configuration.EnableLogs = true;
+        configuration.ExceptionsEnabled = true;
+    });
     #endregion
 #endif
 }
 
-
 var app = builder.Build();
 
+#region snippet_Configure
 app.Lifetime.ApplicationStarted.Register(() =>
 {
     var currentTimeUTC = DateTime.UtcNow.ToString();
@@ -51,6 +58,7 @@ app.Lifetime.ApplicationStarted.Register(() =>
         .SetSlidingExpiration(TimeSpan.FromSeconds(20));
     app.Services.GetService<IDistributedCache>().Set("cachedTimeUTC", encodedCurrentTimeUTC, options);
 });
+#endregion
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
