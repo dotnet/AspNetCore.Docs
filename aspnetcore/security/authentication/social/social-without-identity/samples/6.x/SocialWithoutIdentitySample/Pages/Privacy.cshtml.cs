a533eb4eda6169ac324f2c24026b5a3731ac872b
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2022-01-11 21:41:03 +0000

    Update Social without Identity to 6.0 (#24563)

---
+++ aspnetcore/security/authentication/social/social-without-identity/samples/6.x/SocialWithoutIdentitySample/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocialWithoutIdentitySample.Pages;
+
+// <snippet_Class>
+[Authorize]
+public class PrivacyModel : PageModel
+{
+
+}
+// </snippet_Class>
