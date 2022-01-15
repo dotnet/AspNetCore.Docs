06b76052cf1932f82f5a7a3c86a0e7eef6486a70
Rick Anderson <riande@microsoft.com>
2022-01-14 15:36:04 -1000

    .Net 6 auth with specific scheme /3

--- aspnetcore/security/authorization/limitingidentitybyscheme/samples/AuthScheme/Controllers/RegistrationController.cs
+++ aspnetcore/security/authorization/limitingidentitybyscheme/samples/AuthScheme/Controllers/RegistrationController.cs
@@ -5,12 +5,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace AuthScheme.Controllers;
 [Authorize(Policy = "Over18")]
 public class RegistrationController : Controller
-    {
+{
     // Do Registration
     #endregion
     public IActionResult Index()
-        {
-            return View();
-        }
+    {
+        return View();
     }
+}
 
