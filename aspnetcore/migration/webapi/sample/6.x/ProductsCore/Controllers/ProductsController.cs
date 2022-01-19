385f493b51cbf28372808b0fb369226b2b9474e5
Wade Pickett <wpickett@microsoft.com>
2022-01-18 20:56:25 -0800

    Added snippet examples for 6.0, review related changes

---
+++ aspnetcore/migration/webapi/sample/6.x/ProductsCore/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿#region snippet_ProductsController
+using ProductsCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        Product[] products = new Product[] 
+        { 
+            new Product
+            {
+                Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1
+            }, 
+            new Product
+            {
+                Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M
+            }, 
+            new Product
+            {
+                Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M
+            } 
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+    }
+}
+#endregion
