a3724f2173e0c248d60a87b3e2db76295bb2c726
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-02-14 12:11:52 -1000

    work

---
+++ aspnetcore/security/authentication/accconfirm/sample/WebPWrecover30/TokenProviders/CustomTokenProvider.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace WebPWrecover.TokenProviders
+{
+    #region snippet1
+    public class CustomEmailConfirmationTokenProvider<TUser>
+                                           : DataProtectorTokenProvider<TUser> where TUser : class
+    {
+        public CustomEmailConfirmationTokenProvider(IDataProtectionProvider dataProtectionProvider,
+            IOptions<EmailConfirmationTokenProviderOptions> options,
+            ILogger<DataProtectorTokenProvider<TUser>> logger)
+                                              : base(dataProtectionProvider, options, logger)
+        {
+
+        }
+    }
+    public class EmailConfirmationTokenProviderOptions : DataProtectionTokenProviderOptions
+    {
+        public EmailConfirmationTokenProviderOptions()
+        {
+            Name = "EmailDataProtectorTokenProvider";
+            TokenLifespan = TimeSpan.FromHours(4);
+        }
+    }
+    #endregion
+
+    public class CustomPasswordResetTokenProvider<TUser> : DataProtectorTokenProvider<TUser> where TUser : class
+    {
+        public CustomPasswordResetTokenProvider(IDataProtectionProvider dataProtectionProvider,
+            IOptions<PasswordResetTokenProviderOptions> options, 
+            ILogger<DataProtectorTokenProvider<TUser>> logger) 
+            : base(dataProtectionProvider, options, logger)
+        {
+
+        }
+    }
+    public class PasswordResetTokenProviderOptions : DataProtectionTokenProviderOptions
+    {
+        public PasswordResetTokenProviderOptions()
+        {
+            Name = "PasswordResetDataProtectorTokenProvider";
+            TokenLifespan = TimeSpan.FromHours(3);
+        }
+    }
+}
