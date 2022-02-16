143c120d6b571b17ee49e205333924df5aaf8adb
toth.alex <toth.alex@hmei.hu>
2022-02-16 08:23:20 +0100

    windows service using minimal api

---
+++ aspnetcore/host-and-deploy/windows-service/samples/6.x/WebAppServiceSample/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SampleApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("Logged from the IndexModel.OnGet method at {Time}", DateTimeOffset.Now);
+        }
+    }
+}
---
+++ aspnetcore/host-and-deploy/windows-service/samples/6.x/WebAppServiceSample/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Hosting.WindowsServices;
+using SampleApp.Services;
+
+var options = new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
+};
+
+var builder = WebApplication.CreateBuilder(options);
+builder.Services.AddRazorPages();
+builder.Services.AddHostedService<ServiceA>();
+builder.Services.AddHostedService<ServiceB>();
+
+builder.Host.UseWindowsService();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
+await app.RunAsync();
