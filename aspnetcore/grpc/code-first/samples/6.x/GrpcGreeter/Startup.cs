23fb9e67d319a5a8b272d8ae1caa377cd2dc7859
wadepickett <10985336+wadepickett@users.noreply.github.com>
2022-02-18 22:25:34 -0800

    v6 prep code first grpc

---
+++ aspnetcore/grpc/code-first/samples/6.x/GrpcGreeter/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProtoBuf.Grpc.Server;
+
+namespace GrpcGreeter
+{
+    public class Startup
+    {
+        #region snippet
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCodeFirstGrpc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>();
+            });
+        }
+        #endregion
+    }
+}
