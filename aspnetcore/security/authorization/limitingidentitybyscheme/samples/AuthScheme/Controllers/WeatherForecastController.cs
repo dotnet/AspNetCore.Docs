f498487d61f459ac37312f7105e63c824c803637
Sepehr Eslami <eslamisepehr@gmail.com>
2022-02-25 11:44:58 +0330

    Clean up indentation

--- aspnetcore/security/authorization/limitingidentitybyscheme/samples/AuthScheme/Controllers/WeatherForecastController.cs
+++ aspnetcore/security/authorization/limitingidentitybyscheme/samples/AuthScheme/Controllers/WeatherForecastController.cs
@@ -10,8 +10,8 @@ namespace AuthScheme.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
