40a8a9fd6e7b15d00eabec29bca48cbd005815aa
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-03-14 14:12:43 -1000

    Mon prep for User Data /2

---
+++ aspnetcore/security/authentication/add-user-data/samples/6.x/SampleApp/Areas/Identity/Data/WebApp1User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApp1.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the WebApp1User class
+public class WebApp1User : IdentityUser
+{
+}
+
