4decc691f771095a409f92ca0808a4dd636ed5ed
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-03-18 16:37:55 +0300

    Add PO Localization sample for .NET Core 6

---
+++ aspnetcore/fundamentals/localization/sample/6.x/POLocalization/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddPortableObjectLocalization();
+
+builder.Services.Configure<RequestLocalizationOptions>(options => options.AddSupportedCultures("fr", "cs"));
+
+builder.Services
+    .AddRazorPages()
+    .AddViewLocalization();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseRouting();
+app.UseStaticFiles();
+
+app.UseRequestLocalization();
+
+app.MapRazorPages();
+
+app.Run();
---
+++ aspnetcore/fundamentals/localization/sample/6.x/POLocalization/wwwroot/lib/bootstrap/dist/css/slash-lg.svg
@@ -0,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-slash-lg" viewBox="0 0 16 16">
+  <path d="M14.707 1.293a1 1 0 0 1 0 1.414l-12 12a1 1 0 0 1-1.414-1.414l12-12a1 1 0 0 1 1.414 0z"/>
+</svg>
