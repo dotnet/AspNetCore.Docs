cf980b3b49eddc24caa922528d4efb2462ce2ff0
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-03-22 16:06:24 -1000

    Moniker prep for Areas /2 (#25394)
    * Moniker prep for Areas /2
    
    * Moniker prep for Areas /2

---
+++ aspnetcore/mvc/controllers/areas/60samples/MVCareas/Areas/Products/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Docs.Samples;
+
+namespace MVCareas.Areas.Products.Controllers
+{
+    [Area("Products")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["routeInfo"] = ControllerContext.MyDisplayRouteInfo();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["routeInfo"] = ControllerContext.MyDisplayRouteInfo();
+            return View();
+        }
+    }
+}
---
+++ aspnetcore/mvc/controllers/areas/60samples/MVCareas/Areas/Products/Controllers/ManageController.cs
@@ -0,0 +1,25 @@
+﻿#region snippet
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Docs.Samples;
+
+namespace MVCareas.Areas.Products.Controllers
+{
+    #region snippet2
+    [Area("Products")]
+    public class ManageController : Controller
+    {
+        #endregion
+        public IActionResult Index()
+        {
+            ViewData["routeInfo"] = ControllerContext.MyDisplayRouteInfo();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["routeInfo"] = ControllerContext.MyDisplayRouteInfo();
+            return View();
+        }
+    }
+}
+#endregion
