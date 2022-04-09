66725375ffc241ce0e27d840717a78caacd9f3fd
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-04-08 15:24:02 -1000

    Prep View Components for .NET 6

---
+++ aspnetcore/mvc/views/view-components/sample6.x/ViewComponentSample/ViewComponents/PriorityListViewComponentFinal.cs
@@ -0,0 +1,44 @@
+﻿#define Final
+#if Final
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViewComponentSample.Models;
+
+namespace ViewComponentSample.ViewComponents
+{
+    public class PriorityListViewComponent : ViewComponent
+    {
+        private readonly ToDoContext db;
+
+        public PriorityListViewComponent(ToDoContext context)
+        {
+            db = context;
+        }
+       
+        private Task<List<TodoItem>> GetItemsAsync(int maxPriority, bool isDone)
+        {
+            return db.ToDo.Where(x => x.IsDone == isDone &&
+                                 x.Priority <= maxPriority).ToListAsync();
+        }
+        #region snippet1
+        public async Task<IViewComponentResult> InvokeAsync(
+            int maxPriority, bool isDone)
+        {
+            string MyView = "Default";
+            // If asking for all completed tasks, render with the "PVC" view.
+            if (maxPriority > 3 && isDone == true)
+            {
+                MyView = "PVC";
+            }
+            var items = await GetItemsAsync(maxPriority, isDone);
+            return View(MyView, items);
+        }
+        #endregion
+    }
+}
+
+#endif
