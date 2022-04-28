36cd31d644a7105e5b18ebf625d711f3f2014e7f
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-04-27 15:13:37 -1000

    Ra/shawnwilde/static files (#25743)
    * Added Service Static Files from multiple paths
    
    * shawnwilde static files /1
    
    * shawnwilde static files /1
    
    * shawnwilde static files /1
    
    * shawnwilde static files /1
    
    * shawnwilde static files /1
    
    * shawnwilde static files /1
    
    * shawnwilde static files /1
    
    * shawnwilde static files /1
    
    Co-authored-by: Shawn Wildermuth <shawn@wildermuth.com>

---
+++ aspnetcore/fundamentals/static-files/samples/6.x/StaticFilesSample/Pages/Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StaticFilesSample.Pages
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
