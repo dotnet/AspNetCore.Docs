6ec1983dc0b98e220f63353f5c9a1ea78adaaf76
James Newton-King <james@newtonking.com>
2022-05-13 07:47:23 +0800

    Update Startup.cs

--- aspnetcore/grpc/httpapi/Startup.cs
+++ aspnetcore/grpc/httpapi/Startup.cs
@@ -2,20 +2,10 @@
 public void ConfigureServices(IServiceCollection services)
 {
     services.AddGrpc().AddJsonTranscoding();
-    services.AddSwaggerGen(c =>
-    {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-    });
 }
 
 public void Configure(IApplicationBuilder app)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
-
     app.UseRouting();
 
     app.UseEndpoints(endpoints =>
