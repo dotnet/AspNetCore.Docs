eb221dd9ad89af129d3f21f7325cedf0321fc83f
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-05-13 12:24:07 -1000

    moniker prep for Min API's .NET 7 (#25861)
    * moniker prep for Min API's .NET 7
    
    * work

---
+++ aspnetcore/fundamentals/minimal-apis/7.0-samples/WebMinAPIs/ResultsExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mime;
+using System.Text;
+static class ResultsExtensions
+{
+    public static IResult Html(this IResultExtensions resultExtensions, string html)
+    {
+        ArgumentNullException.ThrowIfNull(resultExtensions);
+
+        return new HtmlResult(html);
+    }
+}
+
+class HtmlResult : IResult
+{
+    private readonly string _html;
+
+    public HtmlResult(string html)
+    {
+        _html = html;
+    }
+
+    public Task ExecuteAsync(HttpContext httpContext)
+    {
+        httpContext.Response.ContentType = MediaTypeNames.Text.Html;
+        httpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(_html);
+        return httpContext.Response.WriteAsync(_html);
+    }
+}
