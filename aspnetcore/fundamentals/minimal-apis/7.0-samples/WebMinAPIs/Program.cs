5dffd2ae2417f11644d69a1daf5dec819fac5b78
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-05-13 15:50:04 -1000

    OpenAPI in minimal web APIs .NET 7 /1

--- aspnetcore/fundamentals/minimal-apis/7.0-samples/WebMinAPIs/Program.cs
+++ aspnetcore/fundamentals/minimal-apis/7.0-samples/WebMinAPIs/Program.cs
@@ -1,4 +1,4 @@
-#define Default // Default CREATE P1 PM PE I1 I0 IP CERT CERT2 CERT3 RE CONFIG LOG #i REB 
+#define SWAG // Default CREATE P1 PM PE I1 I0 IP CERT CERT2 CERT3 RE CONFIG LOG #i REB 
 // CONFIGB LOGB IWHB DEP R1 LE LF IM SM NR NR2 RP WILD PBG PBP EPB OP1 OP2 OP3 OP4
 // CB BA CJSON MULTI STREAM XTN AUTH1 AUTH2 AUTH3 AUTH4 CORS CORS2 SWAG SWAG2 
 // FIL2 IHB CHNGR ADDMID
@@ -767,6 +767,8 @@ app.Run();
 #elif SWAG
 // GET /swagger
 #region snippet_swag
+using Microsoft.AspNetCore.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -785,7 +787,9 @@ if (app.Environment.IsDevelopment())
                                     $"{builder.Environment.ApplicationName} v1"));
 }
 
-app.MapGet("/swag", () => "Hello Swagger!");
+app.MapGet("/swag", () => "Hello Swagger!")
+                                          .WithName("GetSwagger")
+                                          .WithOpenApi();
 
 app.Run();
 #endregion
