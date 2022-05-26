247bd5a1cd52c2467de95ad2503a49a65a81e86a
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-05-26 11:58:11 -1000

    Ra/display/editor/templates (#25959)
    * adds draft of DisplayTemplates section
    
    * adds link to EditorFor docs
    
    * fixes github id and date
    
    * Changes redundant wording
    
    * Update aspnetcore/mvc/views/display-templates.md
    
    * Editor and display templates /4
    
    * Editor and display templates /4
    
    * Editor and display templates /4
    
    * Editor and display templates /4
    
    * Editor and display templates /4
    
    * Editor and display templates /4
    
    * Editor and display templates /4
    
    * Editor and display templates /4
    
    Co-authored-by: alexwichti <alexwichti@gmail.com>
    Co-authored-by: Alexander Wicht <148378+Ducki@users.noreply.github.com>

---
+++ aspnetcore/mvc/views/display-templates/sample/Pages/Adr/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebAddress.Data;
+
+namespace WebAddress.Pages.Adr
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebAddress.Data.WebAddressContext _context;
+
+        public IndexModel(WebAddress.Data.WebAddressContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Address> Address { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Address != null)
+            {
+                Address = await _context.Address.ToListAsync();
+            }
+        }
+    }
+}
---
+++ aspnetcore/mvc/views/display-templates/sample/Pages/Adr2/DetailsCC.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAddress.Pages.Adr2
+{
+    public class DetailsCCModel : PageModel
+    {
+        private readonly WebAddress.Data.WebAddressContext _context;
+
+        public DetailsCCModel(WebAddress.Data.WebAddressContext context)
+        {
+            _context = context;
+        }
+
+        public Address Address { get; set; } = default!;
+
+        public IActionResult OnGet()
+        {
+            Address = new Address
+            {
+                FirstName = "Rick",
+                MiddleName = "M.",
+                LastName = "Anderson",
+                Street = "123 N 456 W",
+                City = "Seattle",
+                State = "WA",
+                Zipcode = "98009"
+            };
+            return Page();
+        }
+    }
+}
