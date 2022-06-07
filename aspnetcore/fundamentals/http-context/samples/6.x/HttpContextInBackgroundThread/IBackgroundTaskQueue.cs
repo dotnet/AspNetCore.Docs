5c6599e717ac5af17b0d5e409aed3e707de11727
Fiyaz Bin Hasan <fiyazhasan@live.com>
2022-06-07 12:29:46 +0600

    work

--- aspnetcore/fundamentals/http-context/samples/6.x/HttpContextInBackgroundThread/IBackgroundTaskQueue.cs
+++
@@ -1,9 +0,0 @@
-namespace HttpContextInBackgroundThread;
-
-public interface IBackgroundTaskQueue
-{
-    ValueTask QueueBackgroundWorkItemAsync(Func<CancellationToken, ValueTask<string>> workItem);
-
-    ValueTask<Func<CancellationToken, ValueTask<string>>> DequeueAsync(
-        CancellationToken cancellationToken);
-}
--- aspnetcore/fundamentals/http-context/samples/6.x/HttpContextInBackgroundThread/Program.cs
+++ aspnetcore/fundamentals/http-context/samples/6.x/HttpContextInBackgroundThread/Program.cs
@@ -1,22 +1,45 @@
+using System.Text.Json;
 using HttpContextInBackgroundThread;
+using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddSingleton<IEmailService, EmailService>();
-builder.Services.AddHostedService<BulkEmailService>();
-builder.Services.AddHostedService<NewsletterService>();
-builder.Services.AddSingleton<IBackgroundTaskQueue>(ctx =>
+builder.Services.AddHostedService<BranchSyncService>();
+
+builder.Services.AddHttpClient("GitHub", httpClient =>
 {
-    if (!int.TryParse(builder.Configuration["QueueCapacity"], out var queueCapacity))
-        queueCapacity = 100;
-    return new BackgroundTaskQueue(queueCapacity);
-});
+    httpClient.BaseAddress = new Uri("https://api.github.com/");
+
+    // using Microsoft.Net.Http.Headers;
+    // The GitHub API requires two headers. The Use-Agent header will be added dynamically through UserAgentHeaderHandler
+    httpClient.DefaultRequestHeaders.Add(
+        HeaderNames.Accept, "application/vnd.github.v3+json");
+}).AddHttpMessageHandler<UserAgentHeaderHandler>();
+
+builder.Services.AddTransient<UserAgentHeaderHandler>();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/send", (IEmailService service, CancellationToken token) =>
-                                            service.SendEmail(token.ToString()!));
+
+app.MapGet("/branches", async (IHttpClientFactory httpClientFactory) =>
+{
+    var httpClient = httpClientFactory.CreateClient("GitHub");
+    var httpResponseMessage = await httpClient.GetAsync(
+        "repos/dotnet/AspNetCore.Docs/branches");
+
+    if (httpResponseMessage.IsSuccessStatusCode)
+    {
+        using var contentStream =
+            await httpResponseMessage.Content.ReadAsStreamAsync();
+
+        return Results.Ok(await JsonSerializer.DeserializeAsync
+            <IEnumerable<GitHubBranch>>(contentStream));
+    } else
+    {
+        return Results.BadRequest();
+    }
+});
 
 app.Run();
