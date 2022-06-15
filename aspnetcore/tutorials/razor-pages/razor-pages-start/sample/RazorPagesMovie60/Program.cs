f4b65dff1b0b8516153c91f2c798b6408065df99
Wade Pickett <wpickett@microsoft.com>
2022-06-14 21:17:17 -0700

    Program.cs - added snippet regions and SQL_Lite version

--- aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie60/Program.cs
+++ aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie60/Program.cs
@@ -1,12 +1,63 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿#define DEFAULT // SQL server is default, SQL_Lite is other
+#if DEFAULT
+#region snippet_all
+#region snippet_di
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
+
+var app = builder.Build();
+#endregion
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+#endregion
+#elif SQL_Lite
+#region snippet_all_sl
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+#region snippet_di_sl
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
+));
+#endregion
 
 var app = builder.Build();
 
@@ -28,3 +79,5 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+#endregion
+#endif
