e81b3e603e29d4fa21f342257b79bae0dd8f0b62
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-06-16 17:31:36 -1000

    work

--- aspnetcore/fundamentals/http-context/samples/6.x/HttpContextInBackgroundThread/Program.cs
+++ aspnetcore/fundamentals/http-context/samples/6.x/HttpContextInBackgroundThread/Program.cs
@@ -1,13 +1,47 @@
+using System.Text.Json;
 using HttpContextInBackgroundThread;
+using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddTransient<IEmailService, EmailService>();
-builder.Services.AddHostedService<NewsletterService>();
+builder.Services.AddHostedService<PeriodicBranchesLoggerService>();
+
+builder.Services.AddHttpClient("GitHub", httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://api.github.com/");
+
+    // The GitHub API requires two headers. The Use-Agent header is added
+    // dynamically through UserAgentHeaderHandler
+    httpClient.DefaultRequestHeaders.Add(
+        HeaderNames.Accept, "application/vnd.github.v3+json");
+}).AddHttpMessageHandler<UserAgentHeaderHandler>();
+
+builder.Services.AddTransient<UserAgentHeaderHandler>();
 
 var app = builder.Build();
 
-app.MapGet("/send", (IEmailService service) => service.SendEmail("microsoft@aka.ms"));
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/branches", async (IHttpClientFactory httpClientFactory, HttpContext context,
+                                                              ILogger<Program> logger) =>
+{
+    var httpClient = httpClientFactory.CreateClient("GitHub");
+    var httpResponseMessage = await httpClient.GetAsync(
+        "repos/dotnet/AspNetCore.Docs/branches");
+
+    if (!httpResponseMessage.IsSuccessStatusCode) 
+        return Results.BadRequest();
+
+    await using var contentStream =
+        await httpResponseMessage.Content.ReadAsStreamAsync();
+
+    var response = await JsonSerializer.DeserializeAsync
+        <IEnumerable<GitHubBranch>>(contentStream);
+
+    app.Logger.LogInformation($"/branches request: {JsonSerializer.Serialize(response)}");
+
+    return Results.Ok(response);
+});
 
 app.Run();
