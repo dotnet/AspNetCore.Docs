9b728a7f016e06fef23a6afbe9b292da77793531
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-06-28 15:40:56 -1000

    Updates the salt hashing example for .NET 6 /4

--- aspnetcore/security/data-protection/consumer-apis/password-hashing/samples/6.x/passwordhasher.cs
+++ aspnetcore/security/data-protection/consumer-apis/password-hashing/samples/6.x/passwordhasher.cs
@@ -1,22 +1,22 @@
 using System.Security.Cryptography;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
-#nullable disable
 
 public class Program
 {
     public static void Main(string[] args)
     {
         Console.Write("Enter a password: ");
-        string password = Console.ReadLine();
+        string? password = Console.ReadLine();
 
-        // generate a 128-bit salt using a cryptographically strong random sequence of nonzero values
+        // Generate a 128-bit salt using a cryptographically strong
+        // random sequence of nonzero values.
         byte[] salt = new byte[128 / 8];
         RandomNumberGenerator.Create().GetNonZeroBytes(salt);
         Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
 
         // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
         string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-            password: password,
+            password: password!,
             salt: salt,
             prf: KeyDerivationPrf.HMACSHA256,
             iterationCount: 100000,
