52a029f98bc1d812dfdbe3bb84e8f3371ca4bbeb
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-06-30 13:39:35 -1000

    Identity api auth/colmprunty/ra (#26278)
    * Update identity-api-authorization.md
    
    In new minimal API that comes from default creation under .NET 6, there isn't a `Startup` class any more. Likewise, there aren't sections for `Startup.ConfigureServices` or `Startup.Configure` any more, everything is within `Program`.  
    I think I've done this right so that anything < .NET 6 will still show the original text, but .NET 6 and up will show the new additions.
    
    * intro to SPAs /1
    
    * intro to SPAs /1
    
    * intro to SPAs /1
    
    Co-authored-by: Colm Prunty <github@colm.prunty.org>

---
+++ aspnetcore/security/authentication/identity-api-authorization/6samples/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using output_directory_name.Data;
+using output_directory_name.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentityServer()
+    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+builder.Services.AddAuthentication()
+    .AddIdentityServerJwt();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseIdentityServer();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
