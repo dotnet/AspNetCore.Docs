765089187171b9a2974e3c455b8df04f340a4ee8
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-07-15 03:22:24 +0100

    added custom controller

--- aspnetcore/fundamentals/minimal-apis/misc-samples/typedResults/TypedResultsApiWithTest/Program.cs
+++ aspnetcore/fundamentals/minimal-apis/misc-samples/typedResults/TypedResultsApiWithTest/Program.cs
@@ -2,5 +2,41 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+// map the /weatherforecast endpoint to a custom action
+app.MapWeatherApi();
 app.Run();
+
+
+
+public static class WeatherApi
+{
+    public static string[] summaries =
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+    public static WebApplication MapWeatherApi(this WebApplication routes)
+    {
+        routes.MapGet("/weatherforecast", GetAllWeathers);
+
+        return routes;
+    }
+    public static IResult GetAllWeathers()
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+      new WeatherForecast
+      (
+          DateTime.Now.AddDays(index),
+          Random.Shared.Next(-20, 55),
+          summaries[Random.Shared.Next(summaries.Length)]
+      ))
+      .ToArray();
+        // return a typed result
+        return TypedResults.Ok(forecast); ;
+    }
+}
+
+public record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
