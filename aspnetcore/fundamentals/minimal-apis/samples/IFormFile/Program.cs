17e329ba526ac736df753745a14367abc5fa1a21
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-07-15 20:35:13 +0100

    added some lines

--- aspnetcore/fundamentals/minimal-apis/samples/IFormFile/Program.cs
+++ aspnetcore/fundamentals/minimal-apis/samples/IFormFile/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+//add cors
+builder.Services.AddCors(option=>{
+    option.AddPolicy("AllowAll", p=>p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+    option.AddDefaultPolicy(p=>p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 var app = builder.Build();
-
+//add cors
+app.UseCors("AllowAll");
 app.MapGet("/", () => "Hello World!");
 
 app.MapPost("/fileUp", async (IFormFile file) =>
@@ -8,6 +14,7 @@ app.MapPost("/fileUp", async (IFormFile file) =>
     string tempfile = Path.GetTempFileName();
     using var stream = File.OpenWrite(tempfile);
     await file.CopyToAsync(stream);
+    return TypedResults.Redirect("/");
 });
 
 app.MapPost("/FilesUp", async (IFormFileCollection myFiles) =>
