13825beeb09dca623eefaf3849c659b93ee7d587
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-07-15 04:09:57 +0100

    added endpoints

--- aspnetcore/fundamentals/minimal-apis/samples/IFormFile/Program.cs
+++ aspnetcore/fundamentals/minimal-apis/samples/IFormFile/Program.cs
@@ -3,4 +3,21 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/fileUp", async (IFormFile file) =>
+{
+    string tempfile = Path.GetTempFileName();
+    using var stream = File.OpenWrite(tempfile);
+    await file.CopyToAsync(stream);
+});
+
+app.MapPost("/FilesUp", async (IFormFileCollection myFiles) =>
+{
+    foreach (var file in myFiles)
+    {
+        string tempfile = Path.GetTempFileName();
+        using var stream = File.OpenWrite(tempfile);
+        await file.CopyToAsync(stream);
+    }
+});
+
 app.Run();
