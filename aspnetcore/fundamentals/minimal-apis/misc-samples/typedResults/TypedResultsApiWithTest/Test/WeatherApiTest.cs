8d990a56e6da471d2a0ac6683d131e60a292a677
sammychinedu2ky <sammychinedu2ky@gmail.com>
2022-07-16 06:04:02 +0100

    removed comment
    Co-authored-by: Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>

--- aspnetcore/fundamentals/minimal-apis/misc-samples/typedResults/TypedResultsApiWithTest/Test/WeatherApiTest.cs
+++ aspnetcore/fundamentals/minimal-apis/misc-samples/typedResults/TypedResultsApiWithTest/Test/WeatherApiTest.cs
@@ -10,7 +10,6 @@ namespace Tests
         public void MapWeatherApiTest()
         {
             var result = WeatherApi.GetAllWeathers();
-            // assert that the result is a typed result of type WeatherForecast[]
             Assert.IsInstanceOfType(result, typeof(Ok<WeatherForecast[]>));
         }
       
