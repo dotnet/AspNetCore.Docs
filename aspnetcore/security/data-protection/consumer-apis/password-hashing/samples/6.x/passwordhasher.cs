15c473d49a2958abc3e430fa5957f455379e9f2f
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-07-19 12:01:55 -1000

    Updates the salt hashing example for .NET 6 /4 (#26253)
    * Updates the salt hashing example for .NET 6 /4
    
    * Updates the salt hashing example for .NET 6 /4
    
    * Updates the salt hashing example for .NET 6 /4
    
    * Updates the salt hashing example for .NET 6 /4
    
    * Updates the salt hashing example for .NET 6 /4
    
    * Replace GetNonZeroBytes with GetBytes
    
    * Add warning about not using to store pw in datastore
    
    * Apply suggestions from code review
    
    Co-authored-by: Levi Broderick <GrabYourPitchforks@users.noreply.github.com>
    
    * react to feedback
    
    * react to feedback
    
    * react to feedback
    
    Co-authored-by: Levi Broderick <GrabYourPitchforks@users.noreply.github.com>

--- aspnetcore/security/data-protection/consumer-apis/password-hashing/samples/6.x/passwordhasher.cs
+++ aspnetcore/security/data-protection/consumer-apis/password-hashing/samples/6.x/passwordhasher.cs
@@ -1,29 +1,23 @@
-using System.Security.Cryptography;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
-#nullable disable
+using System.Security.Cryptography;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.Write("Enter a password: ");
-        string password = Console.ReadLine();
+Console.Write("Enter a password: ");
+string? password = Console.ReadLine();
 
-        // generate a 128-bit salt using a cryptographically strong random sequence of nonzero values
-        byte[] salt = new byte[128 / 8];
-        RandomNumberGenerator.Create().GetNonZeroBytes(salt);
-        Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+// Generate a 128-bit salt using a sequence of
+// cryptographically strong random bytes.
+byte[] salt = RandomNumberGenerator.GetBytes(128 / 8); // divide by 8 to convert bits to bytes
+Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
 
-        // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
-        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-            password: password,
-            salt: salt,
-            prf: KeyDerivationPrf.HMACSHA256,
-            iterationCount: 100000,
-            numBytesRequested: 256 / 8));
-        Console.WriteLine($"Hashed: {hashed}");
-    }
-}
+// derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+    password: password!,
+    salt: salt,
+    prf: KeyDerivationPrf.HMACSHA256,
+    iterationCount: 100000,
+    numBytesRequested: 256 / 8));
+
+Console.WriteLine($"Hashed: {hashed}");
 
 /*
  * SAMPLE OUTPUT
