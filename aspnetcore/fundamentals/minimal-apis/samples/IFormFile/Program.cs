279857af3b4b07cfd5f58379ac73c9b60a80f82a
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-07-19 10:58:46 -1000

    add snippet tags to iFormFile

--- aspnetcore/fundamentals/minimal-apis/samples/IFormFile/Program.cs
+++ aspnetcore/fundamentals/minimal-apis/samples/IFormFile/Program.cs
@@ -3,7 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // End point to support a single file upload
-app.MapGet("/singleFile", () => Results.Text(@"<!DOCTYPE html> <html> <head> <meta charset='utf-8'> </head> <body> <form action='/fileup' method='post' enctype='multipart/form-data'> <input multiple name='formfile' type='file'>Swac <input type='submit'> </form> </body> </html>", "text/html"));
+app.MapGet("/singleFile", () => Results.Text(@"<!DOCTYPE html> <html> <head> <meta charset='utf-8'> 
+</head> <body> <form action='/fileup' method='post' enctype='multipart/form-data'>
+<input multiple name='formfile' type='file'>Swac <input type='submit'> </form>
+</body> </html>", "text/html"));
 
 // End point to support multiple file uploads
 app.MapGet("/multipleFiles", () => Results.Text(@"<!DOCTYPE html> <html> <head> <meta charset='utf-8'> </head> <body> <form action='/filesup' method='post' enctype='multipart/form-data'> <input multiple name='formfile' type='file'>Swac <input type='submit'> </form> </body> </html>", "text/html"));
@@ -12,6 +15,7 @@ app.MapPost("/fileup", async (IFormFile file) =>
 {
     // save a file
     string tempfile = Path.GetTempFileName();
+    tempfile = $@"C:\tmp\{DateTime.Now.Ticks}.jpg";
     using var stream = File.OpenWrite(tempfile);
     await file.CopyToAsync(stream);
     return Results.Accepted();
@@ -23,6 +27,7 @@ app.MapPost("/filesup", async (IFormFileCollection myFiles) =>
     foreach (var file in myFiles)
     {
         string tempfile = Path.GetTempFileName();
+        tempfile = $@"C:\tmp\{DateTime.Now.Ticks}.jpg";
         using var stream = File.OpenWrite(tempfile);
         await file.CopyToAsync(stream);
     }
