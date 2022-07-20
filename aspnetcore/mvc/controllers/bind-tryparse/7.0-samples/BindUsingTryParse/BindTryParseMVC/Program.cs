d3c39c438aa887995acc4f6c107f6b05d187951a
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-07-20 12:00:25 -1000

    Fix datetime culture

--- aspnetcore/mvc/controllers/bind-tryparse/7.0-samples/BindUsingTryParse/BindTryParseMVC/Program.cs
+++ aspnetcore/mvc/controllers/bind-tryparse/7.0-samples/BindUsingTryParse/BindTryParseMVC/Program.cs
@@ -6,16 +6,9 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-//         // GET /WeatherForecast/RangeWithCulture?range=20/07/2022-25/07/2022&culture=en-GB
-
-DateTimeFormatInfo dtfi = CultureInfo.CreateSpecificCulture("en-GB").DateTimeFormat;
-var now = DateTime.Now.ToString("d", dtfi);
-var fiveDays = DateTime.Now.AddDays(5).ToString("d", dtfi);
-var redirectDateRange = $"/WeatherForecast/RangeWithCulture?range={now}" +
-                        $"-{fiveDays}" +
-                        "&culture=en-GB";
-
-app.MapGet("/", () => Results.Redirect(redirectDateRange));
+// GET /en-GB
+// returns an endpoint string for /WeatherForecast/RangeWithCulture
+app.MapGet("/{culture}", (string culture) => DRC.DateRangeCulture(culture, app.Logger));
 
 if (!app.Environment.IsDevelopment())
 {
@@ -33,3 +26,28 @@ app.MapControllerRoute(
     pattern: "{controller=WeatherForecast}/{action=Index}/{id?}");
 
 app.Run();
+
+public static class DRC
+{
+    public static string DateRangeCulture(string culture, ILogger logger)
+    {
+        DateTimeFormatInfo dtfi;
+
+        try
+        {
+            dtfi = CultureInfo.CreateSpecificCulture(culture).DateTimeFormat;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"{culture} is not a valid culture.");
+            logger.LogError(ex.Message);
+            dtfi = CultureInfo.CreateSpecificCulture(CultureInfo.CurrentCulture.Name).DateTimeFormat;
+        }
+        var now = DateTime.Now.ToString("d", dtfi);
+        var fiveDays = DateTime.Now.AddDays(5).ToString("d", dtfi);
+        var dateRangeCulture = $"/WeatherForecast/RangeWithCulture?range={now}" +
+                                $"-{fiveDays}" +
+                                $"&culture={culture}";
+        return dateRangeCulture;
+    }
+}
