a94eb5d2f28ab65c44f1b9b8c141a55321f31648
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-07-22 05:21:08 +0100

    formatted code

--- aspnetcore/fundamentals/minimal-apis/7.0-samples/todo-group/Program.cs
+++ aspnetcore/fundamentals/minimal-apis/7.0-samples/todo-group/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
