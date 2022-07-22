70b10df5bd2a6d94bb984db4194d0bd1b58db98a
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-07-22 04:50:55 +0100

    dbcontext added

---
+++ aspnetcore/fundamentals/minimal-apis/7.0-samples/todo-group/data/DbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; } = default!;
+        public DbSet<Note> Notes { get; set; } = default!;
+
+    }
