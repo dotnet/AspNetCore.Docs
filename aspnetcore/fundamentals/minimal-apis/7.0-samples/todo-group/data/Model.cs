a77b639e33016efd99dd0e92da4c73976b74f84f
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-07-22 04:52:29 +0100

    modified model

--- aspnetcore/fundamentals/minimal-apis/7.0-samples/todo-group/data/Model.cs
+++ aspnetcore/fundamentals/minimal-apis/7.0-samples/todo-group/data/Model.cs
@@ -9,4 +9,5 @@ public class Todo {
 public class Note {
     public int Id {get; set;}
     public string Title {get; set;} = String.Empty;
+    public string Description {get; set;} = String.Empty;
 }
