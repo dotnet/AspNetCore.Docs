0430f662ab3856e5959e749f4069df8dbd4bb07e
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-07-26 15:10:56 -1000

    dotnet user-jwts /1

--- aspnetcore/security/authentication/jwt-authn/samples/MyJWT/Program.cs
+++ aspnetcore/security/authentication/jwt-authn/samples/MyJWT/Program.cs
@@ -1,3 +1,24 @@
+#define FIRST // FIRST SECOND
+#if NEVER
+#elif FIRST
+// <snippet_1>
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("Bearer");
+builder.Authentication.AddJwtBearer();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello, World!");
+app.MapGet("/secret", (ClaimsPrincipal user) => $"Hello {user.Identity?.Name}. This is a secret!")
+    .RequireAuthorization();
+
+app.Run();
+// </snippet_1>
+#elif SECOND
+// <snippet_2>
 using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,3 +35,5 @@ app.MapGet("/secret2", () => "This is a different secret!")
     .RequireAuthorization(p => p.RequireClaim("scope", "myapi:secrets"));
 
 app.Run();
+// </snippet_2>
+#endif
