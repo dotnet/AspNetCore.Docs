be191a89c418e454bbb6ea85abfeca1da1c36fab
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-07-29 09:03:40 +0100

    git format

--- aspnetcore/fundamentals/minimal-apis/7.0-samples/todo-group/data/DbContext.cs
+++ aspnetcore/fundamentals/minimal-apis/7.0-samples/todo-group/data/DbContext.cs
@@ -5,10 +5,7 @@ namespace Data;
 public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
-
+        : base(options){}
     public DbSet<Todo> Todos { get; set; } = default!;
     public DbSet<Note> Notes { get; set; } = default!;
 
