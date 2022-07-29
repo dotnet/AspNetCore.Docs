29b96c203198dfbbfb37af519fe7ae0ee9ad0d5a
sammychinedu2ky <sammychinedu2ky@gmail.com>
2022-07-29 03:39:34 +0100

    added space
    Co-authored-by: Stephen Halter <halter73@gmail.com>

--- aspnetcore/fundamentals/minimal-apis/7.0-samples/todo-group/Dtos.cs
+++ aspnetcore/fundamentals/minimal-apis/7.0-samples/todo-group/Dtos.cs
@@ -1,8 +1,8 @@
 internal class TodoDto
 {
-    public string Title {get; set;} = String.Empty;
-    public string Description {get; set;} = String.Empty;
-    public bool IsDone {get; set;}
+    public string Title { get; set; } = String.Empty;
+    public string Description { get; set; } = String.Empty;
+    public bool IsDone { get; set; }
 }
 
 internal class NoteDto
