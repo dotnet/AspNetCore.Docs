01ac9cfab3face343d7fd519625fbaee1b6613ae
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-08-10 18:52:10 -1000

    Cookie consent /8 (#26700)
    * Cookie consent /8
    
    * Cookie consent /8
    
    * Cookie consent /8

---
+++ aspnetcore/security/gdpr/sample/RP6.0/WebGDPR/Program.cs
@@ -0,0 +1,68 @@
+#define SECOND // FIRST
+#if NEVER
+#elif FIRST
+// <snippet_1>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential 
+    // cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCookiePolicy();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+// </snippet_1>
+#elif SECOND
+// <snippet_2>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+    options.ConsentCookieValue = "true";
+});
+
+var app = builder.Build();
+// </snippet_2>
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCookiePolicy();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+#endif
