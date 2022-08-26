dc5de4ccc967df23902d7ac66a3079f7bcf4a8c1
Wade Pickett <wpickett@microsoft.com>
2022-08-26 13:03:59 -0700

    Added 7.0 sample version with custom httpconnect attribute to allow websocket on http/2

---
+++ aspnetcore/fundamentals/websockets/samples/7.x/WebSocketsSample/Controllers/HttpConnectAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Routing;
+
+public class HttpConnectAttribute : HttpMethodAttribute
+{
+    private static readonly IEnumerable<string> _supportedMethods = new[] { "CONNECT" };
+
+    public HttpConnectAttribute() 
+        : base(_supportedMethods) 
+    { 
+    }
+
+    public HttpConnectAttribute(string template)
+        : base(_supportedMethods, template)
+    {
+    }
+}
---
+++ aspnetcore/fundamentals/websockets/samples/7.x/WebSocketsSample/Controllers/WebSocketController.cs
@@ -0,0 +1,47 @@
+using System.Net.WebSockets;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSocketsSample.Controllers;
+
+// <snippet>
+public class WebSocketController : ControllerBase
+{
+    [HttpConnect("/ws")]
+    public async Task Connect()
+    {
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            await Echo(webSocket);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+    // </snippet>
+
+    private static async Task Echo(WebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4];
+        var receiveResult = await webSocket.ReceiveAsync(
+            new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        while (!receiveResult.CloseStatus.HasValue)
+        {
+            await webSocket.SendAsync(
+                new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+                receiveResult.MessageType,
+                receiveResult.EndOfMessage,
+                CancellationToken.None);
+
+            receiveResult = await webSocket.ReceiveAsync(
+                new ArraySegment<byte>(buffer), CancellationToken.None);
+        }
+
+        await webSocket.CloseAsync(
+            receiveResult.CloseStatus.Value,
+            receiveResult.CloseStatusDescription,
+            CancellationToken.None);
+    }
+}
