383c80575dd2b88a068259b8946bec98e0aa53d0
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-09-01 11:22:25 -1000

    prep for 7.0 changes in Windows Service (#26908)

---
+++ aspnetcore/host-and-deploy/windows-service/samples/7.x/WebAppServiceSample/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Hosting.WindowsServices;
+using SampleApp.Services;
+
+var options = new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService() 
+                                     ? AppContext.BaseDirectory : default
+};
+
+var builder = WebApplication.CreateBuilder(options);
+builder.Services.AddRazorPages();
+builder.Services.AddHostedService<ServiceA>();
+builder.Services.AddHostedService<ServiceB>();
+
+builder.Host.UseWindowsService();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+await app.RunAsync();
