073d72f7da2fb8f75a345aed25c6e8220cdc05c8
Fiyaz Bin Hasan <fiyazhasan@live.com>
2022-09-17 13:29:05 +0600

    bring back todo-group sample

---
+++ aspnetcore/fundamentals/minimal-apis/7.0-samples/todo-group/Data/TodoDto.cs
@@ -0,0 +1,8 @@
+namespace Data;
+
+public class TodoDto
+{
+    public string Title { get; set; } = String.Empty;
+    public string Description { get; set; } = String.Empty;
+    public bool IsDone { get; set; }
+}
