2b68e163697090369fa4744b4a69954233dc54f6
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-09-17 10:29:35 +0100

    used route groups static method

--- aspnetcore/fundamentals/minimal-apis/samples/routeGroups/Program.cs
+++ aspnetcore/fundamentals/minimal-apis/samples/routeGroups/Program.cs
@@ -25,31 +25,10 @@ if (app.Environment.IsDevelopment())
 app.MapGet("/", () => "Hello World!");
 
 // todo endpoints
-var todos = app.MapGroup("/todos").WithTags("Todo Endpoints").AddEndpointFilter(async (context, next) =>
+app.MapGroup("/todos").MapTodosApi().WithTags("Todo Endpoints").AddEndpointFilter(async (context, next) =>
 {
     app.Logger.LogInformation("Accessing todo endpoints");
     return await next(context);
 });
-todos.MapGet("/", RouteHandlers.GetAllTodos);
-todos.MapGet("/{id}", RouteHandlers.GetTodo);
-todos.MapPost("/", RouteHandlers.CreateTodo).AddEndpointFilter(async (context, next) =>
-{
-    // log time taken to process
-    var start = DateTime.Now;
-    var result = await next(context);
-    var end = DateTime.Now;
-    app.Logger.LogInformation($"{context.HttpContext.Request.Path.Value} took {(end - start).TotalMilliseconds}ms");
-    return result;
-});
-todos.MapPut("/{id}", RouteHandlers.UpdateTodo).AddEndpointFilter(async (context, next) =>
-{
-    // log time taken to process
-    var start = DateTime.Now;
-    var result = await next(context);
-    var end = DateTime.Now;
-    app.Logger.LogInformation($"{context.HttpContext.Request.Path.Value} took {(end - start).TotalMilliseconds}ms");
-    return result;
-});
-todos.MapDelete("/{id}", RouteHandlers.DeleteTodo);
 
 app.Run();
