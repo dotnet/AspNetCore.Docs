4b80ff68b8e5937f53e8d5bce3d04f68ceffa62e
Fiyaz Bin Hasan <fiyazhasan@live.com>
2022-09-23 00:56:15 +0600

    remove projects

--- aspnetcore/fundamentals/minimal-apis/7.0-samples/MinApiTestsSample/WebMinRouteGroup/Data/TodoDto.cs
+++
@@ -1,8 +0,0 @@
-namespace WebMinRouteGroup.Data;
-
-public class TodoDto
-{
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public bool IsDone { get; set; }
-}
