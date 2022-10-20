7fa9a8291e54e3d70af012a2500342ce2e6fc86b
Paul Seal <prjseal@gmail.com>
2022-10-20 23:03:49 +0100

    change this sample to use file scoped namespace
    change this sample to use file scoped namespace like the other samples in the page

--- aspnetcore/mvc/views/view-components/sample6.x/ViewComponentSample/ViewComponents/PriorityListViewComponent1.cs
+++ aspnetcore/mvc/views/view-components/sample6.x/ViewComponentSample/ViewComponents/PriorityListViewComponent1.cs
@@ -8,32 +8,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using ViewComponentSample.Models;
 
-namespace ViewComponentSample.ViewComponents
+namespace ViewComponentSample.ViewComponents;
+
+public class PriorityListViewComponent : ViewComponent
 {
-    public class PriorityListViewComponent : ViewComponent
+    private readonly ToDoContext db;
+
+    public PriorityListViewComponent(ToDoContext context)
     {
-        private readonly ToDoContext db;
-
-        public PriorityListViewComponent(ToDoContext context)
-        {
-            db = context;
-        }
-
-        public async Task<IViewComponentResult> InvokeAsync(
-        int maxPriority, bool isDone)
-        {
-            var items = await GetItemsAsync(maxPriority, isDone);
-            return View(items);
-        }
-        #region snippet2
-        private Task<List<TodoItem>> GetItemsAsync(int maxPriority, bool isDone)
-        {
-            return db.ToDo.Where(x => x.IsDone == isDone &&
-                                 x.Priority <= maxPriority).ToListAsync();
-        }
-        #endregion
+        db = context;
     }
+
+    public async Task<IViewComponentResult> InvokeAsync(
+    int maxPriority, bool isDone)
+    {
+        var items = await GetItemsAsync(maxPriority, isDone);
+        return View(items);
+    }
+    #region snippet2
+    private Task<List<TodoItem>> GetItemsAsync(int maxPriority, bool isDone)
+    {
+        return db.ToDo.Where(x => x.IsDone == isDone &&
+                             x.Priority <= maxPriority).ToListAsync();
+    }
+    #endregion
 }
+    
 #endregion
 
 #endif
