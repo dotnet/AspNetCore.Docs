5542b7ad2af883c061bc4775ef8c56d9f5b223be
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-10-20 14:57:57 -1000

    .NET 7: Intro to Web API /2

--- aspnetcore/tutorials/first-web-api/samples/7.0/TodoApi/Controllers/TodoItemsController.cs
+++ aspnetcore/tutorials/first-web-api/samples/7.0/TodoApi/Controllers/TodoItemsController.cs
@@ -23,6 +23,7 @@ namespace TodoApi.Controllers
         }
 
         // GET: api/TodoItems/5
+        // <snippet_GetByID>
         [HttpGet("{id}")]
         public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
         {
@@ -35,6 +36,7 @@ namespace TodoApi.Controllers
 
             return todoItem;
         }
+        // </snippet_GetByID>
 
         // PUT: api/TodoItems/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
