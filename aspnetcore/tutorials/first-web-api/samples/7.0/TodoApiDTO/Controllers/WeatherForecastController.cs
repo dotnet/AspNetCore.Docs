e4a6ffef32685fbf679dddceb20734e39f2abb5c
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-10-21 15:47:14 -1000

    .NET 7: Intro to Web API /2 (#27333)
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * .NET 7: Intro to Web API /2
    
    * work
    
    * work
    
    * work
    
    * work

---
+++ aspnetcore/tutorials/first-web-api/samples/7.0/TodoApiDTO/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
