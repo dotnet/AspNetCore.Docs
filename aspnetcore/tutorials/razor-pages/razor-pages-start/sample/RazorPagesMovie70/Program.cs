751f7259a3c53e3865480a1c3ccb51b2aff062fc
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-10-25 16:58:02 -1000

    .NET 7: RP add a model /2

--- aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie70/Program.cs
+++ aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie70/Program.cs
@@ -35,4 +35,35 @@ app.MapRazorPages();
 app.Run();
 // </snippet_all>
 #elif SQL_Lite
+// </snippet_all_sl>
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+// </snippet_all_sl>
 #endif
