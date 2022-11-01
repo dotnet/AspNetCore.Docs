6ffde459367f8bbaab648863046236c63f3caa50
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-10-31 14:30:41 -1000

    .NET 7 RP intro part 8 /2 (#27446)
    * .NET 7 RP intro part 8 /2
    
    * .NET 7 RP intro part 8 /2
    
    * .NET 7 RP intro part 8 /2
    
    * .NET 7 RP intro part 8 /2
    
    * .NET 7 RP intro part 8 /2

--- aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie70/Models/MovieDateRatingDAmult.cs
+++ aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie70/Models/MovieDateRatingDAmult.cs
@@ -1,32 +1,30 @@
-//#define COMBINED
+#define COMBINED
 #if COMBINED
-#region snippet1
-using System;
+// <snippet1>
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RazorPagesMovie.Models
+namespace RazorPagesMovie.Models;
+
+public class Movie
 {
-    public class Movie
-    {
-        public int ID { get; set; }
+    public int Id { get; set; }
 
-        [StringLength(60, MinimumLength = 3)]
-        public string Title { get; set; } = string.Empty;
+    [StringLength(60, MinimumLength = 3)]
+    public string Title { get; set; } = string.Empty;
 
-        [Display(Name = "Release Date"), DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set; }
+    [Display(Name = "Release Date"), DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
-        public string Genre { get; set; } = string.Empty;
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+    public string Genre { get; set; } = string.Empty;
 
-        [Range(1, 100), DataType(DataType.Currency)]
-        [Column(TypeName = "decimal(18, 2)")]
-        public decimal Price { get; set; }
+    [Range(1, 100), DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
-        public string Rating { get; set; } = string.Empty;
-    }
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+    public string Rating { get; set; } = string.Empty;
 }
-#endregion
+// </snippet1>
 #endif
