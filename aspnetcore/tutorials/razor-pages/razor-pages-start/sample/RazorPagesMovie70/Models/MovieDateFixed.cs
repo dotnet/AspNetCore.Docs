0145c2b891c17c8c390f22cc9639eb58b3c2a627
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-10-31 15:26:41 -1000

    clean up snippet

--- aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie70/Models/MovieDateFixed.cs
+++ aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie70/Models/MovieDateFixed.cs
@@ -1,25 +1,24 @@
-//#define First
+#define First
 #if First
 // <snippet_1>
-using System;
+
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RazorPagesMovie.Models
+namespace RazorPagesMovie.Models;
+
+public class Movie
 {
-    public class Movie
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; } = string.Empty;
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string Genre { get; set; } = string.Empty;
 
-        [Column(TypeName = "decimal(18, 2)")]
-        public decimal Price { get; set; }
-    }
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
 }
 // </snippet_1>
 #endif
