78d557762c689237e33cadb94227e8512d0f79b8
Tom Dykstra <tdykstra@microsoft.com>
2022-11-04 17:39:05 -0700

    Put moniker block 7 on top (#27501)

--- aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie70/Models/Movie.cs
+++ aspnetcore/tutorials/first-mvc-app/start-mvc/sample/MvcMovie70/Models/Movie.cs
@@ -76,22 +76,27 @@ namespace MvcMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
+
     [StringLength(60, MinimumLength = 3)]
     [Required]
     public string? Title { get; set; }
+
     #region DataType
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
+
     [Range(1, 100)]
     [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
-    public decimal Price { get; set; }
+    public decimal Price { get; set; }    
     #endregion
+
     [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
     [Required]
     [StringLength(30)]
     public string? Genre { get; set; }
+    
     [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
     [StringLength(5)]
     [Required]
