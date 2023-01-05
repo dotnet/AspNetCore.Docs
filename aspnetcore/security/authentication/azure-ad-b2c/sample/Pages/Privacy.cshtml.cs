36e707ea394e0c8cbd8aac79e906bf5d57c0210d
damienbod <damien_bod@hotmail.com>
2023-01-05 21:54:13 +0100

    Cloud authentication AAD B2C (#27918)
    * remove version, use the latest default
    
    * Remove default setup, if using B2C, then user flows
    
    * Update _LoginPartial
    
    * Update _Layout
    
    * Update to latest
    
    * Updaate setup
    
    * Add styles
    
    * Update sample
    
    * Update styles
    
    * Update style
    
    * fix naming
    
    * Update aspnetcore/security/authentication/azure-ad-b2c.md
    
    * Review feedback fixes
    
    Co-authored-by: Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>

--- aspnetcore/security/authentication/azure-ad-b2c/sample/Pages/Privacy.cshtml.cs
+++ aspnetcore/security/authentication/azure-ad-b2c/sample/Pages/Privacy.cshtml.cs
@@ -1,24 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace azure_ad_b2c.Pages
+namespace azure_ad_b2c.Pages;
+
+public class PrivacyModel : PageModel
 {
-    public class PrivacyModel : PageModel
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        _logger = logger;
+    }
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
+    public void OnGet()
+    {
     }
 }
+
