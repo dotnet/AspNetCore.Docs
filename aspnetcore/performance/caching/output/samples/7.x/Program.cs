b7055548303db67767dd0d1cbb61040169ce9792
Tom Dykstra <tdykstra@microsoft.com>
2023-01-05 16:59:02 -0800

    Show how to use a custom policy in output caching (#27688)
    * custompolicy
    
    * cleanup
    
    * tweak code
    
    * shorten line lengths
    
    * fix highlighting, line lengths
    
    * fix highlighting
    
    * fix typo
    
    * DI alternative
    
    * shorten some lines
    
    * Fix highlighting
    
    * Apply suggestions from code review
    
    Co-authored-by: Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * Update aspnetcore/performance/caching/output/samples/7.x/Program.cs
    
    Co-authored-by: Sébastien Ros <sebastienros@gmail.com>
    
    Co-authored-by: Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
    Co-authored-by: Sébastien Ros <sebastienros@gmail.com>

--- aspnetcore/performance/caching/output/samples/7.x/Program.cs
+++ aspnetcore/performance/caching/output/samples/7.x/Program.cs
@@ -1,4 +1,4 @@
-#define Version4
+#define Version3c // Version1 / Version2 / Version3 / Version 3b / Version 3c / Version4
 using Microsoft.AspNetCore.OutputCaching;
 using System.Globalization;
 
@@ -16,9 +16,12 @@ public class Program
         //<policies1>
         builder.Services.AddOutputCache(options =>
         {
-            options.AddBasePolicy(builder => builder.Expire(TimeSpan.FromSeconds(10)));
-            options.AddPolicy("Expire20", builder => builder.Expire(TimeSpan.FromSeconds(20)));
-            options.AddPolicy("Expire30", builder => builder.Expire(TimeSpan.FromSeconds(30)));
+            options.AddBasePolicy(builder => 
+                builder.Expire(TimeSpan.FromSeconds(10)));
+            options.AddPolicy("Expire20", builder => 
+                builder.Expire(TimeSpan.FromSeconds(20)));
+            options.AddPolicy("Expire30", builder => 
+                builder.Expire(TimeSpan.FromSeconds(30)));
         });
         //</policies1>
 #endif
@@ -44,6 +47,23 @@ public class Program
         });
         //</policies3>
 #endif
+#if Version3b
+        //<policies3b>
+        builder.Services.AddOutputCache(options =>
+        {
+            options.AddPolicy("CachePost", MyCustomPolicy.Instance);
+        });
+        //</policies3b>
+#endif
+#if Version3c
+        //<policies3c>
+        builder.Services.AddOutputCache(options =>
+        {
+            options.AddBasePolicy(builder => 
+                builder.AddPolicy<MyCustomPolicy2>(), true);
+        });
+        //</policies3c>
+#endif
 #if Version4
         //<policies4>
         builder.Services.AddOutputCache();
@@ -60,12 +80,14 @@ public class Program
 
         //<oneendpoint>
         app.MapGet("/cached", Gravatar.WriteGravatar).CacheOutput();
-        app.MapGet("/attribute", [OutputCache] (context) => Gravatar.WriteGravatar(context));
+        app.MapGet("/attribute", [OutputCache] (context) => 
+            Gravatar.WriteGravatar(context));
         //</oneendpoint>
 
         //<selectpolicy>
         app.MapGet("/20", Gravatar.WriteGravatar).CacheOutput("Expire20");
-        app.MapGet("/30", [OutputCache(PolicyName = "Expire30")] (context) => Gravatar.WriteGravatar(context));
+        app.MapGet("/30", [OutputCache(PolicyName = "Expire30")] (context) => 
+            Gravatar.WriteGravatar(context));
         //</selectpolicy>
 
         //<selectquery>
@@ -117,8 +139,18 @@ public class Program
         });
         // </evictbytag>
 
+        // <post>
+        app.MapPost("/cachedpost", Gravatar.WriteGravatar)
+            .CacheOutput("CachePost");
+        // </post>
+
+        // <postalt>
+        app.MapPost("/cachedpost2", Gravatar.WriteGravatar);
+        // </postalt>
+        
         // <selectnolock>
-        app.MapGet("/nolock", Gravatar.WriteGravatar).CacheOutput("NoLock");
+        app.MapGet("/nolock", Gravatar.WriteGravatar)
+            .CacheOutput("NoLock");
         // </selectnolock>
 
         app.Run();
