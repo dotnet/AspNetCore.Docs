d8b2efd4b46dbc81ef1d68abd2e60130a4dc015a
maroonedsia <smortazavi@hotmail.com>
2023-01-10 12:32:30 -0800

    Fix/add missing use windows service dotnet7 (#28048)
    * Reference Microsoft.Extensions.Hosting.WindowsServices package
    
    * Set builder's host to Windows Service

--- aspnetcore/host-and-deploy/windows-service/samples/7.x/WebAppServiceSample/Program.cs
+++ aspnetcore/host-and-deploy/windows-service/samples/7.x/WebAppServiceSample/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddHostedService<ServiceA>();
 
+builder.Host.UseWindowsService();
+
 var app = builder.Build();
 
 app.UseStaticFiles();
