1c638cb56d221cdc3b9898c86b6362113ea76b3d
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-01-12 13:43:54 -1000

    Intro to SPA's /4 (#28047)
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work

---
+++ aspnetcore/client-side/spa/intro/samples/Ang_proxy.conf.js
@@ -0,0 +1,19 @@
+const { env } = require('process');
+
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:51951';
+
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/weatherforecast",
+   ],
+    target: target,
+    secure: false,
+    headers: {
+      Connection: 'Keep-Alive'
+    }
+  }
+]
+
+module.exports = PROXY_CONFIG;
