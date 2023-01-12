d10f2ebb2180f2b1466b825a9f36e6942408e3df
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-01-12 13:43:54 -1000

    Intro to SPA's /4 (#28047)
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * Intro to SPA's /4
    
    * work
    
    * work
    
    * work
    
    * work
    
    * work

---
+++ aspnetcore/client-side/spa/intro/samples/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
