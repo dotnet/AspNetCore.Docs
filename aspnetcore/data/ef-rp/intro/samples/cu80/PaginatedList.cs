661d062e781e6cdf3661e6694e7b7c812e786748
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-01-12 10:03:09 -1000

    Proposed new PaginatedList.cs
    https://github.com/dotnet/AspNetCore.Docs/pull/28077/files

---
+++ aspnetcore/data/ef-rp/intro/samples/cu80/PaginatedList.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public PaginatedList(IEnumerable<T> items, int count, int pageIndex, int pageSize = 20)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public static async Task<PaginatedList<T>> CreateAsync(
+            IQueryable<T> source, int pageIndex, int pageSize = 20)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip(
+                (pageIndex - 1) * pageSize)
+                .Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
