0b1c75f23b61eebf10994918574ce6045d8e137d
Tom Dykstra <tdykstra@microsoft.com>
2023-01-20 13:35:06 -0800

    APIs TOC work (#28113)
    * draft
    
    * tweak wording and fix links
    
    * fix toc
    
    * fix list formatting
    
    * fix toc node name
    
    * Update aspnetcore/fundamentals/apis.md
    
    Co-authored-by: Jeremy Likness <jeremy@jeremylikness.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Jeremy Likness <jeremy@jeremylikness.com>
    
    * web API --> API
    
    * your API --> an API
    
    * Update aspnetcore/fundamentals/apis.md
    
    Co-authored-by: Safia Abdalla <safia@safia.rocks>
    
    * Update aspnetcore/toc.yml
    
    Co-authored-by: Safia Abdalla <safia@safia.rocks>
    
    * web API --> API
    
    Co-authored-by: Jeremy Likness <jeremy@jeremylikness.com>
    Co-authored-by: Safia Abdalla <safia@safia.rocks>

---
+++ aspnetcore/fundamentals/apis/MinimalAPI/Program.cs
@@ -0,0 +1,34 @@
+
+namespace MinimalAPI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var app = builder.Build();
+
+        app.UseHttpsRedirection();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = summaries[Random.Shared.Next(summaries.Length)]
+                })
+                .ToArray();
+            return forecast;
+        });
+
+        app.Run();
+    }
+}
