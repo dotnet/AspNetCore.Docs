4ab29c171ae8e96f3293c906517a9b5da3853d5e
Fiyaz Bin Hasan <fiyazhasan@live.com>
2023-02-27 02:38:34 +0600

    modify cors sample and doc

---
+++ aspnetcore/security/cors/8.0sample/Cors/Web2API/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Docs.Samples;
+
+namespace Web2API.Controllers;
+
+#region snippet
+[EnableCors("MyPolicy")]
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    // GET api/values
+    [HttpGet]
+    public IActionResult Get() =>
+        ControllerContext.MyDisplayRouteInfo();
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public IActionResult Get(int id) =>
+        ControllerContext.MyDisplayRouteInfo(id);
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public IActionResult Put(int id) =>
+        ControllerContext.MyDisplayRouteInfo(id);
+
+
+    // GET: api/values/GetValues2
+    [DisableCors]
+    [HttpGet("{action}")]
+    public IActionResult GetValues2() =>
+        ControllerContext.MyDisplayRouteInfo();
+
+}
+#endregion
