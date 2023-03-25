3d189b15fd4108717a2b8fa37598d40b358ac2ee
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-03-25 07:31:16 +0100

    feat : implemented the IResettable and IPooledObjectPolicy
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

--- aspnetcore/performance/ObjectPool/ObjectPoolSample8/Program.cs
+++ aspnetcore/performance/ObjectPool/ObjectPoolSample8/Program.cs
@@ -2,14 +2,13 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.ObjectPool;
 using ObjectPoolSample;
 using System.Text;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.TryAddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
 builder.Services.TryAddSingleton<ObjectPool<StringBuilder>>(serviceProvider =>
 {
     var provider = serviceProvider.GetRequiredService<ObjectPoolProvider>();
-    var policy = new Microsoft.Extensions.ObjectPool.StringBuilderPooledObjectPolicy();
+    var policy = new StringBuilderPolicy();
     return provider.Create(policy);
 });
 
@@ -23,3 +22,31 @@ app.UseMiddleware<BirthdayMiddleware>();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+
+public class ReusableStringBuilder : IResettable
+{
+    public StringBuilder Data { get; } = new StringBuilder();
+
+    public bool TryReset()
+    {
+        Data.Clear();
+        return true;
+    }
+}
+
+class StringBuilderPolicy : IPooledObjectPolicy<StringBuilder>
+{
+    public ReusableStringBuilder? reusableStringBuilder;
+    public StringBuilder Create()
+    {
+        reusableStringBuilder = new ReusableStringBuilder();
+        return reusableStringBuilder.Data;
+    }
+
+    public bool Return(StringBuilder obj)
+    {
+        return reusableStringBuilder!.TryReset();
+    }
+}
