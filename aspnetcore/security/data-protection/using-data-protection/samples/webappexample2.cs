34fcbf64e8138b5cc38b33f78f18ecdd21310429
Emrebener <33965796+Emrebener@users.noreply.github.com>
2023-03-28 14:36:53 +0300

    Update webappexample2.cs
    extended the code to include using the service within an action method

--- aspnetcore/security/data-protection/using-data-protection/samples/webappexample2.cs
+++ aspnetcore/security/data-protection/using-data-protection/samples/webappexample2.cs
@@ -7,4 +7,22 @@ public class HomeController : Controller
         _dataProtector = dataProtectionProvider.CreateProtector("HomeControllerPurpose");
     }
 
-  // ...
+    // ...
+
+    public IActionResult Privacy()
+    {
+        // The original data to protect
+        string originalData = "original data";
+
+        // Protect the data (encrypt)
+        string protectedData = _dataProtector.Protect(originalData);
+        Console.WriteLine($"Protected Data: {protectedData}");
+
+        // Unprotect the data (decrypt)
+        string unprotectedData = _dataProtector.Unprotect(protectedData);
+        Console.WriteLine($"Unprotected Data: {unprotectedData}");
+
+        return View();
+    }
+    
+    // ...
