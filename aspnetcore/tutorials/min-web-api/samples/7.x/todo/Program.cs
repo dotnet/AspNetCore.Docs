296ba7c7dc3b3a3b0ec9689b888548d7fa1510b7
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-04-11 12:28:51 -1000

    WHy use typedresults /3 (#28918)
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * Apply suggestions from code review
    
    Co-authored-by: Damian Edwards <damian@damianedwards.com>
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    * WHy use typedresults /3
    
    ---------
    
    Co-authored-by: Damian Edwards <damian@damianedwards.com>

--- aspnetcore/tutorials/min-web-api/samples/7.x/todo/Program.cs
+++ aspnetcore/tutorials/min-web-api/samples/7.x/todo/Program.cs
@@ -1,4 +1,4 @@
-#define FINAL // MINIMAL FINAL
+#define TYPEDR // MINIMAL FINAL TYPEDR
 #if MINIMAL
 // <snippet_min>
 var builder = WebApplication.CreateBuilder(args);
@@ -77,4 +77,55 @@ app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
 
 app.Run();
 // </snippet_all>
+#elif TYPEDR
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+
+app.MapGet("/todoitems", async (TodoDb db) =>
+    await db.Todos.ToListAsync());
+
+app.MapGet("/todoitems/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+// <snippet_1b>
+ app.MapGet("/todos/{id}", async Task<Results<Ok<Todo>, NotFound>> (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+     is Todo todo
+        ? TypedResults.Ok(todo)
+        : TypedResults.NotFound());
+// </snippet_1b>
+
+/*
+// <snippet_111>
+app.MapGet("/todos/{id}", async (int id, TodoDb db) =>
+     await db.Todos.FindAsync(id)
+     is Todo todo
+        ? TypedResults.Ok(todo)
+        : TypedResults.NotFound());
+// </snippet_111>
+*/
+
+// <snippet_11b>
+app.MapGet("/hello", () => Results.Ok(new Message() { Text = "Hello World!" }))
+    .Produces<Message>();
+// </snippet_11b>
+
+// <snippet_112b>
+app.MapGet("/hello2", () => TypedResults.Ok(new Message() { Text = "Hello World!" }));
+// </snippet_112b>
+
+app.Run();
 #endif
+
