fd10025bd03f7c5b7b41fc967623d04d04b74eec
Wade Pickett <wpickett@microsoft.com>
2023-04-13 10:21:17 -0700

    8.0 update Web API with MongoDB (#28944)
    * v8 update Web API with MongoDB
    
    * Added includes at bottom of v8 doc
    
    * Added enable preview steps for Mac
    
    * Added comma per review.
    
    Co-authored-by: Tom Dykstra <tdykstra@microsoft.com>
    
    ---------
    
    Co-authored-by: Tom Dykstra <tdykstra@microsoft.com>

---
+++ aspnetcore/tutorials/first-mongo-app/samples/8.x/BookStoreApi/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿// <snippet_UsingSystemTextJsonSerialization>
+using System.Text.Json.Serialization;
+// </snippet_UsingSystemTextJsonSerialization>
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStoreApi.Models;
+
+public class Book
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    // <snippet_BookName>
+    [BsonElement("Name")]
+    [JsonPropertyName("Name")]
+    public string BookName { get; set; } = null!;
+    // </snippet_BookName>
+
+    public decimal Price { get; set; }
+
+    public string Category { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+}
