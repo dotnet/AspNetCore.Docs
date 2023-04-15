cf8529ec07be7ba75702362d665df81d9aab1d53
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-04-15 13:28:20 +0100

    created new buffer

--- aspnetcore/performance/ObjectPool/ObjectPoolSample8/Program.cs
+++ aspnetcore/performance/ObjectPool/ObjectPoolSample8/Program.cs
@@ -1,51 +1,72 @@
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.ObjectPool;
-using ObjectPoolSample;
-using System.Text;
+using System.Security.Cryptography;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.TryAddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
-builder.Services.TryAddSingleton<ObjectPool<ReusableStringBuilder>>(serviceProvider =>
+
+builder.Services.TryAddSingleton<ObjectPool<ReusableBuffer>>(serviceProvider =>
 {
     var provider = serviceProvider.GetRequiredService<ObjectPoolProvider>();
-    var policy = new ReusableStringBuilderPolicy();
+    var policy = new ReusableBufferPolicy();
     return provider.Create(policy);
 });
 
-builder.Services.AddWebEncoders();
-
 var app = builder.Build();
-
-// Test using /?firstname=Steve&lastName=Gordon&day=28&month=9
-app.UseMiddleware<BirthdayMiddleware>();
 
 app.MapGet("/", () => "Hello World!");
 
+// return the SHA256 hash of a word 
+// https://localhost:7214/SamsonAmaugo
+app.MapGet("/{name}", async (string name, ObjectPool<ReusableBuffer> bufferPool) =>
+{
+
+    var buffer = bufferPool.Get();
+    try
+    {
+        // Set the buffer data to the ASCII values of a word
+        for (int i = 0; i < name.Length; i++)
+        {
+            buffer.Data[i] = (byte)name[i];
+        }
+
+        using (var sha256 = SHA256.Create())
+        {
+            // Calculate the hash of the data
+            var hash = sha256.ComputeHash(buffer.Data);
+            return "Hash: " + BitConverter.ToString(hash).Replace("-", "");
+        }
+    }
+    finally
+    {
+        // Data is automatically reset
+        bufferPool.Return(buffer); 
+    }
+});
 app.Run();
 
-
-
-public class ReusableStringBuilder : IResettable
+public class ReusableBuffer : IResettable
 {
-    public StringBuilder StringBuilder { get; } = new StringBuilder();
+    public byte[] Data { get; } = new byte[1024 * 1024]; // 1 MB
 
     public bool TryReset()
     {
-        StringBuilder.Clear();
+        Array.Clear(Data);
         return true;
     }
 }
 
-class ReusableStringBuilderPolicy : IPooledObjectPolicy<ReusableStringBuilder>
+public class ReusableBufferPolicy : IPooledObjectPolicy<ReusableBuffer>
 {
-    public ReusableStringBuilder? reusableStringBuilder;
-    public ReusableStringBuilder Create()
+    public ReusableBuffer? reusableBuffer;
+    public ReusableBuffer Create()
     {
-        reusableStringBuilder = new ReusableStringBuilder();
-        return reusableStringBuilder;
+        reusableBuffer = new ReusableBuffer();
+        return reusableBuffer;
     }
 
-    public bool Return(ReusableStringBuilder obj)
+    public bool Return(ReusableBuffer obj)
     {
         return obj.TryReset();
     }
