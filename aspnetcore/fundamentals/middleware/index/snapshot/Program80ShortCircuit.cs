fd4f7d958b221e049c468053949609b4e0668f64
Tom Dykstra <tdykstra@microsoft.com>
2023-04-27 17:56:41 -0700

    add no-short-circuit route

--- aspnetcore/fundamentals/middleware/index/snapshot/Program80ShortCircuit.cs
+++ aspnetcore/fundamentals/middleware/index/snapshot/Program80ShortCircuit.cs
@@ -2,8 +2,9 @@ var app = WebApplication.Create();
 
 app.UseHttpLogging();
 
+app.MapGet("/", () => "No short-circuiting!");
 // <mapget>
-app.MapGet("/short-circuit", () => "Hello world!").ShortCircuit();
+app.MapGet("/short-circuit", () => "Short circuiting!").ShortCircuit();
 // </mapget>
 // <mapshortcircuit>
 app.MapShortCircuit(404, "robots.txt", "favicon.ico");
