c5111536e948ae3a09614de1e7f6e84c0475a8bc
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-05-02 01:19:25 +0100

    feat : added sample for validation result using error message (#29123)
    * added sample for validation result using error message
    
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>
    
    * added sample for validation result using error message
    
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>
    
    * deleted folder
    
    ---------
    
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

---
+++ aspnetcore/mvc/models/validation/samples/ValidationResultErrorMessage/Data/ValidationResultErrorMessageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ValidationResultErrorMessage.Models;
+
+namespace ValidationResultErrorMessage.Data
+{
+    public class ValidationResultErrorMessageContext : DbContext
+    {
+        public ValidationResultErrorMessageContext (DbContextOptions<ValidationResultErrorMessageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ValidationResultErrorMessage.Models.Contact> Contact { get; set; } = default!;
+    }
+}
