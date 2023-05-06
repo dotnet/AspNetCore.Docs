ed5ea89456bec6515223105bbf9bc1e21a8ae0c0
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-05-06 12:50:09 +0100

    Changed Error Model
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

--- aspnetcore/mvc/models/validation/samples/ValidationResultErrorMessage/Models/Contact.cs
+++ aspnetcore/mvc/models/validation/samples/ValidationResultErrorMessage/Models/Contact.cs
@@ -1,40 +1,39 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace ValidationResultErrorMessage.Models
+namespace ValidationResultErrorMessage.Models;
+
+public class Contact
 {
-    public class Contact
-    {
-        public Guid Id { get; set; }
+    public Guid Id { get; set; }
 
-        [ValidateName(ErrorMessage = "Name must be at least 3 characters long.")] public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? PhoneNumber { get; set; }
+    [ValidateName(ErrorMessage = "Name must be at least 3 characters long.")] public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+}
+
+public class ValidateNameAttribute : ValidationAttribute
+{
+    public ValidateNameAttribute()
+    {
+        // Default error message
+        const string defaultErrorMessage = "Error with Inputted Name";
+        ErrorMessage ??= defaultErrorMessage;
     }
 
-    public class ValidateNameAttribute : ValidationAttribute
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        public ValidateNameAttribute()
+        if (value == null)
         {
-            // Default error message
-            const string defaultErrorMessage = "Error with Inputted Name";
-            ErrorMessage ??= defaultErrorMessage;
+            return new ValidationResult("Name is required.");
         }
 
-        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        if (value.ToString()!.Length < 3)
         {
-            if (value == null)
-            {
-                return new ValidationResult("Name is required.");
-            }
 
-            if (value.ToString()!.Length < 3)
-            {
-
-                return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
-            }
-
-            return ValidationResult.Success;
+            return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
         }
 
+        return ValidationResult.Success;
     }
+
 }
