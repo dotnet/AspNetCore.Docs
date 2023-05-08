03bc725be4f7232116b0b2ad04b475a203a01b3d
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-05-08 22:13:32 +0100

    fix: Validation result (#29197)
    * added sample for validation result using error message
    
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>
    
    * added sample for validation result using error message
    
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>
    
    * deleted folder
    
    * Changed Error Model
    
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>
    
    * Changed Error Model
    
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>
    
    ---------
    
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

--- aspnetcore/mvc/models/validation/samples/ValidationResultErrorMessage/Controllers/HomeController.cs
+++ aspnetcore/mvc/models/validation/samples/ValidationResultErrorMessage/Controllers/HomeController.cs
@@ -1,32 +1,31 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ValidationResultErrorMessage.Models;
 
-namespace ValidationResultErrorMessage.Controllers
+namespace ValidationResultErrorMessage.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
