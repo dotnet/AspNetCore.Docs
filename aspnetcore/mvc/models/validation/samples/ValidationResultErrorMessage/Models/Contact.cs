fded6050afc2627d897089937afebedca38340c3
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-05-08 17:32:25 -1000

    Validation Result /2 (#29202)
    * Validation Result /2
    
    * Validation Result /2
    
    * Validation Result /2
    
    * Validation Result /2
    
    * Validation Result /2
    
    * Validation Result /2

--- aspnetcore/mvc/models/validation/samples/ValidationResultErrorMessage/Models/Contact.cs
+++ aspnetcore/mvc/models/validation/samples/ValidationResultErrorMessage/Models/Contact.cs
@@ -1,33 +1,35 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace ValidationResultErrorMessage.Models;
-
+// <snippet_1>
 public class Contact
 {
     public Guid Id { get; set; }
 
-    [ValidateName(ErrorMessage = "Name must be at least 3 characters long.")] public string? Name { get; set; }
+    [ValidateName(ErrorMessage = "Name must not contain `zz`")] 
+                                  public string? Name { get; set; }
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
 }
+// </snippet_1>
 
+// <snippet_2>
 public class ValidateNameAttribute : ValidationAttribute
 {
     public ValidateNameAttribute()
     {
-        // Default error message
-        const string defaultErrorMessage = "Error with Inputted Name";
+        const string defaultErrorMessage = "Error with Name";
         ErrorMessage ??= defaultErrorMessage;
     }
 
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        if (value == null)
+        if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
         {
             return new ValidationResult("Name is required.");
         }
 
-        if (value.ToString()!.Length < 3)
+        if (value.ToString()!.ToLower().Contains("zz"))
         {
 
             return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
@@ -35,5 +37,5 @@ public class ValidateNameAttribute : ValidationAttribute
 
         return ValidationResult.Success;
     }
-
 }
+// </snippet_2>
