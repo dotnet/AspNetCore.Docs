84d3dcaaa8a38f7610a5ac7e5c84b33c682f55c4
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-05-21 08:54:11 -1000

    Optional route params 2

--- aspnetcore/fundamentals/routing/samples/8.x/OptionalParams/Controllers/MyController.cs
+++ aspnetcore/fundamentals/routing/samples/8.x/OptionalParams/Controllers/MyController.cs
@@ -2,12 +2,13 @@
 
 namespace WebApplication1.Controllers;
 
+[Route("api/[controller]")]
 public class MyController : ControllerBase
 {
     // GET /api/my/red/2/joe
     // GET /api/my/red/2
     // GET /api/my
-    [Route("api/[controller]/{color}/{id:int?}/{name?}")]
+    [HttpGet("{color}/{id:int?}/{name?}")]
     public IActionResult GetByIdAndOptionalName(string color, int id = 1, string? name = null)
     {
         return Ok($"{color} {id} {name ?? ""}");
