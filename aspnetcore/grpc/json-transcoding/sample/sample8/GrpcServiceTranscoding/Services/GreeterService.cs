f164514ecbd4f1a3ce297b1d830c925b4d307287
Wade Pickett <wpickett@microsoft.com>
2023-05-23 12:01:53 -0700

    8.0 Update: gRPC Json Transcoding (#29279)
    * 8.0 Update Json Transcoding
    
    * Updated code link
    
    * updated code path on version 7 topic
    
    * updated code highlights
    
    * Added version 7 app sample and removed test from sample 8 namespace
    
    * Fixed project file example code highlight
    
    * Added more specific instruction in v7 for registering transcoding.
    
    * Added a link to the transcoding and streaming example on grpc/grpc-dotnet at end of streaming section.
    
    * Update aspnetcore/grpc/json-transcoding.md
    
    Co-authored-by: James Newton-King <james@newtonking.com>
    
    ---------
    
    Co-authored-by: James Newton-King <james@newtonking.com>

---
+++ aspnetcore/grpc/json-transcoding/sample/sample8/GrpcServiceTranscoding/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+using Grpc.Core;
+using GrpcServiceTranscoding;
+
+namespace GrpcServiceTranscoding.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
