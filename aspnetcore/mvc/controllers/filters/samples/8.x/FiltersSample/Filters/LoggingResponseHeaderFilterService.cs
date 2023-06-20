75bf3a1a163f2003206d5ce2151cf78c0c77430f
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-06-19 14:28:09 -1000

    Moniker prep (#29559)
    * Moniker prep
    
    * Moniker prep
    
    * Moniker prep

---
+++ aspnetcore/mvc/controllers/filters/samples/8.x/FiltersSample/Filters/LoggingResponseHeaderFilterService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FiltersSample.Filters;
+
+// <snippet_Class>
+public class LoggingResponseHeaderFilterService : IResultFilter
+{
+    private readonly ILogger _logger;
+
+    public LoggingResponseHeaderFilterService(
+            ILogger<LoggingResponseHeaderFilterService> logger) =>
+        _logger = logger;
+
+    public void OnResultExecuting(ResultExecutingContext context)
+    {
+        _logger.LogInformation(
+            $"- {nameof(LoggingResponseHeaderFilterService)}.{nameof(OnResultExecuting)}");
+
+        context.HttpContext.Response.Headers.Add(
+            nameof(OnResultExecuting), nameof(LoggingResponseHeaderFilterService));
+    }
+
+    public void OnResultExecuted(ResultExecutedContext context)
+    {
+        _logger.LogInformation(
+            $"- {nameof(LoggingResponseHeaderFilterService)}.{nameof(OnResultExecuted)}");
+    }
+}
+// </snippet_Class>
---
+++ aspnetcore/mvc/controllers/filters/samples/8.x/FiltersSample/Filters/SampleActionTypeFilterAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FiltersSample.Filters;
+
+// <snippet_Class>
+public class SampleActionTypeFilterAttribute : TypeFilterAttribute
+{
+    public SampleActionTypeFilterAttribute()
+         : base(typeof(InternalSampleActionFilter)) { }
+
+    private class InternalSampleActionFilter : IActionFilter
+    {
+        private readonly ILogger<InternalSampleActionFilter> _logger;
+
+        public InternalSampleActionFilter(ILogger<InternalSampleActionFilter> logger) =>
+            _logger = logger;
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation(
+                $"- {nameof(InternalSampleActionFilter)}.{nameof(OnActionExecuting)}");
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation(
+                $"- {nameof(InternalSampleActionFilter)}.{nameof(OnActionExecuted)}");
+        }
+    }
+}
+// </snippet_Class>
