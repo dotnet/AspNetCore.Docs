79b2e543ecc288cba94ec580dcf8218c87953afe
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-06-19 14:06:49 -1000

    Moniker prep

---
+++ aspnetcore/mvc/controllers/filters/samples/8.x/FiltersSample/Filters/SampleActionFilterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FiltersSample.Filters;
+
+public class SampleActionFilterAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        Console.WriteLine(
+            $"- {nameof(SampleActionFilterAttribute)}.{nameof(OnActionExecuting)}");
+
+        base.OnActionExecuting(context);
+    }
+
+    public override void OnActionExecuted(ActionExecutedContext context)
+    {
+        Console.WriteLine(
+            $"- {nameof(SampleActionFilterAttribute)}.{nameof(OnActionExecuted)}");
+
+        base.OnActionExecuted(context);
+    }
+}
