7c58be9eb78733e06e7a9231a51446f5a0b94a2a
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-06-23 09:15:53 -1000

    Update snippet ref to 8.x /2

--- aspnetcore/fundamentals/servers/httpsys/samples/8.x/SampleApp/Program.cs
+++ aspnetcore/fundamentals/servers/httpsys/samples/8.x/SampleApp/Program.cs
@@ -1,9 +1,13 @@
+#define SECOND // FIRST SECOND
+#if NEVER
+#elif FIRST
+// <snippet_1>
+// <snippet_2>
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Server.HttpSys;
 
-// <snippet_1>
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseHttpSys(options =>
@@ -17,10 +21,9 @@ builder.WebHost.UseHttpSys(options =>
 });
 
 builder.Services.AddRazorPages();
-// </snippet_1>
 
-// <snippet_2>
 var app = builder.Build();
+// </snippet_1>
 
 app.Use((context, next) =>
 {
@@ -55,3 +58,60 @@ app.MapRazorPages();
 
 app.Run();
 // </snippet_2>
+#elif SECOND
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.HttpSys;
+
+// <snippet_11>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseHttpSys(options =>
+{
+    options.AllowSynchronousIO = false;
+    options.Authentication.Schemes = AuthenticationSchemes.None;
+    options.Authentication.AllowAnonymous = true;
+    options.MaxConnections = null;
+    options.MaxRequestBodySize = 30_000_000;
+    options.UrlPrefixes.Add("http://localhost:5005");
+});
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+// </snippet_11>
+
+app.Use((context, next) =>
+{
+    var maxRequestBodySizeFeature = context.Features.GetRequiredFeature<IHttpMaxRequestBodySizeFeature>();
+    maxRequestBodySizeFeature.MaxRequestBodySize = 10 * 1024;
+
+    var server = context.RequestServices.GetRequiredService<IServer>();
+    var serverAddressesFeature = server.Features.GetRequiredFeature<IServerAddressesFeature>();
+
+    var addresses = string.Join(", ", serverAddressesFeature.Addresses);
+
+    var loggerFactory = context.RequestServices.GetRequiredService<ILoggerFactory>();
+    var logger = loggerFactory.CreateLogger("Sample");
+
+    logger.LogInformation("Addresses: {addresses}", addresses);
+
+    return next(context);
+});
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+
+app.Run();
+#endif
