5ce1cd49c399dafeb863b3b758a72b9188bc0e6c
Tom Dykstra <tdykstra@microsoft.com>
2023-07-27 17:17:27 -0700

    format code

--- aspnetcore/performance/caching/output/samples/8.x/Program.cs
+++ aspnetcore/performance/caching/output/samples/8.x/Program.cs
@@ -7,26 +7,25 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddAuthorization();
         // <redis>
         builder.Services.AddStackExchangeRedisOutputCache(options =>
         {
             options.Configuration = builder.Configuration.GetConnectionString("MyRedisConStr");
             options.InstanceName = "SampleInstance";
         });
+
         builder.Services.AddOutputCache(options =>
         {
             options.AddBasePolicy(builder => 
                 builder.Expire(TimeSpan.FromSeconds(10)));
         });
         // </redis>
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         app.UseHttpsRedirection();
         app.UseOutputCache();
-        app.UseAuthorization();
 
         app.MapGet("/", Gravatar.WriteGravatar);
 
