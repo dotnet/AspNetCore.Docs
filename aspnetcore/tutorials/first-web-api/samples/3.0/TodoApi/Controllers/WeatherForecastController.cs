36704f3bce9e0d4760ab58ec440b8f3d2726635f
mu lei <ml2257875@163.com>
2023-10-08 11:32:21 +0800

    remove unnecessary ToArray

--- aspnetcore/tutorials/first-web-api/samples/3.0/TodoApi/Controllers/WeatherForecastController.cs
+++ aspnetcore/tutorials/first-web-api/samples/3.0/TodoApi/Controllers/WeatherForecastController.cs
@@ -32,8 +32,7 @@ namespace TodoApi.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            });
         }
     }
 }
