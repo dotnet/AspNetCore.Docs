baf944d1d6a010711635166d48b26a80a3d21514
damienbod <damien_bod@hotmail.com>
2023-11-05 08:00:13 +0100

    namespace clean up .NET 8

--- aspnetcore/security/authentication/claims/sample8/WebRPmapClaims/Pages/Index.cshtml.cs
+++ aspnetcore/security/authentication/claims/sample8/WebRPmapClaims/Pages/Index.cshtml.cs
@@ -1,20 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebRPmapClaims.Pages
+namespace WebRPmapClaims.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
     {
-        private readonly ILogger<IndexModel> _logger;
+        _logger = logger;
+    }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+    public void OnGet()
+    {
 
-        public void OnGet()
-        {
-
-        }
     }
 }
--- aspnetcore/security/authentication/claims/sample8/WebRPmapClaims/Pages/Privacy.cshtml.cs
+++ aspnetcore/security/authentication/claims/sample8/WebRPmapClaims/Pages/Privacy.cshtml.cs
@@ -1,19 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebRPmapClaims.Pages
+namespace WebRPmapClaims.Pages;
+
+public class PrivacyModel : PageModel
 {
-    public class PrivacyModel : PageModel
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        _logger = logger;
+    }
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
+    public void OnGet()
+    {
     }
 }
