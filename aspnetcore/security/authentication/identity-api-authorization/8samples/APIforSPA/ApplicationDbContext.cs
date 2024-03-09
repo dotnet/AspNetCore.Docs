430914e9b21133c2cdd2da0ad297b88de6de971d
Tom Dykstra <tdykstra@microsoft.com>
2024-03-08 20:08:49 -0800

    Convert inline code to snippet references (#32003)

---
+++ aspnetcore/security/authentication/identity-api-authorization/8samples/APIforSPA/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+        base(options)
+    { }
+}
