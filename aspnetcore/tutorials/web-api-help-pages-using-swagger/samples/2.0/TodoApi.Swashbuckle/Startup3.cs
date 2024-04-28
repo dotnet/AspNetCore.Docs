45d15b7e6fae6f2ac4da19489ac5b058d1afd7af
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-04-27 17:04:06 -1000

    Update Startup3.cs

--- aspnetcore/tutorials/web-api-help-pages-using-swagger/samples/2.0/TodoApi.Swashbuckle/Startup3.cs
+++ aspnetcore/tutorials/web-api-help-pages-using-swagger/samples/2.0/TodoApi.Swashbuckle/Startup3.cs
@@ -34,7 +34,7 @@ namespace TodoApi
                 // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
                 // specifying the Swagger JSON endpoint.
                 // <snippet_UseSwaggerUI>
-                app.UseSwaggerUI(c =>
+                app.UseSwaggerUI(c => // UseSwaggerUI Protected by if (env.IsDevelopment())
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
                     c.RoutePrefix = string.Empty;
