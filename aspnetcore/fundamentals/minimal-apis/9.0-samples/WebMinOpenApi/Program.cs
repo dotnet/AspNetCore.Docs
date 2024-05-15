c0cd2ebceecc23b9946e74f0f1739d085c26ba5b
Safia Abdalla <safia@microsoft.com>
2024-05-15 12:00:36 -0700

    Update Scalar docs to use Scalar.AspNetCore package (#32585)
    Co-authored-by: Tom Dykstra <tdykstra@microsoft.com>

--- aspnetcore/fundamentals/minimal-apis/9.0-samples/WebMinOpenApi/Program.cs
+++ aspnetcore/fundamentals/minimal-apis/9.0-samples/WebMinOpenApi/Program.cs
@@ -313,6 +313,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.OpenApi;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder();
 
@@ -324,44 +325,11 @@ app.MapOpenApi();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapScalarUi();
+    app.MapScalarApiReference();
 }
 
 app.MapGet("/", () => "Hello world!");
 
-app.Run();
-
-static class OpenApiEndpointRouteBuilderExtensions
-{
-    public static IEndpointConventionBuilder MapScalarUi(this IEndpointRouteBuilder endpoints)
-    {
-        return endpoints.MapGet("/scalar/{documentName}", (string documentName) => Results.Content($$"""
-        <!doctype html>
-        <html>
-        <head>
-            <title>Scalar API Reference -- {{documentName}}</title>
-            <meta charset="utf-8" />
-            <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1" />
-        </head>
-        <body>
-            <script
-            id="api-reference"
-            data-url="/openapi/{{documentName}}.json"></script>
-            <script>
-            var configuration = {
-                theme: 'purple',
-            }
-
-            document.getElementById('api-reference').dataset.configuration =
-                JSON.stringify(configuration)
-            </script>
-            <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
-        </body>
-        </html>
-        """, "text/html")).ExcludeFromDescription();
-    }
-}   
+app.Run();   
 // </snippet_openapiwithscalar>
 #endif
