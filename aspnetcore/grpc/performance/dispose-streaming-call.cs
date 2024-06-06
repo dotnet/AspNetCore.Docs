5c6f34245b42819255fa8e53880e4678ea5a8be7
James Newton-King <james@newtonking.com>
2024-06-06 08:10:30 +0800

    Add gRPC perf docs around gracefully completing and disposing streaming calls (#32742)
    Co-authored-by: Wade Pickett <wpickett@microsoft.com>

---
+++ aspnetcore/grpc/performance/dispose-streaming-call.cs
@@ -0,0 +1,12 @@
+var client = new Counter.CounterClient(channel);
+using var call = client.AccumulateCount();
+
+for (var i = 0; i < 3; i++)
+{
+    await call.RequestStream.WriteAsync(new CounterRequest { Count = 1 });
+}
+await call.RequestStream.CompleteAsync();
+
+var response = await call;
+Console.WriteLine($"Count: {response.Count}");
+// Count: 3
