72091180fa9205dbb46e7fa696a352c6573133b0
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-08-12 08:16:30 -1000

    delete keys for wn

--- aspnetcore/security/data-protection/configuration/samples/9/deleteKeys/Program.cs
+++ aspnetcore/security/data-protection/configuration/samples/9/deleteKeys/Program.cs
@@ -1,10 +1,27 @@
-var keyManager = services.GetService<IKeyManager>();
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+
+var services = new ServiceCollection();
+services.AddDataProtection();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var keyManager = serviceProvider.GetService<IKeyManager>();
+
 if (keyManager is IDeletableKeyManager deletableKeyManager)
 {
     var utcNow = DateTimeOffset.UtcNow;
     var yearAgo = utcNow.AddYears(-1);
+
     if (!deletableKeyManager.DeleteKeys(key => key.ExpirationDate < yearAgo))
     {
-        throw new InvalidOperationException("Failed to delete keys.");
+        Console.WriteLine("Failed to delete keys.");
     }
+    else
+    {
+        Console.WriteLine("Old keys deleted successfully.");
+    }
+}
+else
+{
+    Console.WriteLine("Key manager does not support deletion.");
 }
