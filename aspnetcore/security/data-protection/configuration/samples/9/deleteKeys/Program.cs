7cd8277ab51a76978dc99506fa16604f2a942528
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-08-12 08:33:15 -1000

    delete keys for wn (#33324)
    * delete keys for wn
    
    * delete keys for wn
    
    * delete keys for wn
    
    * delete keys for wn
    
    * delete keys for wn
    
    * delete keys for wn
    
    * delete keys for wn
    
    * delete keys for wn
    
    * delete keys for wn
    
    * delete keys for wn
    
    * delete keys for wn
    
    * delete keys for wn
    
    * Update aspnetcore/release-notes/aspnetcore-9/includes/delete_keys.md
    
    Co-authored-by: Andrew Casey <amcasey@users.noreply.github.com>
    
    * delete keys for wn
    
    * delete keys for wn
    
    * delete keys for wn
    
    ---------
    
    Co-authored-by: Andrew Casey <amcasey@users.noreply.github.com>

---
+++ aspnetcore/security/data-protection/configuration/samples/9/deleteKeys/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+
+var services = new ServiceCollection();
+services.AddDataProtection();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var keyManager = serviceProvider.GetService<IKeyManager>();
+
+if (keyManager is IDeletableKeyManager deletableKeyManager)
+{
+    var utcNow = DateTimeOffset.UtcNow;
+    var yearAgo = utcNow.AddYears(-1);
+
+    if (!deletableKeyManager.DeleteKeys(key => key.ExpirationDate < yearAgo))
+    {
+        Console.WriteLine("Failed to delete keys.");
+    }
+    else
+    {
+        Console.WriteLine("Old keys deleted successfully.");
+    }
+}
+else
+{
+    Console.WriteLine("Key manager does not support deletion.");
+}
