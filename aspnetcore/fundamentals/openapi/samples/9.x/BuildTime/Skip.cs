8acbf9239e52f9911810da9c0169663aebe1f562
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-08-15 14:33:53 -1000

    Add SqlDB to skip

---
+++ aspnetcore/fundamentals/openapi/samples/9.x/BuildTime/Skip.cs
@@ -0,0 +1,34 @@
+#if NEVER
+<!--<snippet_1>-->
+using ControllerApi.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+if (Assembly.GetEntryAssembly()?.GetName().Name != "GetDocument.Insider")
+{
+    builder.Services.AddDbContext<ControllerApiContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ControllerApiContext")
+        ?? throw new InvalidOperationException("Connection string 'ControllerApiContext' not found.")));
+}
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+<!--</snippet_1>-->
+#endif
