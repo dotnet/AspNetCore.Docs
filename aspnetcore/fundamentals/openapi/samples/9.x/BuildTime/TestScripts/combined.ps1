38ecacbde1b817f7b5959c3f98bb2a0bcfc482a3
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-08-16 15:22:47 -1000

    react to feedback

---
+++ aspnetcore/fundamentals/openapi/samples/9.x/BuildTime/TestScripts/combined.ps1
@@ -0,0 +1,24 @@
+# Creates a Web API project, adds ApiDescription.Server
+# and displays first few lines
+$ProgramName = "MyOpenApiTest"
+$Project = "$ProgramName.csproj"
+Write-Output $ProgramName
+dotnet new webapi -n $ProgramName
+Set-Location $ProgramName
+dotnet add package Microsoft.Extensions.ApiDescription.Server --version 9.0.0-*
+dotnet build
+Select-String -Path "obj\$ProgramName.json" -Pattern "." | Select-Object -First 5
+
+# Add prob and test
+$env:Project = "$($env:PRONAM).csproj"
+$newPropertyGroup = $xml.CreateElement("PropertyGroup")
+$newElement = $xml.CreateElement("OpenApiDocumentsDirectory")
+$newElement.InnerText = "./"
+$newPropertyGroup.AppendChild($newElement)
+$xml.Project.AppendChild($newPropertyGroup)
+$xml.OuterXml | Set-Content -Path $env:Project
+$xml.OuterXml
+# verify change is saved
+# $xml2 = [xml](Get-Content $env:Project)
+# $xml2.OuterXml
+dotnet build
