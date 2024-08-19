739dcb573f394adc7a0fab1f36073e1a6cb3c14e
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-08-19 11:40:17 -1000

    react to feedback

--- aspnetcore/fundamentals/openapi/samples/9.x/BuildTime/TestScripts/combined.ps1
+++ aspnetcore/fundamentals/openapi/samples/9.x/BuildTime/TestScripts/combined.ps1
@@ -2,23 +2,26 @@
 # and displays first few lines
 $ProgramName = "MyOpenApiTest"
 $Project = "$ProgramName.csproj"
-Write-Output $ProgramName
+# $ProgramName
 dotnet new webapi -n $ProgramName
 Set-Location $ProgramName
 dotnet add package Microsoft.Extensions.ApiDescription.Server --version 9.0.0-*
 dotnet build
 Select-String -Path "obj\$ProgramName.json" -Pattern "." | Select-Object -First 5
 
-# Add prob and test
-$env:Project = "$($env:PRONAM).csproj"
+# Add property and test
+$xml = [xml](Get-Content $Project) 
 $newPropertyGroup = $xml.CreateElement("PropertyGroup")
 $newElement = $xml.CreateElement("OpenApiDocumentsDirectory")
 $newElement.InnerText = "./"
 $newPropertyGroup.AppendChild($newElement)
 $xml.Project.AppendChild($newPropertyGroup)
+$xml.Save($Project) 
 $xml.OuterXml | Set-Content -Path $env:Project
 $xml.OuterXml
-# verify change is saved
-# $xml2 = [xml](Get-Content $env:Project)
-# $xml2.OuterXml
+#verify change is saved
+$xml2 = [xml](Get-Content $env:Project)
+$xml2.OuterXml
 dotnet build
+cd ..
+# Remove-Item -Recurse -Force $ProgramName
