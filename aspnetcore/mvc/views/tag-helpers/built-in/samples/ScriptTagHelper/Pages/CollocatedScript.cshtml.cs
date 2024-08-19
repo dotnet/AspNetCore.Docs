51142838e9b2ce1f29b2a031af4419bcfc03b9d2
Steven Liekens <steven.liekens@gmail.com>
2024-08-19 22:52:38 +0200

    Add ScriptTagHelper sample with collocated JS files

---
+++ aspnetcore/mvc/views/tag-helpers/built-in/samples/ScriptTagHelper/Pages/CollocatedScript.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScriptTagHelper.Pages;
+
+public class CollocatedScriptModel : PageModel
+{
+    private readonly ILogger<CollocatedScriptModel> _logger;
+
+    public CollocatedScriptModel(ILogger<CollocatedScriptModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
