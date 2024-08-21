8d7b0464dbb1b483430288d39ed37615a4bc4784
Safia Abdalla <safia@safia.rocks>
2024-08-21 12:32:50 -0700

    Add v2 document to app

--- aspnetcore/fundamentals/openapi/samples/9.x/BuildTime/Program.cs
+++ aspnetcore/fundamentals/openapi/samples/9.x/BuildTime/Program.cs
@@ -2,7 +2,9 @@ var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddAuthorization();
 
+// Default `v1` document
 builder.Services.AddOpenApi();
+builder.Services.AddOpenApi("v2");
 
 var app = builder.Build();
 
@@ -17,8 +19,8 @@ app.UseAuthorization();
 
 var summaries = new[]
 {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
 app.MapGet("/weatherforecast", (HttpContext httpContext) =>
 {
@@ -33,5 +35,19 @@ app.MapGet("/weatherforecast", (HttpContext httpContext) =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+app.MapGet("/v2/weatherforecast", (HttpContext httpContext) =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = summaries[Random.Shared.Next(summaries.Length)]
+        })
+        .ToArray();
+    return forecast;
+})
+.WithGroupName("v2");
 
 app.Run();
