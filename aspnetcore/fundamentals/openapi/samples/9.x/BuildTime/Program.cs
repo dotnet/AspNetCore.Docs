ff894fffa941e6f9b9a6fa9459160dd9092b99c9
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-08-21 17:35:05 -1000

    fix script

--- aspnetcore/fundamentals/openapi/samples/9.x/BuildTime/Program.cs
+++ aspnetcore/fundamentals/openapi/samples/9.x/BuildTime/Program.cs
@@ -1,12 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
