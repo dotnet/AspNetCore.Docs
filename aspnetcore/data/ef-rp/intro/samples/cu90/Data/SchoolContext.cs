6f15b096976507124ce8e7806d7d23a3960b6410
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-10-28 17:00:17 -0600

    Copy .NET 6 code to .NET 9 folder (#33960)

---
+++ aspnetcore/data/ef-rp/intro/samples/cu90/Data/SchoolContext.cs
@@ -0,0 +1,30 @@
+#define First // LAST First
+#if First
+// <snippet_first>
+using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
+// </snippet_first>
+#elif LAST
+#endif
