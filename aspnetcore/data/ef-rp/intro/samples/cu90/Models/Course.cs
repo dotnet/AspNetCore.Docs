43a314bbf1e3ace0684680f1237650fccccaa8be
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-10-28 17:00:17 -0600

    Copy .NET 6 code to .NET 9 folder (#33960)

---
+++ aspnetcore/data/ef-rp/intro/samples/cu90/Models/Course.cs
@@ -0,0 +1,46 @@
+#define FIRST
+#if FIRST
+// <snippet_first>
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
+// </snippet_first>
+#elif LAST
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+// <snippet_last>
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
+        public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Range(0, 5)]
+        public int Credits { get; set; }
+
+        public int DepartmentID { get; set; }
+
+        public Department Department { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<Instructor> Instructors { get; set; }
+    }
+}
+// </snippet_last>
+#endif
