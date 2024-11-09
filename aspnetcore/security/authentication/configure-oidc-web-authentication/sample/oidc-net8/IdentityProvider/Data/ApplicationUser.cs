b342f941ee47ec262229bf1b56c932b7280bd30b
damienbod <damien_bod@hotmail.com>
2024-11-09 09:34:01 +0100

    Clean up sample using feedback

--- aspnetcore/security/authentication/configure-oidc-web-authentication/sample/oidc-net8/IdentityProvider/Data/ApplicationUser.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace OpeniddictServer.Data;
-
-// Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser { }
