8d84cbd8551966172ce7a00fad2e1122d50d87b6
damienbod <damien_bod@hotmail.com>
2024-12-07 11:27:59 +0100

    Init sample

---
+++ aspnetcore/security/authentication/configure-jwt-bearer-authentication/sample/JwtBearer/Program.cs
@@ -0,0 +1,29 @@
+
+namespace JwtBearer;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
