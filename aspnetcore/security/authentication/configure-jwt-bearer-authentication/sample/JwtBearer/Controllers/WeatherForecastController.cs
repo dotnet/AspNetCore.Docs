f11022e194cdf2eeaccf099f316d4e63f5712d6e
damienbod <damien_bod@hotmail.com>
2024-12-08 15:44:03 +0100

    Add auth attribute for docs

--- aspnetcore/security/authentication/configure-jwt-bearer-authentication/sample/JwtBearer/Controllers/WeatherForecastController.cs
+++ aspnetcore/security/authentication/configure-jwt-bearer-authentication/sample/JwtBearer/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JwtBearer.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
