8e5dbef4d80117cef82735bd54f3f2d947c92054
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-12-09 12:25:09 -1000

    Add AOT /1

--- aspnetcore/fundamentals/openapi/samples/9.x/GetDocument.Insider/Program.cs
+++ aspnetcore/fundamentals/openapi/samples/9.x/GetDocument.Insider/Program.cs
@@ -2,15 +2,25 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddOpenApi();
+
 if (Assembly.GetEntryAssembly()?.GetName().Name != "GetDocument.Insider")
 {
    // 'IServiceCollection' does not contain a definition for 'AddDefaults' 
-   // builder.Services.AddDefaults();
+    builder.Services.AddDefaults();
 }
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
 var myKeyValue = app.Configuration["MyKey"];
 
-app.MapGet("/", () => myKeyValue);
+app.MapGet("/", () => { 
+    return Results.Ok($"The value of MyKey is: {myKeyValue}"); 
+})
+.WithName("TestKey");
 
 app.Run();
