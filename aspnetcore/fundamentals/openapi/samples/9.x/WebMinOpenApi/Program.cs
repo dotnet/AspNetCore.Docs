d91968b53327ce2868292a9be2123b36d7e6c503
Dave Callan <106764096+davepcallan@users.noreply.github.com>
2025-01-23 11:22:34 +0000

    Move MapOpenApi() Inside the IsDevelopment() block for consistency with templates and other docs
    The default template with .NET 9 has MapOpenApi() inside the IsDevelopment block. Many examples on MS Learn also have it inside due to limit information exposure. 
    
    Move inside to minimize potential for confusion to devs.

--- aspnetcore/fundamentals/openapi/samples/9.x/WebMinOpenApi/Program.cs
+++ aspnetcore/fundamentals/openapi/samples/9.x/WebMinOpenApi/Program.cs
@@ -279,9 +279,10 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-app.MapOpenApi();
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
+    
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/openapi/v1.json", "v1");
