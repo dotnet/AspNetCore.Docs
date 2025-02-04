0e000ed35077f5599f3f25b9692e9f16e0ee3a0c
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2025-02-04 13:29:10 -1000

    Kestrel named pipes

--- aspnetcore/fundamentals/servers/kestrel/endpoints/samples/KestrelNamedEP/Program.cs
+++ aspnetcore/fundamentals/servers/kestrel/endpoints/samples/KestrelNamedEP/Program.cs
@@ -1,4 +1,6 @@
 using System.IO.Pipes;
+using System.Security.AccessControl;
+using System.Security.Principal;
 
 var builder = WebApplication.CreateBuilder();
 
@@ -12,10 +14,24 @@ builder.WebHost.UseNamedPipes(options =>
 {
     options.CreateNamedPipeServerStream = (context) =>
     {
-        var pipeSecurity = CreatePipeSecurity(context.NamedPipeEndpoint.PipeName);
+        var pipeSecurity = CreatePipeSecurity(context.NamedPipeEndPoint.PipeName);
 
-        return NamedPipeServerStreamAcl.Create(context.NamedPipeEndPoint.PipeName, PipeDirection.InOut,
+        return NamedPipeServerStreamAcl.Create(context.NamedPipeEndPoint.PipeName,PipeDirection.InOut,
             NamedPipeServerStream.MaxAllowedServerInstances, PipeTransmissionMode.Byte,
-            context.PipeOptions, inBufferSize: 0, outBufferSize: 0, pipeSecurity);
+            PipeOptions.None, inBufferSize: 0, outBufferSize: 0, pipeSecurity);
     };
 });
+
+static PipeSecurity CreatePipeSecurity(string pipeName)
+{
+    var pipeSecurity = new PipeSecurity();
+
+    // Get the current process identity.
+    var currentIdentity = WindowsIdentity.GetCurrent();
+    var processUser = new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, currentIdentity.User.AccountDomainSid);
+
+    // Allow only the current process read and write access to the pipe.
+    pipeSecurity.AddAccessRule(new PipeAccessRule(processUser, PipeAccessRights.ReadWrite, AccessControlType.Allow));
+
+    return pipeSecurity;
+}
