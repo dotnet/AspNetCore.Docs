3c483c5bc0f0bd39459e3b323dacaae552f25847
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-02-16 14:05:24 +0100

    add OpenAPI example (no swagger)

---
+++ aspnetcore/tutorials/first-web-api/samples/9.0/TodoApi/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
