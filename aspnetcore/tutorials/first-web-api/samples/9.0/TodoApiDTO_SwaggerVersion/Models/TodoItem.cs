b398f8fa656ad92074cbe704c5a8fb5b78d992a2
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-02-17 19:18:42 +0100

    remove unused example

--- aspnetcore/tutorials/first-web-api/samples/9.0/TodoApiDTO_SwaggerVersion/Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-namespace TodoApi.Models;
-
-public class TodoItem
-{
-    public long Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsComplete { get; set; }
-    public string? Secret { get; set; }
-}
--- aspnetcore/tutorials/first-web-api/samples/9.0/TodoApiDTO_SwaggerVersion/Models/TodoItemDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace TodoApi.Models;
-
-public class TodoItemDTO
-{
-    public long Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsComplete { get; set; }
-}
