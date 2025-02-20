5b76b7bbd61c982563e3b942dd69775bd65604a1
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-02-20 21:53:44 +0100

    update WebApi to .NET 9 (without Swagger) (#34746)
    * Update .NET version references in first-web-api.md to 9.0
    
    * update sample to target .NET 9.0
    
    * rename example to _SwaggerExample
    
    * add OpenAPI example (no swagger)
    
    * update WebApi to .NET 9 (without swagger)
    
    * remove unused example
    
    * review fixes
    
    * apply changes from #34676
    
    * include IsDevelopment codeblock to match the text description
    
    * VS new project settings moved to order they appear in dialog
    
    * Right-click or Command-click on project in VSC
    
    Control-click on a project in VSC on windows does not work, must be Right-click for Windows and Control-click on macOS for VSC.
    
    * Specifying the DTO file and location in the instruction
    
    * Apply suggestions from code review
    
    * Update author names in web API tutorial
    
    * Add comment block in first-web-api tutorial
    
    * Apply suggestions from code review
    
    revert
    
    * Update aspnetcore/tutorials/first-web-api.md
    
    * remove unused file 'create-new-project'
    
    ---------
    
    Co-authored-by: Wade Pickett <wpickett@microsoft.com>
    Co-authored-by: Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>

--- aspnetcore/tutorials/first-web-api/samples/9.0/TodoApiDTO/Program.cs
+++ aspnetcore/tutorials/first-web-api/samples/9.0/TodoApiDTO/Program.cs
@@ -4,17 +4,15 @@ using TodoApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddOpenApi();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
--- aspnetcore/tutorials/first-web-api/samples/9.0/TodoApi_SwaggerVersion/Program.cs
+++ aspnetcore/tutorials/first-web-api/samples/9.0/TodoApi_SwaggerVersion/Program.cs
@@ -8,12 +8,18 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
 
+// <snippet_UseSwagger>
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
 }
+// </snippet_UseSwagger>
 
 app.UseHttpsRedirection();
 
