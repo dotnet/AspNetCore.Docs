d78662ecf3911a2d972789faa7670dcadb106831
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2025-04-04 14:28:48 -1000

    SSE moniker prep (#35150)
    * SSE moniker prep
    
    * SSE moniker prep

---
+++ aspnetcore/fundamentals/minimal-apis/10.0-samples/MinimalServerSentEvents/Program.cs
@@ -0,0 +1,70 @@
+using System.Net.ServerSentEvents;
+using System.Runtime.CompilerServices;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// <snippet_string>
+app.MapGet("/string-item", (CancellationToken cancellationToken) =>
+{
+    async IAsyncEnumerable<string> GetHeartRate(
+        [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var heartRate = Random.Shared.Next(60, 100);
+            yield return $"Hear Rate: {heartRate} bpm";
+            await Task.Delay(2000, cancellationToken);
+        }
+    }
+
+    return TypedResults.ServerSentEvents(GetHeartRate(cancellationToken), eventType: "heartRate");
+});
+// </snippet_string>
+
+// <snippet_json>
+app.MapGet("/json-item", (CancellationToken cancellationToken) =>
+{
+    async IAsyncEnumerable<HearRate> GetHeartRate(
+        [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var heartRate = Random.Shared.Next(60, 100);
+            yield return HearRate.Create(heartRate);
+            await Task.Delay(2000, cancellationToken);
+        }
+    }
+
+    return TypedResults.ServerSentEvents(GetHeartRate(cancellationToken), eventType: "heartRate");
+});
+// </snippet_json>
+
+// <snippet_item>
+app.MapGet("sse-item", (CancellationToken cancellationToken) =>
+{
+    async IAsyncEnumerable<SseItem<int>> GetHeartRate(
+        [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var heartRate = Random.Shared.Next(60, 100);
+            yield return new SseItem<int>(heartRate, eventType: "heartRate")
+            {
+                ReconnectionInterval = TimeSpan.FromMinutes(1)
+            };
+            await Task.Delay(2000, cancellationToken);
+        }
+    }
+
+    return TypedResults.ServerSentEvents(GetHeartRate(cancellationToken));
+});
+// </snippet_item>
+
+
+app.Run();
+
+public record HearRate(DateTime Timestamp, int HeartRate)
+{
+    public static HearRate Create(int heartRate) => new(DateTime.UtcNow, heartRate);
+}
