5e7022dc4168f9a510c014b09728fd220542d2f3
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-04-20 01:59:22 +0200

    document ConfigurationKeyName (#35274)

---
+++ aspnetcore/fundamentals/configuration/options/samples/6.x/OptionsSample/Models/PositionOptionsWithConfigurationKeyName.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleApp.Models
+{
+    #region snippet
+    public class PositionOptionsWithConfigurationKeyName
+    {
+        public const string Position = "Position";
+
+        [ConfigurationKeyName("position-title")]
+        public string Title { get; set; } = string.Empty;
+
+        [ConfigurationKeyName("position-name")]
+        public string Name { get; set; } = string.Empty;
+    }
+    #endregion
+}
