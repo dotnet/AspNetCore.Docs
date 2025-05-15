236aded05b44cc9984e9094fbf5c934afcdbdea5
Jono Rogers <jonorogers@users.noreply.github.com>
2025-05-16 04:50:04 +1000

    Integration tests documentation add nunit mstest (#35328)

---
+++ aspnetcore/test/integration-tests/snippets/xunit/IntegrationTests/BasicTests.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace RazorPagesProject.Tests.IntegrationTests;
+
+// <snippet1>
+public class BasicTests 
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public BasicTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Theory]
+    [InlineData("/")]
+    [InlineData("/Index")]
+    [InlineData("/About")]
+    [InlineData("/Privacy")]
+    [InlineData("/Contact")]
+    public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(url);
+
+        // Assert
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        Assert.Equal("text/html; charset=utf-8", 
+            response.Content.Headers.ContentType.ToString());
+    }
+}
+// </snippet1>
