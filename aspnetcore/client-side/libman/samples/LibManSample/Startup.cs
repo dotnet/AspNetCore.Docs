8f790a1c201eaaa236c1ac64a76f76d1bba3326a
John Zabroski <johnzabroski@gmail.com>
2025-05-21 09:29:10 -0400

    Update the LibMan sample app (#35497)

--- aspnetcore/client-side/libman/samples/LibManSample/Startup.cs
+++ aspnetcore/client-side/libman/samples/LibManSample/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace LibManSample
 {
@@ -13,12 +14,12 @@ namespace LibManSample
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices()
         {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
