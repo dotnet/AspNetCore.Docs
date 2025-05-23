0d3f9112dd2efb174cf4ed073c5ed616ab9acca5
Dmitrii Korolev <dmkorolev@microsoft.com>
2025-05-23 15:42:29 +0200

    add description

--- aspnetcore/mvc/models/file-uploads/samples/9.x/FileManagerSample/Program.cs
+++ aspnetcore/mvc/models/file-uploads/samples/9.x/FileManagerSample/Program.cs
@@ -1,21 +1,21 @@
+using System.Drawing;
 using Microsoft.AspNetCore.Http.Features;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure Kestrel to allow large files
+// allow large files
 builder.WebHost.ConfigureKestrel(options =>
 {
+    // if not present, will throw similar exception:
+        // fail: FileManagerSample.Controllers.FileController[0]
+        //   Error during file upload
+        //   Microsoft.AspNetCore.Server.Kestrel.Core.BadHttpRequestException: Request body too large. The max request body size is 30000000 bytes.
     options.Limits.MaxRequestBodySize = 6L * 1024 * 1024 * 1024; // 6 GB
 
     // timeout settings
     options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10); // Extend timeout for large file uploads
     options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(10);
-});
-
-// Configure FormOptions to allow large multipart body
-builder.Services.Configure<FormOptions>(options =>
-{
-    options.MultipartBodyLengthLimit = 6L * 1024 * 1024 * 1024; // 6 GB
 });
 
 builder.Services.AddControllers();
