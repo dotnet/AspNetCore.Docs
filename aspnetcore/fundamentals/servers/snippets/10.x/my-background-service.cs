8f0edaf725f9bbc1f4896eda4143c24b6a8534f2
Tom Dykstra <tdykstra@microsoft.com>
2025-08-04 11:33:05 -0700

    Automatic eviction from memory pool - Kestrel and HTTP.sys (#35822)
    * Update aspnetcore/fundamentals/servers/includes/memory-eviction2.md
    
    * ms.date
    
    * Change link to xref link
    
    * Change date to trigger build of include
    
    * check MaxBufferSize
    
    Co-authored-by: Brennan <brecon@microsoft.com>
    
    ---------
    
    Co-authored-by: Brennan <brecon@microsoft.com>

---
+++ aspnetcore/fundamentals/servers/snippets/10.x/my-background-service.cs
@@ -0,0 +1,28 @@
+public class MyBackgroundService : BackgroundService
+{
+    private readonly MemoryPool<byte> _memoryPool;
+
+    public MyBackgroundService(IMemoryPoolFactory<byte> factory)
+    {
+        _memoryPool = factory.Create();
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                await Task.Delay(20, stoppingToken);
+                // do work that needs memory
+                // consider checking _memoryPool.MaxBufferSize
+                var rented = _memoryPool.Rent(100);
+                rented.Dispose();
+            }
+            catch (OperationCanceledException)
+            {
+                return;
+            }
+        }
+    }
+}
