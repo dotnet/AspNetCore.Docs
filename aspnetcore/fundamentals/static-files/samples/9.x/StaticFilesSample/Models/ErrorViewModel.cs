cecbd3bebd2a7b5f2eda598c1cf763a5adff47f0
guardrex <1622880+guardrex@users.noreply.github.com>
2025-08-20 10:01:08 -0400

    Updates

--- aspnetcore/fundamentals/static-files/samples/9.x/StaticFilesSample/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StaticFilesSample.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
