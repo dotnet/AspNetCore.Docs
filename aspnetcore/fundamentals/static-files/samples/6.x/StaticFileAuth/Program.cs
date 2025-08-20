bdfa8edfa1d8971f4af867fc49bb95bed0a9f99b
guardrex <1622880+guardrex@users.noreply.github.com>
2025-08-20 10:14:38 -0400

    Revert "Updates"
    This reverts commit cecbd3bebd2a7b5f2eda598c1cf763a5adff47f0.

---
+++ aspnetcore/fundamentals/static-files/samples/6.x/StaticFileAuth/Program.cs
@@ -0,0 +1,139 @@
+#define DEFAULT // DEFAULT AUTH AUTH2
+#if NEVER
+#elif DEFAULT
+// <snippet>
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StaticFileAuth.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+// </snippet>
+#elif AUTH
+// <snippet_auth>
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using StaticFileAuth.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "MyStaticFiles")),
+    RequestPath = "/StaticFiles"
+});
+
+app.MapRazorPages();
+
+app.Run();
+// </snippet_auth>
+#elif AUTH2
+// <snippet_auth2>
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StaticFileAuth.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+// </snippet_auth2>
+#endif
---
+++ aspnetcore/fundamentals/static-files/samples/9.x/StaticFilesSample/Program.cs
@@ -0,0 +1,474 @@
+#define TREE // DEFAULT RR RH DB DF DF2 UFS UFS2 TREE FECTP NS MUL MULT2
+// Test1
+#if NEVER
+#elif DEFAULT
+// <snippet>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.MapStaticAssets();
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+// </snippet>
+#elif RR
+// <snippet_rr>
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();    //Serve files from wwwroot
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "MyStaticFiles")),
+    RequestPath = "/StaticFiles"
+});
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+// </snippet_rr>
+#elif RH
+// <snippet_rh>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+var cacheMaxAgeOneWeek = (60 * 60 * 24 * 7).ToString();
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers.Append(
+            "Cache-Control", $"public, max-age={cacheMaxAgeOneWeek}");
+    }
+});
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+// </snippet_rh>
+#elif DB  // Directory Browsing
+// <snippet_db>
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDirectoryBrowser();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.MapStaticAssets();
+
+var fileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.WebRootPath, "images"));
+var requestPath = "/MyImages";
+
+// Enable displaying browser links.
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = fileProvider,
+    RequestPath = requestPath
+});
+
+app.UseDirectoryBrowser(new DirectoryBrowserOptions
+{
+    FileProvider = fileProvider,
+    RequestPath = requestPath
+});
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+// </snippet_db>
+#elif DF  // Default file
+https://localhost:50921/def/
+// <snippet_df>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseDefaultFiles();
+
+app.UseStaticFiles();
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+// </snippet_df>
+#elif DF2
+// https://localhost:50921/def/
+// <snippet_df2>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+var options = new DefaultFilesOptions();
+options.DefaultFileNames.Clear();
+options.DefaultFileNames.Add("mydefault.html");
+app.UseDefaultFiles(options);
+
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+// </snippet_df2>
+#elif UFS
+// <snippet_ufs>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseFileServer();
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+// </snippet_ufs>
+#elif UFS2
+// <snippet_ufs2>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDirectoryBrowser();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseFileServer(enableDirectoryBrowsing: true);
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+// </snippet_ufs2>
+#elif TREE
+// <snippet_tree>
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDirectoryBrowser();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseFileServer(new FileServerOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "MyStaticFiles")),
+    RequestPath = "/StaticFiles",
+    EnableDirectoryBrowsing = true
+});
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+// </snippet_tree>
+#elif FECTP
+// <snippet_fec>
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+// Set up custom content types - associating file extension to MIME type
+var provider = new FileExtensionContentTypeProvider();
+// Add new mappings
+provider.Mappings[".myapp"] = "application/x-msdownload";
+provider.Mappings[".htm3"] = "text/html";
+provider.Mappings[".image"] = "image/png";
+// Replace an existing mapping
+provider.Mappings[".rtf"] = "application/x-msdownload";
+// Remove MP4 videos.
+provider.Mappings.Remove(".mp4");
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    ContentTypeProvider = provider
+});
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+// </snippet_fec>
+#elif NS
+// <snippet_ns>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    ServeUnknownFileTypes = true,
+    DefaultContentType = "image/png"
+});
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+// </snippet_ns>
+#elif MUL
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+// <snippet_mul>
+app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "MyStaticFiles"))
+});
+// </snippet_mul>
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+#elif MULT2
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+// <snippet_mult2>
+var webRootProvider = new PhysicalFileProvider(builder.Environment.WebRootPath);
+var newPathProvider = new PhysicalFileProvider(
+    Path.Combine(builder.Environment.ContentRootPath, "MyStaticFiles"));
+
+var compositeProvider = new CompositeFileProvider(webRootProvider, newPathProvider);
+
+// Update the default provider.
+app.Environment.WebRootFileProvider = compositeProvider;
+
+app.MapStaticAssets();
+// </snippet_mult2>
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+#elif Test1   // no snippet to import, just for testing
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles(); // Serve files from wwwroot
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "MyStaticFiles")),
+    RequestPath = "/StaticFiles"
+});
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute().WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+app.Run();
+#endif
