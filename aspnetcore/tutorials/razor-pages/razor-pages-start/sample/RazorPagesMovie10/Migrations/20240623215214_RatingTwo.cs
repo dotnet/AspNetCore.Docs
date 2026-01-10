ba5b60206c2cf10065c3b6dc0934ebf5741f8a74
Meaghan Osagie <mosagie@microsoft.com>
2026-01-09 17:32:21 -0800

    add RazorPagesSampleMovie10

---
+++ aspnetcore/tutorials/razor-pages/razor-pages-start/sample/RazorPagesMovie10/Migrations/20240623215214_RatingTwo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class RatingTwo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
