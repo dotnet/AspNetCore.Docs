d0c4200ef0ef40243d5ba932c5c4a81f6a600eec
wadepickett <wpickett@microsoft.com>
2026-05-27 14:58:19 -0700

    Update sample to mikekistler aspnet-json-patch latest

--- aspnetcore/web-api/jsonpatch/samples/10.x/JsonPatchSample/CustomerApi.cs
+++ aspnetcore/web-api/jsonpatch/samples/10.x/JsonPatchSample/CustomerApi.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.JsonPatch.SystemTextJson;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using System.ComponentModel.DataAnnotations;
 using App.Data;
 using App.Models;
 
@@ -10,20 +11,42 @@ internal static class CustomerApi {
     {
         var group = routes.MapGroup("/customers").WithTags("Customers");
 
-        group.MapGet("/{id}", async Task<Results<Ok<Customer>, NotFound>> (AppDb db, string id) =>
+        group.MapGet("/{id}", async Task<Results<Ok<Customer>, NotFound<ProblemDetails>>> (AppDb db, string id) =>
         {
             return await db.Customers.Include(c => c.Orders).FirstOrDefaultAsync(c => c.Id == id) is Customer customer
                 ? TypedResults.Ok(customer)
-                : TypedResults.NotFound();
+                : TypedResults.NotFound<ProblemDetails>(new ());
         });
 
-        group.MapPatch("/{id}", async Task<Results<Ok<Customer>,NotFound,BadRequest, ValidationProblem>> (AppDb db, string id,
+        group.MapPut("/{id}", async Task<Results<Ok<Customer>,Created<Customer>,ValidationProblem>> (AppDb db, string id, Customer body) =>
+        {
+            var customer = await db.Customers.Include(c => c.Orders).FirstOrDefaultAsync(c => c.Id == id);
+            if (customer is null)
+            {
+                body.Id = id;
+                db.Customers.Add(body);
+                await db.SaveChangesAsync();
+                return TypedResults.Created($"/customers/{id}", body);
+            }
+
+            customer.Name = body.Name;
+            customer.Email = body.Email;
+            customer.PhoneNumber = body.PhoneNumber;
+            customer.Address = body.Address;
+
+            await db.SaveChangesAsync();
+
+            return TypedResults.Ok(customer);
+        });
+
+        // <snippet_PatchMethod>
+        group.MapPatch("/{id}", async Task<Results<Ok<Customer>,ValidationProblem,NotFound<ProblemDetails>>> (AppDb db, string id,
             JsonPatchDocument<Customer> patchDoc) =>
         {
             var customer = await db.Customers.Include(c => c.Orders).FirstOrDefaultAsync(c => c.Id == id);
             if (customer is null)
             {
-                return TypedResults.NotFound();
+                return TypedResults.NotFound<ProblemDetails>(new ());
             }
             if (patchDoc != null)
             {
@@ -38,15 +61,19 @@ internal static class CustomerApi {
                         }
                         errors[key] = errors[key].Append(jsonPatchError.ErrorMessage).ToArray();
                     });
+
                 if (errors != null)
                 {
                     return TypedResults.ValidationProblem(errors);
                 }
+
+                // Only save if there are no errors
                 await db.SaveChangesAsync();
             }
 
             return TypedResults.Ok(customer);
         })
         .Accepts<JsonPatchDocument<Customer>>("application/json-patch+json");
+        // </snippet_PatchMethod>
     }
 }
